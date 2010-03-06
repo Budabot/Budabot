7fdfac3fd9bb60170a653f6d88f2c2cfe28ef390
Tyrence <bigwheels16@gmail.com>
2010-03-06 08:16:14 +0000

    updated modules to look for .sql files in their local directories instead of the /sql/ directory

--- modules/BOSSLOOT_MODULE/setup.php
+++ modules/BOSSLOOT_MODULE/setup.php
@@ -10,13 +10,10 @@
    Last Modified 5/14/07
    */
    
-copy("./modules/BOSSLOOT_MODULE/boss_namedb.sql", "./sql/boss_namedb.sql");
-copy("./modules/BOSSLOOT_MODULE/boss_lootdb.sql", "./sql/boss_lootdb.sql");
-   
 $db->query("CREATE TABLE IF NOT EXISTS boss_namedb (`bossid` int(10),`bossname` varchar(50),`location` varchar(50), 'keyname' varchar(200))");
 
 $db->query("SELECT * FROM boss_namedb");
-$filearray = file("./sql/boss_namedb.sql");
+$filearray = file("./modules/BOSSLOOT_MODULE/boss_namedb.sql");
 if ($db->numrows() != count($filearray)) {
 	$db->query("DELETE FROM boss_namedb");
 	echo "Creating Boss Name Database. \nDepending on the Database you are using this process can take a few minutes.\n";
@@ -31,7 +28,7 @@ if ($db->numrows() != count($filearray)) {
 $db->query("CREATE TABLE IF NOT EXISTS boss_lootdb (`bossid` int(10),`itemid` int(10), 'itemname' varchar (100))");
 
 $db->query("SELECT * FROM boss_lootdb");
-$filearray = file("./sql/boss_lootdb.sql");
+$filearray = file("./modules/BOSSLOOT_MODULE/boss_lootdb.sql");
 if ($db->numrows() != count($filearray)) {
 	$db->query("DELETE FROM boss_lootdb");
 	echo "Creating Boss Loot Database. \nDepending on the Database you are using this process can take a few minutes.\n";
--- modules/POCKETBOSS_MODULE/setup.php
+++ modules/POCKETBOSS_MODULE/setup.php
@@ -2,7 +2,7 @@
 $db->query("CREATE TABLE IF NOT EXISTS pbdb (`id` INT, `pb` VARCHAR(30), `pb_location` VARCHAR(50), `bp_mob` VARCHAR(50), `bp_lvl` INT, `bp_location` VARCHAR(50), `type` VARCHAR(25), `slot` VARCHAR(25), `line` VARCHAR(25), `ql` VARCHAR(5), `itemid` INT)");
 
 $db->query("SELECT * FROM pbdb");
-$filearray = file("./sql/pbdb.sql");
+$filearray = file("./modules/POCKETBOSS_MODULE/pbdb.sql");
 if($db->numrows() != count($filearray)) {
   	$db->query("DELETE FROM pbdb");
 	echo "Creating Pocketboss Database. \nDepending on the Database you are using this process can take a few mins.\n";	
