3df9136cc4190a2e07b1be2f14747c6723e01f3b
Tyrence <bigwheels16@gmail.com>
2010-03-18 04:00:33 +0000

    updated news table to use INTEGER instead of INT (even tho the sql class will convert it)

--- modules/ADV_GUILD_MODULE/setup.php
+++ modules/ADV_GUILD_MODULE/setup.php
@@ -1,5 +1,5 @@
 <?
-$db->query("CREATE TABLE IF NOT EXISTS org_city_<myname> (`time` INT, `action` VARCHAR(10), `player` VARCHAR(25))");
-$db->query("CREATE TABLE IF NOT EXISTS news_<myname> (`id` INT PRIMARY KEY AUTO_INCREMENT, `time` INT NOT NULL, `name` VARCHAR(30), `news` TEXT)");
+$db->query("CREATE TABLE IF NOT EXISTS org_city_<myname> (`time` INTEGER, `action` VARCHAR(10), `player` VARCHAR(25))");
+$db->query("CREATE TABLE IF NOT EXISTS news_<myname> (`id` INTEGER PRIMARY KEY AUTO_INCREMENT, `time` INT NOT NULL, `name` VARCHAR(30), `news` TEXT)");
 $this->vars["newsdelay"] = time() + $this->settings["CronDelay"] + 60;
 ?>
