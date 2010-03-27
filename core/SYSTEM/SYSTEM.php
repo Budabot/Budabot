eb058dc96478a1f4f94a9a63f33a61b20b62793a
Tyrence <bigwheels16@gmail.com>
2010-03-27 18:14:45 +0000

    extended messages parsed using sql import from vhab's magical mmdb tool v1.2; added mmdb sql file to be loaded from the core SYSTEM module

--- core/SYSTEM/SYSTEM.php
+++ core/SYSTEM/SYSTEM.php
@@ -2,6 +2,9 @@
 $MODULE_NAME = "SYSTEM";
 $PLUGIN_VERSION = 0.1;
 
+	//Load extended messages
+	bot::loadSQLFile($MODULE_NAME, "mmdb");
+
 	//Commands
 	bot::regcommand("msg", "$MODULE_NAME/plugins.php", "newplugins", "admin");
 	bot::regcommand("msg", "$MODULE_NAME/reboot.php", "reboot", "admin");
