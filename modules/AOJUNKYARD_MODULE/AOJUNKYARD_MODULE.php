92826859f564e11746568e40bb7b1ba95db8c39e
Tyrence <bigwheels16@gmail.com>
2010-04-26 06:03:26 +0000

    added AOJUNKYARD_MODULE

---
+++ modules/AOJUNKYARD_MODULE/AOJUNKYARD_MODULE.php
@@ -0,0 +1,9 @@
+<?php
+
+	$MODULE_NAME = "AOJUNKYARD_MODULE";
+
+	//Show Members
+	bot::command("", "$MODULE_NAME/wtb.php", "wtb", "all", "Brings up a listing of items that have been posting to shopping channel");
+	bot::event("extPriv", "$MODULE_NAME/wtb.php", "none", "");
+
+?>
