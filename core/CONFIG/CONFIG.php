a9fb466b6ce591c52566625f24dfb0f4498a9311
Tyrence <bigwheels16@gmail.com>
2010-05-09 10:19:48 +0000

    made it so !config could be use in prv and org channels not just tells

--- core/CONFIG/CONFIG.php
+++ core/CONFIG/CONFIG.php
@@ -4,6 +4,8 @@ $PLUGIN_VERSION = 0.1;
 
 	//Commands
 	bot::regcommand("msg", "$MODULE_NAME/cmdcfg.php", "config", "mod");
+	bot::regcommand("org", "$MODULE_NAME/cmdcfg.php", "config", "mod");
+	bot::regcommand("prv", "$MODULE_NAME/cmdcfg.php", "config", "mod");
 
 	//Help Files
 	bot::help("config", "$MODULE_NAME/config.txt", "mod", "Configure Commands/Events of the Bot.", "Configuration of the Bot");
