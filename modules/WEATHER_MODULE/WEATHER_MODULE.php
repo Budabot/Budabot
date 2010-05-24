6fc414a6e670045bd7903120448cf24d77a023d0
Arsenal <effendiarsenal@gmx.de>
2010-05-24 14:28:25 +0000

    Added WEATHER_MODULE back in (had it in an old installation)

---
+++ modules/WEATHER_MODULE/WEATHER_MODULE.php
@@ -0,0 +1,8 @@
+﻿<?php
+$MODULE_NAME = "WEATHER_MODULE";
+$PLUGIN_VERSION = 0.1;
+
+bot::command("", "$MODULE_NAME/weather.php", "weather", "all", "View Weather");
+
+bot::help("weather", "$MODULE_NAME/weather.txt", "guild", "Get weather info.", "Weather Module"); 
+?>
