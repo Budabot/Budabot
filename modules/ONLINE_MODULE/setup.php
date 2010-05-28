2609b5abd8cc6e1daa5a0bd02b76a9b55a9fd62a
Tyrence <bigwheels16@gmail.com>
2010-05-28 21:44:31 +0000

    move sql statement for guild_chatlist to a sql file

--- modules/ONLINE_MODULE/setup.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db->query("CREATE TABLE IF NOT EXISTS priv_chatlist_<myname> (`name` CHAR(25) PRIMARY KEY, `faction` CHAR(10), `profession` CHAR(20), `guild` CHAR(255), `breed` CHAR(25), `level` INT, `ai_level` INT, `afk` VARCHAR(255) DEFAULT '0', `guest` INT DEFAULT '0')");
-
-$db->query("CREATE TABLE IF NOT EXISTS guild_chatlist_<myname> (`name` CHAR(25) PRIMARY KEY, `profession` CHAR(20), `guild` CHAR(255), `rank` CHAR(25), `breed` CHAR(25), `level` INT, `ai_level` INT, `afk` VARCHAR(255) DEFAULT '0')");
-?>
