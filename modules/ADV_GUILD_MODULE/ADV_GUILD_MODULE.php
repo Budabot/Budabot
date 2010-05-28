7ced0e9494d191f6809a7a292ae5d24a1810fbd5
Tyrence <bigwheels16@gmail.com>
2010-05-28 19:36:49 +0000

    moved files relating to relay using tells from ADV_GUILD_MODULE to RELAY_MODULE

--- modules/ADV_GUILD_MODULE/ADV_GUILD_MODULE.php
+++ modules/ADV_GUILD_MODULE/ADV_GUILD_MODULE.php
@@ -8,20 +8,9 @@
 	bot::subcommand("", "$MODULE_NAME/news.php", "news (.+)", "guildadmin", "news", "Add News");
 	bot::subcommand("", "$MODULE_NAME/news.php", "news del (.+)", "guildadmin", "news", "Delete a Newsentry");
 
-	//Guildrelay
-  	bot::event("guild", "$MODULE_NAME/relay_guild_out.php", "grc", "Send Guildrelay messages");
-	bot::event("logOn", "$MODULE_NAME/relay_guild_logon.php", "grc", "Sends Logon messages");
-	bot::event("logOff", "$MODULE_NAME/relay_guild_logoff.php", "grc", "Sends Logoff messages");
-  	bot::command("msg", "$MODULE_NAME/relay_guild_inc.php", "grc", "all", "Relays incoming messages to guildchat");
-  	bot::command("msg", "$MODULE_NAME/relay_guild_cfg.php", "guildrelay", "all", "Relay between guildchats.");
-  	bot::command("priv", "$MODULE_NAME/relay_guild_cfg.php", "guildrelay", "all", "Relay between guildchats.");
-	bot::regGroup("Guild_Relay", $MODULE_NAME, "Relay Chat between guilds", "grc", "guildrelay");
-	bot::addsetting("relaybot", "Bot for Guildrelay", "noedit", "0", "none", '0', "mod", "$MODULE_NAME/relaybot_help.txt");
-
 	//Setup
 	bot::event("setup", "$MODULE_NAME/setup.php");
 
 	//Helpfiles
-	bot::help("guildrelay", "$MODULE_NAME/guildrelay.txt", "guild", "How to relay chats between two guilds", "Org Commands");
 	bot::help("news", "$MODULE_NAME/news.txt", "guild", "News", "Org Commands");
 ?>
--- modules/RELAY_MODULE/RELAY_MODULE.php
+++ modules/RELAY_MODULE/RELAY_MODULE.php
@@ -2,6 +2,7 @@
 	$MODULE_NAME = "RELAY_MODULE";
 	$PLUGIN_VERSION = 1.0;
 	
+	// Guild relay using pgroup
 	bot::command("msg", "$MODULE_NAME/send_relay_message.php", "@test", "mod", "Test relay");
 
 	bot::event("guild", "$MODULE_NAME/send_relay_message.php", "none");
@@ -11,4 +12,17 @@
 	
 	bot::addsetting("externalrelaybot", "Bot for external relay", "edit", "unknown", "text", '0', "mod", "");
 	bot::addsetting("externalrelaysymbol", "Symbol for external relay", "edit", "@", "!;#;*;@;$;+;-", '0', "mod", "");
+	
+	// Guild relay using tells
+  	bot::event("guild", "$MODULE_NAME/relay_guild_out.php", "grc", "Send Guildrelay messages");
+	bot::event("logOn", "$MODULE_NAME/relay_guild_logon.php", "grc", "Sends Logon messages");
+	bot::event("logOff", "$MODULE_NAME/relay_guild_logoff.php", "grc", "Sends Logoff messages");
+  	bot::command("msg", "$MODULE_NAME/relay_guild_inc.php", "grc", "all", "Relays incoming messages to guildchat");
+  	bot::command("msg", "$MODULE_NAME/relay_guild_cfg.php", "guildrelay", "all", "Relay between guildchats.");
+  	bot::command("priv", "$MODULE_NAME/relay_guild_cfg.php", "guildrelay", "all", "Relay between guildchats.");
+	bot::regGroup("Guild_Relay", $MODULE_NAME, "Relay Chat between guilds", "grc", "guildrelay");
+	bot::addsetting("relaybot", "Bot for Guildrelay", "noedit", "0", "none", '0', "mod", "$MODULE_NAME/relaybot_help.txt");
+	
+	//Helpfiles
+	bot::help("guildrelay", "$MODULE_NAME/guildrelay.txt", "guild", "How to relay chats between two guilds", "Org Commands");
 ?>
