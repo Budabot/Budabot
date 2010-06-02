4d254e87d74c1c53b5d32b7f39cc330d98046e2e
Tyrence <bigwheels16@gmail.com>
2010-06-02 06:16:15 +0000

    added !uptime command

--- core/SYSTEM/SYSTEM.php
+++ core/SYSTEM/SYSTEM.php
@@ -7,14 +7,20 @@
 
 	//Commands
 	bot::regcommand("msg", "$MODULE_NAME/plugins.php", "newplugins", "admin");
-	bot::regcommand("msg", "$MODULE_NAME/reboot.php", "reboot", "admin");
-	bot::regcommand("msg", "$MODULE_NAME/shutdown.php", "shutdown", "admin");
 	bot::regcommand("priv", "$MODULE_NAME/plugins.php", "newplugins", "admin");
-	bot::regcommand("priv", "$MODULE_NAME/reboot.php", "reboot", "admin");
-	bot::regcommand("priv", "$MODULE_NAME/shutdown.php", "shutdown", "admin");
 	bot::regcommand("guild", "$MODULE_NAME/plugins.php", "newplugins", "admin");
-	bot::regcommand("guild", "$MODULE_NAME/reboot.php", "reboot", "admin");
+	
+	bot::regcommand("msg", "$MODULE_NAME/reboot.php", "reboot", "admin");
+	bot::regcommand("priv", "$MODULE_NAME/reboot.php", "reboot", "admin");
+	bot::regcommand("guild", "$MODULE_NAME/reboot.php", "reboot", "admin");	
+	
+	bot::regcommand("msg", "$MODULE_NAME/shutdown.php", "shutdown", "admin");
+	bot::regcommand("priv", "$MODULE_NAME/shutdown.php", "shutdown", "admin");
 	bot::regcommand("guild", "$MODULE_NAME/shutdown.php", "shutdown", "admin");
+	
+	bot::regcommand("msg", "$MODULE_NAME/uptime.php", "uptime", "rl");
+	bot::regcommand("priv", "$MODULE_NAME/uptime.php", "uptime", "rl");
+	bot::regcommand("guild", "$MODULE_NAME/uptime.php", "uptime", "guild");
 
 	//Help Files
 	bot::help("systemhelp", "$MODULE_NAME/system.txt", "admin", "Admin System Help file.", "Administration");
--- main.php
+++ main.php
@@ -242,4 +242,55 @@ main(true, $chatBot);
 			return 0;
 		}
 	}
+
+	// taken from http://www.php.net/manual/en/function.date-diff.php
+	function date_difference($sdate, $edate)
+	{
+		$time = $edate - $sdate;
+		if($time>=0 && $time<=59) {
+			// Seconds
+			$timeshift = $time.' seconds';
+
+		} elseif($time>=60 && $time<=3599) {
+			// Minutes + Seconds
+			$pmin = ($edate - $sdate) / 60;
+			$premin = explode('.', $pmin);
+			
+			$presec = $pmin-$premin[0];
+			$sec = $presec*60;
+			
+			$timeshift = $premin[0].' min '.round($sec,0).' sec';
+
+		} elseif($time>=3600 && $time<=86399) {
+			// Hours + Minutes
+			$phour = ($edate - $sdate) / 3600;
+			$prehour = explode('.',$phour);
+			
+			$premin = $phour-$prehour[0];
+			$min = explode('.',$premin*60);
+			
+			$presec = '0.'.$min[1];
+			$sec = $presec*60;
+
+			$timeshift = $prehour[0].' hrs '.$min[0].' min '.round($sec,0).' sec';
+
+		} elseif($time>=86400) {
+			// Days + Hours + Minutes
+			$pday = ($edate - $sdate) / 86400;
+			$preday = explode('.',$pday);
+
+			$phour = $pday-$preday[0];
+			$prehour = explode('.',$phour*24); 
+
+			$premin = ($phour*24)-$prehour[0];
+			$min = explode('.',$premin*60);
+			
+			$presec = '0.'.$min[1];
+			$sec = $presec*60;
+			
+			$timeshift = $preday[0].' days '.$prehour[0].' hrs '.$min[0].' min '.round($sec,0).' sec';
+
+		}
+		return $timeshift;
+	}
 ?>
