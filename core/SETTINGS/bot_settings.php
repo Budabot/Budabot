43bf6f97cb03c7d370fe9081643509f23a14a73d
Tyrence <bigwheels16@gmail.com>
2010-06-11 01:41:34 +0000

    fix for settings when saving a value with quotes in it

--- core/SETTINGS/bot_settings.php
+++ core/SETTINGS/bot_settings.php
@@ -204,7 +204,7 @@ if (preg_match("/^settings$/i", $message)) {
 		}
 	}
 	if($new_setting != "") {
-		$db->query("UPDATE settings_<myname> SET `setting` = '$new_setting' WHERE `name` = '$name_setting'");	  	
+		$db->query("UPDATE settings_<myname> SET `setting` = '".str_replace("'", "''", $new_setting)."' WHERE `name` = '$name_setting'");	  	
 		$this->settings[$name_setting] = $new_setting;
 		$msg = "Setting successfull saved.";
 		//If the source is the config file renew it
