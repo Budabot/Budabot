50e09c770e5a5388b3c7d0328bddb0d618945b82
Tyrence <bigwheels16@gmail.com>
2010-06-16 07:11:18 +0000

    fix for !rembuddy to show syntax error when applicable

--- modules/FRIENDLIST_DIAG_MODULE/rembuddy.php
+++ modules/FRIENDLIST_DIAG_MODULE/rembuddy.php
@@ -4,13 +4,15 @@ if (preg_match("/^rembuddy (.+) (.+)$/i", $message, $arr)) {
 	$name = $arr[1];
 	$type = $arr[2];
 	
-	if($this->remove_buddy($name, $type)) {
+	if ($this->remove_buddy($name, $type)) {
 		$msg = "$name removed from the buddy list successfully.";
 	} else {
 		$msg = "Could not remove $name from the buddy list.";
 	}
 	
 	bot::send($msg, $sendto);
+} else {
+	$syntax_error = true;
 }
 
 ?>
