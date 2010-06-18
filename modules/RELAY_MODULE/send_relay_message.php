fe4ce25f5d72b8631b921b07b6e45c721460b1c4
Tyrence <bigwheels16@gmail.com>
2010-06-18 02:41:49 +0000

    minor fixes

--- modules/RELAY_MODULE/send_relay_message.php
+++ modules/RELAY_MODULE/send_relay_message.php
@@ -5,7 +5,7 @@ if ($this->settings["relaybot"] != "Off" && $args[2][0] != $this->settings["symb
 	if ($this->settings['relaysymbol'] == 'Always relay') {
 		$relayMessage = $message;
 	} else if (substr($message, 0, 1) == $this->settings['relaysymbol']) {
-		$relayMessage = substr($message, 1)
+		$relayMessage = substr($message, 1);
 	}
 
 	if ($relayMessage != '') {
