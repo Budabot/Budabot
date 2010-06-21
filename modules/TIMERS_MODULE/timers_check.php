361600228d1934e0f6fca4e29ca01e061caa4b24
Arsenal <effendiarsenal@gmx.de>
2010-06-21 14:00:09 +0000

    Fixed timers_check.php, it didn't display any messages because $sendto contains no useful data in an event, used the saved data in the array

--- modules/TIMERS_MODULE/timers_check.php
+++ modules/TIMERS_MODULE/timers_check.php
@@ -1,53 +1,53 @@
 <?php
-   /*
-   ** Author: Derroylo (RK2)
-   ** Description: Checks the running timers
-   ** Version: 1.2
-   **
-   ** Developed for: Budabot(http://sourceforge.net/projects/budabot)
-   **
-   ** Date(created): 26.12.2005
-   ** Date(last modified): 21.11.2006
-   ** 
-   ** Copyright (C) 2005, 2006 Carsten Lohmann
-   **
-   ** Licence Infos: 
-   ** This file is part of Budabot.
-   **
-   ** Budabot is free software; you can redistribute it and/or modify
-   ** it under the terms of the GNU General Public License as published by
-   ** the Free Software Foundation; either version 2 of the License, or
-   ** (at your option) any later version.
-   **
-   ** Budabot is distributed in the hope that it will be useful,
-   ** but WITHOUT ANY WARRANTY; without even the implied warranty of
-   ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   ** GNU General Public License for more details.
-   **
-   ** You should have received a copy of the GNU General Public License
-   ** along with Budabot; if not, write to the Free Software
-   ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-   */
+/*
+ ** Author: Derroylo (RK2)
+ ** Description: Checks the running timers
+ ** Version: 1.2
+ **
+ ** Developed for: Budabot(http://sourceforge.net/projects/budabot)
+ **
+ ** Date(created): 26.12.2005
+ ** Date(last modified): 21.11.2006
+ **
+ ** Copyright (C) 2005, 2006 Carsten Lohmann
+ **
+ ** Licence Infos:
+ ** This file is part of Budabot.
+ **
+ ** Budabot is free software; you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation; either version 2 of the License, or
+ ** (at your option) any later version.
+ **
+ ** Budabot is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with Budabot; if not, write to the Free Software
+ ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 //Check if at least one timer is running
 if(count($this->vars["Timers"]) == 0)
-	return;
+return;
 
 foreach($this->vars["Timers"] as $key => $value) {
-   	$msg = "";
+	$msg = "";
 
 	$tleft = $this->vars["Timers"][$key]["timer"] - time();
 	$set_time = $this->vars["Timers"][$key]["settime"];
 	$name = $this->vars["Timers"][$key]["name"];
 	$owner = $this->vars["Timers"][$key]["owner"];
 	$mode = $this->vars["Timers"][$key]["mode"];
-	
+
 	if($tleft <= 0 && $tleft >= -600) {
 		if($name == "PrimTimer")
-			$msg = "<highlight>$owner<end> your timer has gone off";
+		$msg = "<highlight>$owner<end> your timer has gone off";
 		else
-			$msg = "<highlight>$owner<end> your timer named <highlight>$name<end> has gone off";
-	
+		$msg = "<highlight>$owner<end> your timer named <highlight>$name<end> has gone off";
+
 		unset($this->vars["Timers"][$key]);
 		$db->query("DELETE FROM timers_<myname> WHERE `name` = '$name' AND `owner` = '$owner'");
 	} elseif ($tleft <= 0) {
@@ -55,21 +55,30 @@ foreach($this->vars["Timers"] as $key => $value) {
 		$db->query("DELETE FROM timers_<myname> WHERE `name` = '$name' AND `owner` = '$owner'");
 	} elseif ($tleft >= 3599 && $tleft < 3601 && ((time() - $set_time) >= 30)) {
 		if($name == "PrimTimer")
-			$msg = "Reminder: Timer has <highlight>1hour<end> left [set by <highlight>$owner<end>]";
+		$msg = "Reminder: Timer has <highlight>1hour<end> left [set by <highlight>$owner<end>]";
 		else
-			$msg = "Reminder: Timer <highlight>$name<end> has <highlight>1hour<end> left [set by <highlight>$owner<end>]";
+		$msg = "Reminder: Timer <highlight>$name<end> has <highlight>1hour<end> left [set by <highlight>$owner<end>]";
 	} elseif ($tleft >= 899 && $tleft < 901 && ((time() - $set_time) >= 30)) {
 		if($name == "PrimTimer")
-			$msg = "Reminder: Timer has <highlight>15minutes<end> left [set by <highlight>$owner<end>]";
+		$msg = "Reminder: Timer has <highlight>15minutes<end> left [set by <highlight>$owner<end>]";
 		else
-			$msg = "Reminder: Timer <highlight>$name<end> has <highlight>15minutes<end> left [set by <highlight>$owner<end>]";
+		$msg = "Reminder: Timer <highlight>$name<end> has <highlight>15minutes<end> left [set by <highlight>$owner<end>]";
 	} elseif ($tleft >= 59 && $tleft < 61 && ((time() - $set_time) >= 30)) {
 		if($name == "PrimTimer")
-			$msg = "Reminder: Timer has <highlight>1minute<end> left [set by <highlight>$owner<end>]";
+		$msg = "Reminder: Timer has <highlight>1minute<end> left [set by <highlight>$owner<end>]";
 		else
-			$msg = "Reminder: Timer <highlight>$name<end> has <highlight>1minute<end> left [set by <highlight>$owner<end>]";
+		$msg = "Reminder: Timer <highlight>$name<end> has <highlight>1minute<end> left [set by <highlight>$owner<end>]";
 	}
-
-   	bot::send($msg, $sendto);
+	if ('' != $msg)
+	{
+		if ('msg' == $mode)
+		{
+			bot::send($msg, $owner);
+		}
+		else
+		{
+			bot::send($msg, $mode);
+		}
+	}
 }
 ?>
