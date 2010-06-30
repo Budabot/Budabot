d3984b9107902d9c9c943a578f584891be0790cb
Arsenal <effendiarsenal@gmx.de>
2010-06-30 14:11:56 +0000

    Changed the logging mechanism to actually use the created log-directories ($vars must be declared global)

--- main.php
+++ main.php
@@ -164,6 +164,7 @@ main(true, $chatBot);
 ** Name: log
 ** Record incoming info into the chatbot's log.
 */	function newLine($channel, $sender, $message, $target){
+		global $vars;
 		if($channel == "")
 			return;
 			
