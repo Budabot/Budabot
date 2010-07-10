a6e32f2361a14bd62c357a4262cee976fe771eda
Tyrence <bigwheels16@gmail.com>
2010-07-10 06:35:32 +0000

    disabled NEUTNET_MODULE from spamming to guest channel

--- modules/NEUTNET_MODULE/neutnet.php
+++ modules/NEUTNET_MODULE/neutnet.php
@@ -1,7 +1,7 @@
 <?php
 
 if ($type == "msg" && preg_match("/^Neutnet[\\d]{1,2}$/", $sender)) {
-	bot::send($message, 'guild');
+	bot::send($message, 'guild', true);
 	
 	// keeps the bot from sending a message back to the neutnet satellite bot
 	$sender = NULL;
