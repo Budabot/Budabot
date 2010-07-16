587a2493ee2a40452f1f27b96a90c6c42926a116
Tyrence <bigwheels16@gmail.com>
2010-07-16 16:26:45 +0000

    added help file that tells how to register bots with neutnet

--- modules/NEUTNET_MODULE/NEUTNET_MODULE.php
+++ modules/NEUTNET_MODULE/NEUTNET_MODULE.php
@@ -4,4 +4,6 @@
 	$PLUGIN_VERSION = 0.1;
 
 	bot::event("msg", "$MODULE_NAME/neutnet.php", 'none', 'Relays neutnet shopping messages to a channel/player');
+	
+	bot::help("neutnet", "$MODULE_NAME/neutnet.txt", "all", "Shows the commands needed to register a bot with Neutnet", "Neutnet");
 ?>
