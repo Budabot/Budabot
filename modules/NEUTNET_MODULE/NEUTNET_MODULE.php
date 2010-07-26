1b4d4b5833e1b6fe5ff6b0eed6eafbf7e793f15d
Tyrence <bigwheels16@gmail.com>
2010-07-26 19:29:42 +0000

    updated NEUTNET_MODULE to automatically add neutnet satellite bots to the whitelist; neutnet module will only be loaded if bot is running on rk2

--- modules/NEUTNET_MODULE/NEUTNET_MODULE.php
+++ modules/NEUTNET_MODULE/NEUTNET_MODULE.php
@@ -1,9 +1,15 @@
 <?php
-
-	$MODULE_NAME = "NEUTNET_MODULE";
-	$PLUGIN_VERSION = 0.1;
-
-	bot::event("msg", "$MODULE_NAME/neutnet.php", 'none', 'Relays neutnet shopping messages to a channel/player');
-	
-	bot::help("neutnet", "$MODULE_NAME/neutnet.txt", "all", "Shows the commands needed to register a bot with Neutnet", "Neutnet");
+	if ($this->vars['dimension'] == 2) {
+		$MODULE_NAME = "NEUTNET_MODULE";
+		
+		// add neutnet bots to whitelist
+		$NUM_BOTS = 14;
+		for ($i = 0; $i <= $NUM_BOTS; $i++) {
+			Whitelist::add($MODULE_NAME, "Neutnet$i");
+		}
+		
+		bot::event("msg", "$MODULE_NAME/neutnet.php", 'none', 'Relays neutnet shopping messages to a channel/player');
+		
+		bot::help("neutnet", "$MODULE_NAME/neutnet.txt", "all", "Shows the commands needed to register a bot with Neutnet", "Neutnet");
+	}
 ?>
