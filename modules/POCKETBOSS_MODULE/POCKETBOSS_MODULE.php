02f81255eaf5a604114890c5a3dabed11dddf1c1
Tyrence <bigwheels16@gmail.com>
2010-07-30 01:03:01 +0000

    updated pocketboss module db so sort by ql would work; set default access level to ALL

--- modules/POCKETBOSS_MODULE/POCKETBOSS_MODULE.php
+++ modules/POCKETBOSS_MODULE/POCKETBOSS_MODULE.php
@@ -5,11 +5,11 @@
 	bot::loadSQLFile($MODULE_NAME, "pocketboss");
 
 	//Pocketboss module
-	bot::command("", "$MODULE_NAME/pocketboss.php", "pb", "guild", "Shows what symbs a PB drops");
-	bot::command("", "$MODULE_NAME/pocketboss.php", "symb", "guild", "Shows what PB drops a symb");
+	bot::command("", "$MODULE_NAME/pocketboss.php", "pb", "all", "Shows what symbs a PB drops");
+	bot::command("", "$MODULE_NAME/pocketboss.php", "symb", "all", "Shows what PB drops a symb");
 
 	bot::regGroup("PB_SYMB", $MODULE_NAME, "PocketBoss List and Symb search", "symb", "pb");
 
 	//Helpiles
-    bot::help("pocketboss", "$MODULE_NAME/pocketboss.txt", "guild", "See what drops which Pocketboss", "Helpbot");
+    bot::help("pocketboss", "$MODULE_NAME/pocketboss.txt", "all", "See what drops which Pocketboss", "Helpbot");
 ?>
