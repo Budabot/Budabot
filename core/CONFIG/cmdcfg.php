b3ee3e2020ba73dd9672bf5248ca8d5d775a1422
Tyrence <bigwheels16@gmail.com>
2010-08-12 00:06:03 +0000

    added Logger.class.php to replace newLine in main.php

--- core/CONFIG/cmdcfg.php
+++ core/CONFIG/cmdcfg.php
@@ -103,7 +103,7 @@ if (preg_match("/^config$/i", $message)) {
 	$sql = "UPDATE cmdcfg_<myname> SET `status` = $status WHERE (`cmdevent` = 'cmd' OR `cmdevent` = 'subcmd') AND ($typeSql)";
 	$db->exec($sql);
 	
-	bot::send("Commands(s) updated successfully.", $sendto);	
+	bot::send("Command(s) updated successfully.", $sendto);	
 } else if (preg_match("/^config (mod|cmd|grp|event) (.+) (enable|disable) (priv|msg|guild|all)$/i", $message, $arr)) {
 	if($arr[1] == "event") {
 		$temp = explode(" ", $arr[2]);
