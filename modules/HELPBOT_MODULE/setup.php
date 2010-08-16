d04d51202067830f6b62afed0803701e39523bad
Tyrence <bigwheels16@gmail.com>
2010-08-16 03:25:36 +0000

    removed code that removed flip/roll verifications when there was 200 or more of them

--- modules/HELPBOT_MODULE/setup.php
+++ modules/HELPBOT_MODULE/setup.php
@@ -1,8 +1,4 @@
 <?php
 $db->query("CREATE TABLE IF NOT EXISTS roll_<myname> (`id` INTEGER PRIMARY KEY AUTO_INCREMENT, `time` INT, `name` VARCHAR(25), `type` INT, `start` INT, `end` INT, `result` INT)");
-$db->query("SELECT * FROM roll_<myname>");
-if($db->numrows() >= 200)
-	$db->query("DELETE FROM roll_<myname>");
-	
 $db->query("CREATE TABLE IF NOT EXISTS koslist_<myname> (`time` INT PRIMARY KEY NOT NULL, `name` VARCHAR(25), `sender` VARCHAR(25), `reason` VARCHAR(50) DEFAULT '0')");
 ?>
