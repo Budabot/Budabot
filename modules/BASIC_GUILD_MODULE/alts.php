9df8588e290c7b7eeab06d6e3d9d895869ce6e57
Arsenal <effendiarsenal@gmx.de>
2010-08-18 16:17:38 +0000

    added support for multiple alts added with one command

--- modules/BASIC_GUILD_MODULE/alts.php
+++ modules/BASIC_GUILD_MODULE/alts.php
@@ -1,107 +1,156 @@
 <?php
-   /*
-   ** Author: Derroylo (RK2)
-   ** Description: Alt Char Handling
-   ** Version: 1.0
-   **
-   ** Developed for: Budabot(http://sourceforge.net/projects/budabot)
-   **
-   ** Date(created): 23.11.2005
-   ** Date(last modified): 21.11.2006
-   ** 
-   ** Copyright (C) 2005, 2006 Carsten Lohmann
-   **
-   ** Licence Infos: 
-   ** This file is part of Budabot.
-   **
-   ** Budabot is free software; you can redistribute it and/or modify
-   ** it under the terms of the GNU General Public License as published by
-   ** the Free Software Foundation; either version 2 of the License, or
-   ** (at your option) any later version.
-   **
-   ** Budabot is distributed in the hope that it will be useful,
-   ** but WITHOUT ANY WARRANTY; without even the implied warranty of
-   ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   ** GNU General Public License for more details.
-   **
-   ** You should have received a copy of the GNU General Public License
-   ** along with Budabot; if not, write to the Free Software
-   ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-   */
+/*
+ ** Author: Derroylo (RK2)
+ ** Description: Alt Char Handling
+ ** Version: 1.0
+ **
+ ** Developed for: Budabot(http://sourceforge.net/projects/budabot)
+ **
+ ** Date(created): 23.11.2005
+ ** Date(last modified): 21.11.2006
+ **
+ ** Copyright (C) 2005, 2006 Carsten Lohmann
+ **
+ ** Licence Infos:
+ ** This file is part of Budabot.
+ **
+ ** Budabot is free software; you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation; either version 2 of the License, or
+ ** (at your option) any later version.
+ **
+ ** Budabot is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with Budabot; if not, write to the Free Software
+ ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
-if(preg_match("/^alts add (.+)$/i", $message, $arr)) {
-    $name = ucfirst(strtolower($arr[1]));
-    $uid = AoChat::get_uid($arr[1]);
-    if(!$uid)
-        $msg = "Player <highlight>$name<end> does not exist.";
-    else {
-        $db->query("SELECT * FROM alts WHERE `alt` = '$name'");
-        $row = $db->fObject();
-        if($row->alt == $name)
-            $msg = "Player <highlight>$name<end> is already registered as alt from <highlight>$row->main<end>.";
-        else {
-            $db->query("SELECT * FROM alts WHERE `main` = '$name'");
-            if($db->numrows() != 0)
-                $msg = "Player <highlight>$name<end> is already registered as main from someone.";
-            else {
-				//check added to make sure the $sender himself isn't already an alt
-				$db->query("SELECT * FROM alts WHERE `alt` = '$sender'");
-				$row = $db->fObject();
-				if($row->alt == $sender) {
-					$db->query("INSERT INTO alts (`alt`, `main`) VALUES ('$name', '$row->main')");
-					$msg = "<highlight>You<end> are already an alt of <highlight>$row->main<end>. <highlight>$name<end> has been registered as an alt of <highlight>$row->main<end>.";
-				}
-				else {
-					$db->query("INSERT INTO alts (`alt`, `main`) VALUES ('$name', '$sender')");
-					$msg = "<highlight>$name<end> has been registered as your alt.";
-				}
-            }
-        }
-    }
-} else if(preg_match("/^alts (rem|del|remove|delete) (.+)$/i", $message, $arr)) {
-    $name = ucfirst(strtolower($arr[2]));
-    $uid = AoChat::get_uid($arr[2]);
-    if(!$uid)
-        $msg = "Player <highlight>".$name."<end> does not exist.";
-    else {
-        $db->query("SELECT * FROM alts WHERE `main` = '$sender' AND `alt` = '$name'");
-        $row = $db->fObject();
-        if($row->main == $sender) {
-            $db->query("DELETE FROM alts WHERE `main` = '$sender' AND `alt` = '$name'");
-            $msg = "<highlight>$name<end> has been deleted from your alt list.";
+if (preg_match("/^alts add (.+)$/i", $message, $arr))
+{
+	/* get all names in an array */
+	$names = explode(' ', $arr[1]);
+	/* initialize some arrays for different outcomes */
+	$names_not_existing = array();
+	$names_already_registered = array();
+	$names_succeeded = array();
+	/* Pop a name from the array until none are left (checking for null) */
+	while (null != ($name = array_pop($names)))
+	{
+		$name = ucfirst(strtolower($arr[1]));
+		$uid = AoChat::get_uid($arr[1]);
+		/* check if player exists */
+		if (!$uid)
+		{
+			$names_not_existing[] = $name;
+			continue;
 		}
-        else {
+		/* check if player is already an alt */
+		$db->query("SELECT * FROM alts WHERE `alt` = '$name'");
+		if ($db->numrows() != 0)
+		{
+			$names_already_registered[] = $name.' (as alt)';
+			continue;
+		}
+		/* check if player is already a main */
+		$db->query("SELECT * FROM alts WHERE `main` = '$name'");
+		if ($db->numrows() != 0)
+		{
+			$names_already_registered = $name.' (as main)';
+			continue;
+		}
+
+		/* check to make sure the $sender himself isn't already an alt */
+		/* insert into database */
+		$db->query("SELECT * FROM alts WHERE `alt` = '$sender'");
+		if ($db->numrows() != 0)
+		{
+			$db->query("INSERT INTO alts (`alt`, `main`) VALUES ('$name', '$row->main')");
+		}
+		else
+		{
+			$db->query("INSERT INTO alts (`alt`, `main`) VALUES ('$name', '$sender')");
+		}
+	}
+	$window = 'Alts added:<br>';
+	foreach ($names_succeeded as $alt)
+	{
+		$window .= $alt.' ';
+	}
+	$window .= '<br><br>Alts already registered:<br>';
+	foreach ($names_already_registered as $alt)
+	{
+		$window .= $alt.' ';
+	}
+	$window .= '<br><br>Alts not existing :<br>';
+	foreach ($names_not_existing as $alt)
+	{
+		$window .= $alt.' ';
+	}
+	$link = 'Added '.count($names_succeeded).' alts to your list.';
+	$failed_count = count($names_already_registered) + count($names_not_existing);
+	if ($failed_count > 0)
+	{
+		$link .= ' Failed adding $failed_count alts to your list.';
+	}
+	$msg = $this->makeLink($link, $window);
+}
+
+elseif (preg_match("/^alts (rem|del|remove|delete) (.+)$/i", $message, $arr))
+{
+	$name = ucfirst(strtolower($arr[2]));
+	$uid = AoChat::get_uid($arr[2]);
+	if (!$uid)
+		$msg = "Player <highlight>".$name."<end> does not exist.";
+	else
+	{
+		$db->query("SELECT * FROM alts WHERE `main` = '$sender' AND `alt` = '$name'");
+		$row = $db->fObject();
+		if ($row->main == $sender)
+		{
+			$db->query("DELETE FROM alts WHERE `main` = '$sender' AND `alt` = '$name'");
+			$msg = "<highlight>$name<end> has been deleted from your alt list.";
+		}
+		else
+		{
 			//sender was not found as a main.  checking if he himself is an alt and let him be able to modify his own alts list
 			$db->query("SELECT * FROM alts WHERE `alt` = '$sender'");
 			$row = $db->fObject();
 			//retrieve his main, use the main's name to do searches and modifications with
 			$main = $row->main;
 			$db->query("SELECT * FROM alts WHERE main = '$main' AND alt = '$name'");
-			if($db->numrows() != 0) {
+			if ($db->numrows() != 0)
+			{
 				$db->query("DELETE FROM alts WHERE main = '$main' AND alt = '$name'");
 				$msg = "<highlight>$name<end> has been deleted from your ($main) alt list.";
 			}
-			else {
+			else
+			{
 				$msg = "<highlight>$name<end> is not registered as your alt.";
 			}
-        }
-    }
+		}
+	}
 }
-elseif(preg_match("/^alts main (.+)$/i", $message, $arr))
+
+elseif (preg_match("/^alts main (.+)$/i", $message, $arr))
 {
 	$name_alt = $sender;
 	$name_main = ucfirst(strtolower($arr[1]));
 	$uid = $this->get_uid($name_main);
 	if (!$uid)
 		$msg .= " Player <highlight>$name_main<end> does not exist.";
-	if ($uid) {
+	if ($uid)
+	{
 		$db->query("DELETE FROM alts WHERE `alt` = '$name_alt'");
 		$db->query("SELECT * FROM alts WHERE `main` = '$name_alt'");
-		if($db->numrows() != 0)
+		if ($db->numrows() != 0)
 		{
 			$msg = "You are already registered as main from someone.";
 		}
-		else 
+		else
 		{
 			$db->query("SELECT * FROM alts WHERE `alt` = '$name_main'");
 			if ($db->numrows() != 0)
@@ -112,126 +161,169 @@ elseif(preg_match("/^alts main (.+)$/i", $message, $arr))
 			$db->query("INSERT INTO alts (`alt`, `main`) VALUES ('$name_alt', '$name_main')");
 			$msg = "You have been registered as an alt of $name_main.";
 		}
-		
+
 	}
 }
- else if(preg_match("/^alts (.+)$/i", $message, $arr)) {
-    $name = ucfirst(strtolower($arr[1]));
-    $uid = AoChat::get_uid($arr[1]);
-    if(!$uid)
-        $msg = "Player <highlight>".$name."<end> does not exist.";
-    else {
-        $main = false;
-        // Check if sender is himelf the main
-        $db->query("SELECT * FROM alts WHERE `main` = '$name'");
-        if($db->numrows() == 0){
-            // Check if sender is an alt
-            $db->query("SELECT * FROM alts WHERE `alt` = '$name'");
-            if($db->numrows() == 0)
-                $msg = "No alts are registered for <highlight>$name<end>.";
-            else {
-                $row = $db->fObject();
-                $main = $row->main;
-            }
-        } else
-            $main = $name;
 
-        // If a main was found create the list
-        if($main) {
-            $list = "<header>::::: Alternative Character List :::::<end> \n \n";
-            $list .= ":::::: Main Character\n";
-            $list .= "<tab><tab>".bot::makeLink($main, "/tell ".$this->vars["name"]." whois $main", "chatcmd")." - ";
-			$online = $this->buddy_online($main);
-            if ($online === null) {
-                $list .= "No status.\n";
-            } else if ($online == 1) {
-                $list .= "<green>Online<end>\n";
-            } else {
-                $list .= "<red>Offline<end>\n";
+elseif (preg_match("/^alts (.+)$/i", $message, $arr))
+{
+	$name = ucfirst(strtolower($arr[1]));
+	$uid = AoChat::get_uid($arr[1]);
+	if (!$uid)
+		$msg = "Player <highlight>".$name."<end> does not exist.";
+	else
+	{
+		$main = false;
+		// Check if sender is himelf the main
+		$db->query("SELECT * FROM alts WHERE `main` = '$name'");
+		if ($db->numrows() == 0)
+		{
+			// Check if sender is an alt
+			$db->query("SELECT * FROM alts WHERE `alt` = '$name'");
+			if ($db->numrows() == 0)
+				$msg = "No alts are registered for <highlight>$name<end>.";
+			else
+			{
+				$row = $db->fObject();
+				$main = $row->main;
 			}
-            $list .= ":::::: Alt Character(s)\n";
-            $db->query("SELECT * FROM alts WHERE `main` = '$main'");
-            while($row = $db->fObject()) {
-                $list .= "<tab><tab>".bot::makeLink($row->alt, "/tell ".$this->vars["name"]." whois $row->alt", "chatcmd")." - ";
-				$online = $this->buddy_online($row->alt);
-                if ($online === null) {
-                    $list .= "No status.\n";
-                } else if ($online == 1) {
-                    $list .= "<green>Online<end>\n";
-                } else {
-                    $list .= "<red>Offline<end>\n";
-				}
-            }
-            $msg = bot::makeLink($main."`s Alts", $list);
-        }
-    }
-} elseif(preg_match("/^alts$/i", $message)) {
-    $main = false;
-    // Check if $sender is himself the main
-    $db->query("SELECT * FROM alts WHERE `main` = '$sender'");
-    if($db->numrows() == 0){
-        // Check if $sender is an alt
-        $db->query("SELECT * FROM alts WHERE `alt` = '$sender'");
-        if($db->numrows() == 0)
-            $msg = "No alts are registered for <highlight>$sender<end>.";
-        else {
-            $row = $db->fObject();
-            $main = $row->main;
-        }
-    } else
-        $main = $sender;
-
-
-    // If a main was found create the list
-    if($main) {
-        $list = "<header>::::: Alternative Character List :::::<end> \n \n";
-        $list .= ":::::: Main Character\n";
-        $list .= "<tab><tab>".bot::makeLink($main, "/tell ".$this->vars["name"]." whois $main", "chatcmd")." - ";
-		$online = $this->buddy_online($main);
-        if ($online === null) {
-            $list .= "No status.\n";
-        } else if ($online == 1) {
-            $list .= "<green>Online<end>\n";
-        } else {
-            $list .= "<red>Offline<end>\n";
 		}
-            
-        $list .= ":::::: Alt Character(s)\n";
-        $db->query("SELECT * FROM alts WHERE `main` = '$main'");
-        while($row = $db->fObject()) {
-            $list .= "<tab><tab>".bot::makeLink($row->alt, "/tell ".$this->vars["name"]." whois $row->alt", "chatcmd")." - ";
-			$online = $this->buddy_online($row->alt);
-            if ($online === null) {
-                $list .= "No status.\n";
-            } else if ($online == 1) {
-                $list .= "<green>Online<end>\n";
-            } else {
-                $list .= "<red>Offline<end>\n";
+		else
+			$main = $name;
+
+		// If a main was found create the list
+		if ($main)
+		{
+			$list = "<header>::::: Alternative Character List :::::<end> \n \n";
+			$list .= ":::::: Main Character\n";
+			$list .= "<tab><tab>".bot::makeLink($main, "/tell ".$this->vars["name"]." whois $main", "chatcmd")." - ";
+			$online = $this->buddy_online($main);
+			if ($online === null)
+			{
+				$list .= "No status.\n";
 			}
-        }
-        $msg = bot::makeLink($sender."`s Alts", $list);
-    }
-} elseif (preg_match("/^altsadmin (.+)$/i", $message, $arr)) {
-	if (preg_match("/^add (.+) (.+)$/i", $arr[1], $names)) {
-		if ($names[1] != '' && $names[2] != '') {
+			elseif ($online == 1)
+			{
+				$list .= "<green>Online<end>\n";
+			}
+			else
+			{
+				$list .= "<red>Offline<end>\n";
+			}
+			$list .= ":::::: Alt Character(s)\n";
+			$db->query("SELECT * FROM alts WHERE `main` = '$main'");
+			while ($row = $db->fObject())
+			{
+				$list .= "<tab><tab>".bot::makeLink($row->alt, "/tell ".$this->vars["name"]." whois $row->alt", "chatcmd")." - ";
+				$online = $this->buddy_online($row->alt);
+				if ($online === null)
+				{
+					$list .= "No status.\n";
+				}
+				else if ($online == 1)
+				{
+					$list .= "<green>Online<end>\n";
+				}
+				else
+				{
+					$list .= "<red>Offline<end>\n";
+				}
+			}
+			$msg = bot::makeLink($main."`s Alts", $list);
+		}
+	}
+}
+
+elseif (preg_match("/^alts$/i", $message))
+{
+	$main = false;
+	// Check if $sender is himself the main
+	$db->query("SELECT * FROM alts WHERE `main` = '$sender'");
+	if ($db->numrows() == 0)
+	{
+		// Check if $sender is an alt
+		$db->query("SELECT * FROM alts WHERE `alt` = '$sender'");
+		if ($db->numrows() == 0)
+			$msg = "No alts are registered for <highlight>$sender<end>.";
+		else
+		{
+			$row = $db->fObject();
+			$main = $row->main;
+		}
+	}
+	else
+		$main = $sender;
+
+	// If a main was found create the list
+	if ($main)
+	{
+		$list = "<header>::::: Alternative Character List :::::<end> \n \n";
+		$list .= ":::::: Main Character\n";
+		$list .= "<tab><tab>".bot::makeLink($main, "/tell ".$this->vars["name"]." whois $main", "chatcmd")." - ";
+		$online = $this->buddy_online($main);
+		if ($online === null)
+		{
+			$list .= "No status.\n";
+		}
+		else if ($online == 1)
+		{
+			$list .= "<green>Online<end>\n";
+		}
+		else
+		{
+			$list .= "<red>Offline<end>\n";
+		}
+
+		$list .= ":::::: Alt Character(s)\n";
+		$db->query("SELECT * FROM alts WHERE `main` = '$main'");
+		while ($row = $db->fObject())
+		{
+			$list .= "<tab><tab>".bot::makeLink($row->alt, "/tell ".$this->vars["name"]." whois $row->alt", "chatcmd")." - ";
+			$online = $this->buddy_online($row->alt);
+			if ($online === null)
+			{
+				$list .= "No status.\n";
+			}
+			else if ($online == 1)
+			{
+				$list .= "<green>Online<end>\n";
+			}
+			else
+			{
+				$list .= "<red>Offline<end>\n";
+			}
+		}
+		$msg = bot::makeLink($sender."`s Alts", $list);
+	}
+}
+
+elseif (preg_match("/^altsadmin (.+)$/i", $message, $arr))
+{
+	if (preg_match("/^add (.+) (.+)$/i", $arr[1], $names))
+	{
+		if ($names[1] != '' && $names[2] != '')
+		{
 			$name_alt = ucfirst(strtolower($names[1]));
 			$name_main = ucfirst(strtolower($names[2]));
 			$uid1 = AoChat::get_uid($names[1]);
 			$uid2 = AoChat::get_uid($names[2]);
-			if (!$uid1) 
+			if (!$uid1)
 				$msg = "Player <highlight>$name_alt<end> does not exist.";
 			if (!$uid2)
-					$msg .= " Player <highlight>$name_main<end> does not exist.";
-			if ($uid1 && $uid2) {
+				$msg .= " Player <highlight>$name_main<end> does not exist.";
+			if ($uid1 && $uid2)
+			{
 				$db->query("SELECT * FROM alts WHERE `alt` = '$name_alt'");
 				$row = $db->fObject();
-				if($row->alt == $name_alt)
+				if ($row->alt == $name_alt)
 					$msg = "Player <highlight>$name_alt<end> is already registered as alt from <highlight>$row->main<end>.";
-				else {
+				else
+				{
 					$db->query("SELECT * FROM alts WHERE `main` = '$name_alt'");
-					if($db->numrows() != 0)
+					if ($db->numrows() != 0)
 						$msg = "Player <highlight>$name_alt<end> is already registered as main from someone.";
-					else {
+					else
+					{
 						$db->query("INSERT INTO alts (`alt`, `main`) VALUES ('$name_alt', '$name_main')");
 						$msg = "<highlight>$name_alt<end> has been registered as an alt of $name_main.";
 					}
@@ -239,40 +331,42 @@ elseif(preg_match("/^alts main (.+)$/i", $message, $arr))
 			}
 		}
 	}
-	elseif (preg_match("/^rem (.+) (.+)$/i", $arr[1], $names)) {
-		if ($names[1] != '' && $names[2] != '') {
+	elseif (preg_match("/^rem (.+) (.+)$/i", $arr[1], $names))
+	{
+		if ($names[1] != '' && $names[2] != '')
+		{
 			$name_alt = ucfirst(strtolower($names[1]));
 			$name_main = ucfirst(strtolower($names[2]));
 			$uid1 = AoChat::get_uid($names[1]);
 			$uid2 = AoChat::get_uid($names[2]);
-			if (!$uid1) 
+			if (!$uid1)
 				$msg = "Player <highlight>$name_alt<end> does not exist.";
 			if (!$uid2)
-					$msg .= " Player <highlight>$name_main<end> does not exist.";
-			if ($uid1 && $uid2) {
+				$msg .= " Player <highlight>$name_main<end> does not exist.";
+			if ($uid1 && $uid2)
+			{
 				$db->query("SELECT * FROM alts WHERE alt = '$name_alt' AND main = '$name_main'");
-				if($db->numrows() != 0) {
+				if ($db->numrows() != 0)
+				{
 					$db->query("DELETE FROM alts WHERE main = '$name_main' AND alt = '$name_alt'");
 					$msg = "<highlight>$name_alt<end> has been deleted from the alt list of <highlight>$name_main.<end>";
 				}
-				else 
+				else
 					$msg = "Player <highlight>$name_alt<end> not listed as an alt of Player <highlight>$name_main<end>.  Please check the player's !alts listings.";
 			}
 		}
 	}
-	elseif ($names[1] == '' || $names[2] == '') {
+	elseif ($names[1] == '' || $names[2] == '')
+	{
 		$msg = "Some information is missing. Please check <highlight>/tell <myname> <symbol>help altsadmin<end> for the correct syntax.";
-		}
-	else {
+	}
+	else
+	{
 		$msg = "<highlight>/tell <myname> <symbol>help altsadmin<end> for the correct syntax.";
-		}
+	}
 }
 
 // Send info back
-if($type == "msg")
-    bot::send($msg, $sender);
-elseif($type == "priv")
-   	bot::send($msg);
-elseif($type == "guild")
-   	bot::send($msg, "guild");
+bot::send($msg, $sendto);
+
 ?>
