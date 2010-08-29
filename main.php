2fd475e4ac22c498a818ee4c6847ab5b744edd45
Tyrence <bigwheels16@gmail.com>
2010-08-29 08:02:31 +0000

    updated version numbers to reflect new naming scheme

--- main.php
+++ main.php
@@ -29,7 +29,7 @@
    ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
    */  
 
-$version = "0.6.6";
+$version = "1.0";
 
 echo "\n\n\n\n\n";
 echo "		**************************************************\n";
