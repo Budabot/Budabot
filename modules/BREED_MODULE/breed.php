56f15e0b127b518a0a7d1c264f9256dbbc5c739c
Tyrence <bigwheels16@gmail.com>
2010-09-03 16:44:36 +0000

    updated formatting for !breed

--- modules/BREED_MODULE/breed.php
+++ modules/BREED_MODULE/breed.php
@@ -1,7 +1,7 @@
 <?php
 $blob = 
-"<header>Solitus<end><orange>
-Ability    Rubi-ka  / Shadowlands<end>
+"<header>Solitus<end>
+<orange>Ability    Rubi-ka  / Shadowlands<end>
  
 Strength        472 / 772  
 Agility           480 / 780  
@@ -10,11 +10,11 @@ Intelligence    480 / 780
 Sense           480 / 780  
 Psychic         480 / 780
 Max % Add Nano Cost   -50%
-3 max health per 1 point of hp
-3 max nano per 1 point of np
+3 max health per 1 point of bodydev
+3 max nano per 1 point of nanopool
 
-<header>Nanomage<end><orange>
-Ability    Rubi-ka  / Shadowlands<end>
+<header>Nanomage<end>
+<orange>Ability    Rubi-ka  / Shadowlands<end>
 
 Strength        464 / 664  
 Agility           464 / 664  
@@ -23,11 +23,11 @@ Intelligence    512 / 912
 Sense           480 / 780  
 Psychic         512 / 912
 Max % Add Nano Cost   -55%
-2 max health per 1 point of hp
-4 max nano per 1 point of np
+2 max health per 1 point of bodydev
+4 max nano per 1 point of nanopool
 
-<header>Opifex<end><orange>
-Ability    Rubi-ka  / Shadowlands<end>
+<header>Opifex<end>
+<orange>Ability    Rubi-ka  / Shadowlands<end>
 
 Strength       464 / 764  
 Agility          544 / 944  
@@ -36,11 +36,11 @@ Intelligence   464 / 764
 Sense          512 / 912  
 Psychic        448 / 748
 Max % Add Nano Cost   -50%
-3 max health per 1 point of hp
-3 max nano per 1 point of np
+3 max health per 1 point of bodydev
+3 max nano per 1 point of nanopool
 
-<header><b>Atrox<end><orange>
-Ability    Rubi-ka  / Shadowlands<end>
+<header>Atrox<end>
+<orange>Ability    Rubi-ka  / Shadowlands<end>
  
 Strength      512 / 912  
 Agility          480 / 780  
@@ -49,8 +49,8 @@ Intelligence  400 / 600
 Sense         400 / 600  
 Psychic       400 / 600
 Max % Add Nano Cost   -45%
-4 max health per 1 point of hp
-2 max nano per 1 point of np";
+4 max health per 1 point of bodydev
+2 max nano per 1 point of nanopool";
 
 if (preg_match("/^breed/i", $message, $arr)) {
 	$msg = bot::makeLink("Results of Breedcap Inquiry.", $blob);
