79703e59736927508c5fd1bf378fe028247d07a4
Tyrence <bigwheels16@gmail.com>
2010-09-04 01:56:54 +0000

    added aliases for !breed, !hd, !lag, !nd, and !stats

--- modules/INFO_MODULE/INFO_MODULE.php
+++ modules/INFO_MODULE/INFO_MODULE.php
@@ -1,6 +1,15 @@
 <?php
+	require_once 'info_functions.php';
+
 	$MODULE_NAME = "INFO_MODULE";
 
 	bot::command("", "$MODULE_NAME/info.php", "info", "all", "Shows basic info");
+	
+	// aliases
+	bot::command("", "$MODULE_NAME/info.php", "breed", "all", "Alias for !info breed");
+	bot::command("", "$MODULE_NAME/info.php", "hd", "all", "Alias for !info hd");
+	bot::command("", "$MODULE_NAME/info.php", "lag", "all", "Alias for !info lag");
+	bot::command("", "$MODULE_NAME/info.php", "nd", "all", "Alias for !info nd");
+	bot::command("", "$MODULE_NAME/info.php", "stats", "all", "Alias for !info stats");
 
 ?>
--- modules/INFO_MODULE/info.php
+++ modules/INFO_MODULE/info.php
@@ -12,74 +12,70 @@
    ** Copyright (C) 2007 Jason Wheeler
    */
 
-require_once('info_functions.php');
-if (preg_match("/^info(.*)$/i", $message))
+$path = getcwd() . "/modules/INFO_MODULE/info/";
+$fileExt = ".txt";
+$msg = "";
+
+if (preg_match("/^info$/i", $message))
 {
-	$path = getcwd() . "/modules/INFO_MODULE/info/";
-	$fileExt = ".txt";
-	$msg = "";
-	
-	// if they want the list of topics
-	if (preg_match("/^info$/i", $message))
+	if ($handle = opendir($path))
 	{
-		if ($handle = opendir($path))
-		{
-			$topicList = array();
+		$topicList = array();
 
-		    /* This is the correct way to loop over the directory. */
-		    while (false !== ($fileName = readdir($handle)))
-		    {
-			    // if file has the correct extension, it's a topic file
-		        if (strpos($fileName, $fileExt))
-		        {
-        			$topicList[] =  str_replace($fileExt, '', $fileName);
-		        }
-		    }
-
-		    closedir($handle);
-		    
-		    global $vars;
-		    global $settings;
-		    $linkContents = '';
-		    forEach ($topicList as $topic)
-		    {
-				$linkContents .= bot::makeLink($topic, "/tell " . $vars['name'] . " " . $settings['symbol'] . "info $topic", 'chatcmd') . "\n";  
-				//$linkContents .= bot::makeLink($topic, getTopicContents($path, $topic, $fileExt), "blob") . "\n";  
-		    }
-		    
-		    if ($linkContents)
-		    {
-				$msg = bot::makeLink('Topics (' . count($topicList) . ')', count($topicList) . " Topics Available\n==========\n\n$linkContents", "blob");
-		    }
-		    else
-		    {
-			 	$msg = "No topics available.";   
-		    }
-		}
-		else
+		/* This is the correct way to loop over the directory. */
+		while (false !== ($fileName = readdir($handle)))
 		{
-			$msg = "Error reading topics.";	
+			// if file has the correct extension, it's a topic file
+			if (strpos($fileName, $fileExt))
+			{
+				$topicList[] =  str_replace($fileExt, '', $fileName);
+			}
 		}
-	}
-	// if they want a certain topic
-	else if (preg_match("/^info (.*)$/i", $message, $arr))
-	{
-		// get the filename and read in the file
-		$fileName = $arr[1];
-		$info = getTopicContents($path, $fileName, $fileExt);
+
+		closedir($handle);
 		
-		// make sure the $ql is an integer between 1 and 300
-		if (!$info)
+		global $vars;
+		global $settings;
+		$linkContents = '';
+		forEach ($topicList as $topic)
 		{
-			$msg = "No info for $fileName could be found";
+			$linkContents .= bot::makeLink($topic, "/tell " . $vars['name'] . " " . $settings['symbol'] . "info $topic", 'chatcmd') . "\n";  
+			//$linkContents .= bot::makeLink($topic, getTopicContents($path, $topic, $fileExt), "blob") . "\n";  
+		}
+		
+		if ($linkContents)
+		{
+			$msg = bot::makeLink('Topics (' . count($topicList) . ')', count($topicList) . " Topics Available\n==========\n\n$linkContents", "blob");
 		}
 		else
-		{	
-			$msg = bot::makeLink($fileName, $info);
+		{
+			$msg = "No topics available.";   
 		}
 	}
-	
-	bot::send($msg, $sendto);
+	else
+	{
+		$msg = "Error reading topics.";	
+	}
 }
+// if they want a certain topic
+// 2nd form matches the aliases
+else if (preg_match("/^info ([a-z0-9_-]+)$/i", $message, $arr) || preg_match("/^([a-z0-9_-]+)$/i", $message, $arr))
+{
+	// get the filename and read in the file
+	$fileName = $arr[1];
+	$info = getTopicContents($path, $fileName, $fileExt);
+	
+	// make sure the $ql is an integer between 1 and 300
+	if (!$info)
+	{
+		$msg = "No info for $fileName could be found";
+	}
+	else
+	{	
+		$msg = bot::makeLink($fileName, $info);
+	}
+}
+
+bot::send($msg, $sendto);
 
 ?>
