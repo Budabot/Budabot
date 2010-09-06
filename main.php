5a01b85d34e3eff305bbf6da0f599dfd72057b06
Tyrence <bigwheels16@gmail.com>
2010-09-06 08:21:13 +0000

    updated version number

--- main.php
+++ main.php
@@ -29,7 +29,7 @@
    ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
    */  
 
-$version = "1.0";
+$version = "1.0_GA";
 
 echo "\n\n\n\n\n";
 echo "		**************************************************\n";
