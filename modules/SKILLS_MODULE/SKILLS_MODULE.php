073cb396b5f88216a5eb3655942078b11a0d4550
Tyrence <bigwheels16@gmail.com>
2010-09-08 20:44:57 +0000

    moved !inits into SKILLS_MODULE

--- modules/SKILLS_MODULE/SKILLS_MODULE.php
+++ modules/SKILLS_MODULE/SKILLS_MODULE.php
@@ -1,8 +1,9 @@
 <?php
+	require_once 'utils.php';
+
 	$MODULE_NAME = "SKILLS_MODULE";
 
 	//Skills module
-
 	bot::command("", "$MODULE_NAME/aggdef.php", "aggdef", "all", "Agg/Def: Calculates weapon inits for your Agg/Def bar.");
 	bot::command("", "$MODULE_NAME/as.php", "as", "all", "AS: Calculates Aimed Shot.");
 	bot::command("", "$MODULE_NAME/nanoinit.php", "nanoinit", "all", "Nanoinit: Calculates Nano Init.");
@@ -14,6 +15,9 @@
 	bot::command("", "$MODULE_NAME/brawl.php", "brawl", "all", "Brawl: Calculates brawl facts.");
 	bot::command("", "$MODULE_NAME/fast.php", "fast", "all", "Fast: Calculates Fast Attack recharge.");
 	bot::command("", "$MODULE_NAME/fast.php", "fastattack", "all", "alias for: fast");
+	
+	//Inits
+	bot::command("", "$MODULE_NAME/inits.php", "inits", "all", "shows how much inits you need for 1/1");
 
 	//Helpiles
 	bot::help("skills", "$MODULE_NAME/skills.txt", "all", "Explains the various Skill commands", "Skills");
---
+++ modules/SKILLS_MODULE/inits.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+	Inits Module
+	Author: William "Xyphos" Scott
+	Jan 19, 2010
+*/
+
+/// inits <a href="itemref://280727/280727/300">Sloth of the Xan</a>
+if (preg_match('/^inits \<a href\=\"itemref\:\/\/([0-9]+)\/([0-9]+)\/([0-9]+)\"\>/i', $message, $arr)) {
+	$url = "http://inits.xyphos.com/?";
+	$url .= "lowid={$arr[1]}&";
+	$url .= "highid={$arr[2]}&";
+	$url .= "ql={$arr[3]}&";
+	$url .= "output=aoml";
+
+	$msg = "Calculating Inits... Please wait.";
+	bot::send($msg, $sendto);
+	
+	$ctx - stream_context_create( array( 'http' => array( 'timeout' => 60 ) ) );
+
+	$msg = file_get_contents($url, 0, $ctx);
+	if (empty($msg)) {
+		$msg = "Unable to query Central Items Database.";
+	}
+} else {
+	$msg = "Syntax Error! Proper Syntax is <highlight>inits [drop weapon in chat]<end>";
+}
+
+bot::send($msg, $sendto);
+
+?>
