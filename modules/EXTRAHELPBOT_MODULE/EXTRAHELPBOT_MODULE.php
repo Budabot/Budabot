5e38b94d3758beed4bc498b12930a2be1c9aa77e
Tyrence <bigwheels16@gmail.com>
2010-09-13 21:19:16 +0000

    merged DYNA_MODULE into EXTRAHELPBOT_MODULE

--- modules/EXTRAHELPBOT_MODULE/EXTRAHELPBOT_MODULE.php
+++ modules/EXTRAHELPBOT_MODULE/EXTRAHELPBOT_MODULE.php
@@ -2,16 +2,20 @@
 	require_once 'db_utils.php';
 
 	$MODULE_NAME = "EXTRAHELPBOT_MODULE";
+	
+	bot::loadSQLFile($MODULE_NAME, "dyna");
  
 	bot::command("", "$MODULE_NAME/mobloot.php", "mobloot", "all", "loot QL Infos ");
 	bot::command("", "$MODULE_NAME/random.php", "random", "all", "Random order");
 	bot::command("", "$MODULE_NAME/cluster.php", "cluster", "all", "cluster location");
 	bot::command("", "$MODULE_NAME/buffitem.php", "buffitem", "all", "buffitem look up");
 	bot::command("", "$MODULE_NAME/whatbuffs.php", "whatbuffs", "all", "find items that buff");
+	bot::command("", "$MODULE_NAME/dyna.php", "dyna", "all", "Search for RK Dynaboss");
 	
 	bot::help("buffitem", "$MODULE_NAME/buffitem.txt", "all", "How to use buffitem");
 	bot::help("cluster", "$MODULE_NAME/cluster.txt", "all", "How to use cluster");
 	bot::help("mobloot", "$MODULE_NAME/mobloot.txt", "all", "How to use mobloot");
 	bot::help("whatbuffs", "$MODULE_NAME/whatbuffs.txt", "all", "How to use whatbuffs");
+	bot::help("dyna", "$MODULE_NAME/dyna.txt", "all", "Search for RK Dynaboss");
 	
 ?>
