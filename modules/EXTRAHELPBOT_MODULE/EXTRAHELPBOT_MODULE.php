d178a516b37f71678cb2c9d8469cf15f62399fb9
Tyrence <bigwheels16@gmail.com>
2010-09-13 22:01:47 +0000

    merged CAP_XP_MODULE into EXTRAHELPBOT_MODULE

--- modules/EXTRAHELPBOT_MODULE/EXTRAHELPBOT_MODULE.php
+++ modules/EXTRAHELPBOT_MODULE/EXTRAHELPBOT_MODULE.php
@@ -14,11 +14,16 @@
 	bot::command("", "$MODULE_NAME/dyna.php", "dyna", "all", "Search for RK Dynaboss");
 	bot::command("", "$MODULE_NAME/research.php", "research", "all", "Info on Research");
 	
+	//Max XP calculator
+	bot::command("", "$MODULE_NAME/cap_xp.php", "capsk", "all", "Max SK Calculator");
+	bot::command("", "$MODULE_NAME/cap_xp.php", "capxp", "all", "Max XP Calculator");
+	
 	bot::help("buffitem", "$MODULE_NAME/buffitem.txt", "all", "How to use buffitem");
 	bot::help("cluster", "$MODULE_NAME/cluster.txt", "all", "How to use cluster");
 	bot::help("mobloot", "$MODULE_NAME/mobloot.txt", "all", "How to use mobloot");
 	bot::help("whatbuffs", "$MODULE_NAME/whatbuffs.txt", "all", "How to use whatbuffs");
 	bot::help("dyna", "$MODULE_NAME/dyna.txt", "all", "Search for RK Dynaboss");
-	bot::help("research", "$MODULE_NAME/research.txt", "all", "Info on Research",);
+	bot::help("research", "$MODULE_NAME/research.txt", "all", "Info on Research");
+	bot::help("capxp", "$MODULE_NAME/capxp.txt", "all", "Set your reasearch bar for max xp/sk");
 	
 ?>
