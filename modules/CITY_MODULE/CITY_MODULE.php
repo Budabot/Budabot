a73f47b873978f4f5d19b49e12dfcdb44ac11ca8
Tyrence <bigwheels16@gmail.com>
2010-09-15 18:46:13 +0000

    renamed CITY_CLOAK_MODULE to CITY_MODULE

--- modules/CITY_MODULE/CITY_MODULE.php
+++ modules/CITY_MODULE/CITY_MODULE.php
@@ -29,7 +29,7 @@
    ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
    */
    
-	$MODULE_NAME = "CITY_CLOAK_MODULE";
+	$MODULE_NAME = "CITY_MODULE";
 	
 	bot::loadSQLFile($MODULE_NAME, 'org_city');
 
