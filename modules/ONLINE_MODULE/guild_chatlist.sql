e112cf3370e54d6de11758cddaefb0d220c2c47a
Tyrence <bigwheels16@gmail.com>
2010-12-22 23:21:49 +0000

    guild_chatlist table will be dropped and recreated every time the bot starts up since it doesn't contain persistent data

--- modules/ONLINE_MODULE/guild_chatlist.sql
+++ modules/ONLINE_MODULE/guild_chatlist.sql
@@ -1,1 +1,2 @@
-CREATE TABLE IF NOT EXISTS guild_chatlist_<myname> (`name` CHAR(25) PRIMARY KEY, `afk` VARCHAR(255) DEFAULT '0')
+DROP TABLE IF EXISTS guild_chatlist_<myname>;
+CREATE TABLE guild_chatlist_<myname> (`name` CHAR(25) PRIMARY KEY, `afk` VARCHAR(255) DEFAULT '0')
