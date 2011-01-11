5fef29e4f371928730af247d9830625be92ece90
Tyrence <bigwheels16@gmail.com>
2011-01-11 05:14:27 +0000

    renamed FRIENDLIST_DIAG_MODULE to FRIENDLIST_MODULE

--- modules/FRIENDLIST_MODULE/FRIENDLIST_MODULE.php
+++ modules/FRIENDLIST_MODULE/FRIENDLIST_MODULE.php
@@ -1,5 +1,5 @@
 <?php
-	$MODULE_NAME = "FRIENDLIST_DIAG_MODULE";
+	$MODULE_NAME = "FRIENDLIST_MODULE";
 
 	bot::command("", "$MODULE_NAME/friendlist.php", "friendlist", "admin", "friendlist management");
 	bot::command("", "$MODULE_NAME/rembuddy.php", "rembuddy", "admin", "friendlist management");
