64efd55a981b03560d9bb6aca00230781f04ddb8
Tyrence <bigwheels16@gmail.com>
2011-01-13 22:43:15 +0000

    rewrote !fight to not favor the first player

--- modules/FUN_MODULE/fight.php
+++ modules/FUN_MODULE/fight.php
@@ -36,7 +36,8 @@ if (preg_match("/^fight (.+) vs (.+)$/i", $message, $arr) || preg_match("/^fight
   	$hp2 = 10000;
   	
   	$list = "Fight <highlight>$player1<end> VS <highlight>$player2<end> \n\n";
-  	for ($i = 1; $i <= 10 && $hp1 > 0 && $hp2 > 0; $i++) {
+  	while ($hp1 > 0 && $hp2 > 0) {
+		// player1 dmg to player2
 	    $dmg = rand(50, 4000);
 	    if ($dmg > 3000) {
 			$crit = " <red>Critical Hit!<end>";
@@ -47,28 +48,30 @@ if (preg_match("/^fight (.+) vs (.+)$/i", $message, $arr) || preg_match("/^fight
 		$list .= "<highlight>$player1<end> hit <highlight>$player2<end> for $dmg of nerfstick dmg.$crit\n";
 		$hp2 -= $dmg;
 		
-		if ($hp2 > 0) {
-		    $dmg = rand(50, 4000);
-		    if ($dmg > 3000) {
-				$crit = " <red>Critical Hit!<end>";
-			} else {
-				$crit = "";
-			}
-				
-			$list .= "<highlight>$player2<end> hit <highlight>$player1<end> for $dmg of nerfstick dmg.$crit\n";
-			$hp1 -= $dmg;		  
+		// player2 dmg to player1
+		$dmg = rand(50, 4000);
+		if ($dmg > 3000) {
+			$crit = " <red>Critical Hit!<end>";
+		} else {
+			$crit = "";
 		}
+			
+		$list .= "<highlight>$player2<end> hit <highlight>$player1<end> for $dmg of nerfstick dmg.$crit\n";
+		$hp1 -= $dmg;
+		
+		$list .= "\n";
 	}
 	
 	if ($hp1 > $hp2) {
 		$list .= "\nAnd the winner is ..... <highlight>$player1!<end>";
+		$msg = bot::makeLink("$player1 vs $player2....$player1 wins!", $list);
 	} else if ($hp2 > $hp1) {
 		$list .= "\nAnd the winner is ..... <highlight>$player2!<end>";
+		$msg = bot::makeLink("$player1 vs $player2....$player2 wins!", $list);
 	} else {
-		$list .= "\nNo winner in this fight!";
+		$list .= "\nIt's a tie!!";
+		$msg = bot::makeLink("$player1 vs $player2....It's a tie!", $list);
 	}
-	
-	$msg = bot::makeLink("Result of the fight $player1 vs $player2.", $list);
 
 	bot::send($msg, $sendto);
 } else {
