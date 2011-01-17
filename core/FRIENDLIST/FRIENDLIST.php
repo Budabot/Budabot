edf2aaf92480526628190ace5c2af242462c09ca
Tyrence <bigwheels16@gmail.com>
2011-01-17 23:15:33 +0000

    fix for !friendlist

--- core/FRIENDLIST/FRIENDLIST.php
+++ core/FRIENDLIST/FRIENDLIST.php
@@ -1,9 +1,9 @@
 <?php
 	$MODULE_NAME = "FRIENDLIST";
 
-	bot::regcommand("msg", "$MODULE_NAME/friendlist.php", "friendlist", "admin");
-	bot::regcommand("priv", "$MODULE_NAME/friendlist.php", "friendlist", "admin");
-	bot::regcommand("guild", "$MODULE_NAME/friendlist.php", "friendlist", "admin");
+	bot::regcommand("msg", "$MODULE_NAME/friendlist_cmd.php", "friendlist", "admin");
+	bot::regcommand("priv", "$MODULE_NAME/friendlist_cmd.php", "friendlist", "admin");
+	bot::regcommand("guild", "$MODULE_NAME/friendlist_cmd.php", "friendlist", "admin");
 	
 	bot::regcommand("msg", "$MODULE_NAME/rembuddy.php", "rembuddy", "admin");
 	bot::regcommand("priv", "$MODULE_NAME/rembuddy.php", "rembuddy", "admin");
