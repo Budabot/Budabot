09a0a3df7421a9d128e65ee0eda142c0426a4d9d
Tyrence <bigwheels16@gmail.com>
2011-01-18 04:36:16 +0000

    cleaned up INFO_MODULE and GUIDEBOT_MODULE; changed !nd and !hd to !nanodelta and !healdelta

--- modules/INFO_MODULE/INFO_MODULE.php
+++ modules/INFO_MODULE/INFO_MODULE.php
@@ -7,9 +7,9 @@
 	
 	// aliases
 	bot::command("", "$MODULE_NAME/info.php", "breed", "all", "Alias for !info breed");
-	bot::command("", "$MODULE_NAME/info.php", "hd", "all", "Alias for !info hd");
+	bot::command("", "$MODULE_NAME/info.php", "healdelta", "all", "Alias for !info healdelta");
 	bot::command("", "$MODULE_NAME/info.php", "lag", "all", "Alias for !info lag");
-	bot::command("", "$MODULE_NAME/info.php", "nd", "all", "Alias for !info nd");
+	bot::command("", "$MODULE_NAME/info.php", "nanodelta", "all", "Alias for !info nanodelta");
 	bot::command("", "$MODULE_NAME/info.php", "stats", "all", "Alias for !info stats");
 	bot::command("", "$MODULE_NAME/info.php", "buffs", "all", "Alias for !info buffs");
 ?>
