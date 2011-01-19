1505ebe9f146c2ae8d7d085839ce69d375e5e8ed
Tyrence <bigwheels16@gmail.com>
2011-01-19 07:38:20 +0000

    merged what was left of BASIC_GUILD_MODULE into GUILD_MODULE

--- modules/GUILD_MODULE/GUILD_MODULE.php
+++ modules/GUILD_MODULE/GUILD_MODULE.php
@@ -9,6 +9,19 @@
 	
 	// Logon Handling
 	bot::command("", "$MODULE_NAME/logon_msg.php", "logon", "guild", "Sets a Logon Msg");
+	
+	// Afk Check
+	bot::event($MODULE_NAME, "guild", "afk_check.php", "none", "Afk check");
+	bot::command("guild", "$MODULE_NAME/afk.php", "afk", "all", "Sets a member afk");
+	bot::command("guild", "$MODULE_NAME/kiting.php", "kiting", "all", "Sets a member afk kiting");
+	
+	//Tell and Tellall
+	bot::command("guild msg", "$MODULE_NAME/tell.php", "tell", "leader", "Repeats an message 3 times in Orgchat");
+	bot::command("guild msg", "$MODULE_NAME/tell.php", "tellall", "leader", "Sends a tell to all online guildmembers");
+	
+	//Helpfile
+	bot::help($MODULE_NAME, "afk_kiting", "afk_kiting.txt", "guild", "Set yourself AFK/Kiting");
+	bot::help($MODULE_NAME, "tell", "tell.txt", "guild", "How to use tell and tellall");
 
     // Org Roster list creation and Notify on/off handling
 	bot::regevent("24hrs", "$MODULE_NAME/roster_guild.php");
