d904b4029ce55a26fc9aea76f890cef46fa0234a
Tyrence <bigwheels16@gmail.com>
2011-01-19 00:50:25 +0000

    updated default permissions for !orgmembers and !orgranks from 'all' to 'guild'

--- modules/GUILD_MODULE/GUILD_MODULE.php
+++ modules/GUILD_MODULE/GUILD_MODULE.php
@@ -8,7 +8,7 @@
 	bot::event($MODULE_NAME, "1hour", "online_check.php", "online", "Online check");
 	
 	// Logon Handling
-	bot::command("", "$MODULE_NAME/logon_msg.php", "logon", "all", "Sets a Logon Msg");
+	bot::command("", "$MODULE_NAME/logon_msg.php", "logon", "guild", "Sets a Logon Msg");
 
     // Org Roster list creation and Notify on/off handling
 	bot::regevent("24hrs", "$MODULE_NAME/roster_guild.php");
@@ -23,14 +23,14 @@
 	bot::command("", "$MODULE_NAME/updateorg.php", "updateorg", "mod", "Forcing an update of the org roster");
 	
 	// Show orgmembers
-	bot::command("", "$MODULE_NAME/orgmembers.php", "orgmembers", "all", "Show the Members(sorted by name) of the org");
-	bot::command("", "$MODULE_NAME/orgranks.php", "orgranks", "all", "Show the Members(sorted by rank) of the org");
+	bot::command("", "$MODULE_NAME/orgmembers.php", "orgmembers", "guild", "Show the Members(sorted by name) of the org");
+	bot::command("", "$MODULE_NAME/orgranks.php", "orgranks", "guild", "Show the Members(sorted by rank) of the org");
 
 	bot::addsetting($MODULE_NAME, "bot_notify", "Show/Hide Logoffs in Org Chat (Spam Prevention)", "edit", "1", "Show Logoffs;Hide Logoffs", '1;0', "mod", "$MODULE_NAME/botnotify.txt");
 	
 	//Helpfile
     bot::help($MODULE_NAME, "notify", "notify.txt", "mod", "Add or remove a player from the notify list.");
-	bot::help($MODULE_NAME, "inactivemem", "manage_guild.txt", "all", "Help on Checking for Inactive Members");
+	bot::help($MODULE_NAME, "inactivemem", "manage_guild.txt", "admin", "Help on Checking for Inactive Members");
 	bot::help($MODULE_NAME, "updateorg", "updateorg.txt", "mod", "Force an update of org roster");
 	bot::help($MODULE_NAME, "logonmsg", "logonmsg.txt", "guild", "Changing your logon message");
 	bot::help($MODULE_NAME, "orgmembers", "orgmembers_orgranks.txt", "guild", "Show current OrgMembers");
