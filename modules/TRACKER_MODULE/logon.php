9aa25983d0a6527c8d065f9fb1d6b06050fed985
Tyrence <bigwheels16@gmail.com>
2011-01-21 23:23:13 +0000

    updated TRACKER_MODULE to respect $this->vars["onlinedelay"]

--- modules/TRACKER_MODULE/logon.php
+++ modules/TRACKER_MODULE/logon.php
@@ -1,8 +1,11 @@
 <?php
 
-$uid = $this->get_uid($sender);
-$db->query("SELECT * FROM tracked_users_<myname> WHERE uid = $uid");
-if ($db->numrows() != 0) {
- 	$db->exec("INSERT INTO tracking_<myname> (uid, dt, event) VALUES ($uid, " . time() . ", 'logon')");
+if (time() >= $this->vars["onlinedelay"]) {
+	$uid = $this->get_uid($sender);
+	$db->query("SELECT * FROM tracked_users_<myname> WHERE uid = $uid");
+	if ($db->numrows() != 0) {
+		$db->exec("INSERT INTO tracking_<myname> (uid, dt, event) VALUES ($uid, " . time() . ", 'logon')");
+	}
 }
+
 ?>
