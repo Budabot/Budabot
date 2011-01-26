c652274b1ea828908ebd3ca43d61633d01c6e84c
Tyrence <bigwheels16@gmail.com>
2011-01-26 07:30:31 +0000

    fix for !sm when player info is unknown

--- modules/PRIVATE_CHANNEL_MODULE/sm.php
+++ modules/PRIVATE_CHANNEL_MODULE/sm.php
@@ -2,12 +2,16 @@
 
 if (preg_match("/^sm$/i", $message)) {
 	if (count($this->chatlist) > 0) {
-		$db->query("SELECT * FROM priv_chatlist_<myname> p1 LEFT JOIN players p2 ON p1.name = p2.name ORDER BY `profession`, `level` DESC");
+		$db->query("SELECT p2.*, p1.name as name FROM priv_chatlist_<myname> p1 LEFT JOIN players p2 ON p1.name = p2.name ORDER BY `profession`, `level` DESC");
 		$numguest = $db->numrows();
 
 		$list = "<header> {$numguest} player(s) currently in chat<end>\n\n";
 	    while ($row = $db->fObject()) {
-			$list .= "<white>$row->name - $row->level<end><green>/$row->ai_level<end><white> $row->profession, $row->guild<end>\n";
+			if ($row->profession == null) {
+				$list .= "<white>$row->name<white> - Unknown\n";
+			} else {
+				$list .= "<white>$row->name - $row->level<end><green>/$row->ai_level<end><white> $row->profession, $row->guild<end>\n";
+			}
 	    }
 
 		$msg = bot::makeLink("Chatlist ({$numguest})", $list);
