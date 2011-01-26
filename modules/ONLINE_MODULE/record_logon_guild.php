a137543f4e66755b1acf6396f1f77e5469c4468c
Tyrence <bigwheels16@gmail.com>
2011-01-26 09:08:59 +0000

    fix for not recording guild members logging on

--- modules/ONLINE_MODULE/record_logon_guild.php
+++ modules/ONLINE_MODULE/record_logon_guild.php
@@ -3,7 +3,7 @@
 if (isset($this->guildmembers[$sender])) {
   	$db->query("SELECT name FROM guild_chatlist_<myname> WHERE `name` = '$sender'");
 	if ($db->numrows() == 0) {
-	    $db->exec("INSERT INTO guild_chatlist_<myname> (`name`) VALUES ('$org_member->name')");
+	    $db->exec("INSERT INTO guild_chatlist_<myname> (`name`) VALUES ('$sender')");
 	}
 
 	// update info for player
