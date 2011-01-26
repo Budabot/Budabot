5ce8fd536bd2b51bfe95714aad9df2b4c043d136
Tyrence <bigwheels16@gmail.com>
2011-01-26 02:38:45 +0000

    fix for when the bot is not part of a guild

--- modules/ONLINE_MODULE/record_logon_guild.php
+++ modules/ONLINE_MODULE/record_logon_guild.php
@@ -1,8 +1,6 @@
 <?php
 
-$db->query("SELECT * FROM org_members_<myname> o LEFT JOIN players p ON o.name = p.name WHERE o.`name` = '$sender'");
-$org_member = $db->fObject();
-if ($org_member !== null && $org_member->mode != "del") {
+if (isset($this->guildmembers[$sender])) {
   	$db->query("SELECT name FROM guild_chatlist_<myname> WHERE `name` = '$sender'");
 	if ($db->numrows() == 0) {
 	    $db->exec("INSERT INTO guild_chatlist_<myname> (`name`) VALUES ('$org_member->name')");
