2929a4de9c8887172890181981e967c528b41791
Tyrence <bigwheels16@gmail.com>
2011-01-29 02:19:23 +0000

    added !links to NOTES_MODULE

--- modules/NOTES_MODULE/NOTES_MODULE.php
+++ modules/NOTES_MODULE/NOTES_MODULE.php
@@ -3,9 +3,12 @@
 	
 	//Setup
 	bot::loadSQLFile($MODULE_NAME, "notes");
+	bot::loadSQLFile($MODULE_NAME, "links");
 
-	bot::command("", "$MODULE_NAME/notes.php", "note", "all", "displays, adds, or removes a note from your list");
-	bot::command("", "$MODULE_NAME/notes.php", "notes", "all", "displays, adds, or removes a note from your list");
+	bot::command("", "$MODULE_NAME/notes.php", "note", "guild", "displays, adds, or removes a note from your list");
+	bot::command("", "$MODULE_NAME/notes.php", "notes", "guild", "displays, adds, or removes a note from your list");
+	
+	bot::command("", "$MODULE_NAME/links.php", "links", "guild", "displays, adds, or removes links from the org link list");
 
 	//Help files
 	bot::help($MODULE_NAME, "notes", "notes.txt", "guild", "Notes Help", "How to use notes");
--- modules/NOTES_MODULE/links.php
+++ modules/NOTES_MODULE/links.php
@@ -29,41 +29,41 @@
    ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
    */
 
-if (preg_match("/^notes?$/i", $message)) {
-	$blob = "<header> :::::: Notes for $sender :::::: <end>\n\n";
+if (preg_match("/^links$/i", $message)) {
+	$blob = "<header> :::::: Links :::::: <end>\n\n";
 
-	$sql = "SELECT * FROM notes_<myname> WHERE name LIKE '$sender'";
+	$sql = "SELECT * FROM links ORDER BY dt DESC";
   	$db->query($sql);
 	$data = $db->fObject('all');
   	forEach ($data as $row) {
-	  	$remove = bot::makeLink('Remove', "/tell <myname> <symbol>note rem $note->id" , 'chatcmd');
-	  	$blob .= "$remove $note->note\n\n";
+	  	$remove = bot::makeLink('Remove', "/tell <myname> <symbol>links rem $row->id" , 'chatcmd');
+		$website = bot::makeLink($row->website, "/start $row->website", 'chatcmd');
+		$dt = gmdate("M j, Y, G:i", $row->dt);
+	  	$blob .= "$website <white>$row->comments<end> [<green>$row->name<end>] <white>$dt<end> $remove\n";
 	}
 	
 	if (count($data) == 0) {
-		$msg = "No notes for $sender.";	
+		$msg = "No links found.";	
 	} else {
-		$msg = bot::makeLink("Notes for $sender", $blob, 'blob');
+		$msg = bot::makeLink('Links', $blob, 'blob');
 	}
   	
 	bot::send($msg, $sendto);
-} else if (preg_match("/^notes? (rem|add) (.*)$/i", $message, $arr)) {
-	$action = strtolower($arr[1]);
-	$parm2 = $arr[2];
+} else if (preg_match("/^links add ([^ ]+) (.+)$/i", $message, $arr)) {
+	$website = str_replace("'", "''", $arr[1]);
+	$comments = str_replace("'", "''", $arr[2]);
 
-	if ($action == 'rem') {
-		$numRows = $db->exec("DELETE FROM notes_<myname> WHERE id = $parm2 AND name LIKE '$sender'");
-		
-		if ($numRows) {
-			$msg = "Note deleted successfully.";
-		} else {
-			$msg = "Note could not be found.";
-		}
-	} else if ($action == 'add') {
-		$note = str_replace("'", "''", $parm2);
+	$db->query("INSERT INTO links (`name`, `website`, `comments`, `dt`) VALUES('$sender', '$website', '$comments', '" . time() . "')");
+	$msg = "Link added successfully.";
+    bot::send($msg, $sendto);
+} else if (preg_match("/^links rem ([0-9]+)$/i", $message, $arr)) {
+	$id = $arr[1];
 
-		$db->exec("INSERT INTO notes_<myname> (name, note) VALUES('$sender', '$note')");
-		$msg = "Note added successfully.";
+	$numRows = $db->exec("DELETE FROM links WHERE id = $id AND name LIKE '$sender'");
+	if ($numRows) {
+		$msg = "Link deleted successfully.";
+	} else {
+		$msg = "Link could not be found or was not submitted by you.";
 	}
     bot::send($msg, $sendto);
 }
