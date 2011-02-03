b610a3e80c3204de7331d1d09458775f6b870937
Tyrence <bigwheels16@gmail.com>
2011-02-03 18:35:28 +0000

    made separate help file for !symb command

--- modules/POCKETBOSS_MODULE/POCKETBOSS_MODULE.php
+++ modules/POCKETBOSS_MODULE/POCKETBOSS_MODULE.php
@@ -8,8 +8,7 @@
 	bot::command("", "$MODULE_NAME/pb.php", "pb", "all", "Shows what symbs a PB drops");
 	bot::command("", "$MODULE_NAME/pb.php", "symb", "all", "Shows what PB drops a symb");
 
-	bot::regGroup("PB_SYMB", $MODULE_NAME, "PocketBoss List and Symb search", "symb", "pb");
-
 	//Helpiles
     bot::help($MODULE_NAME, "pb", "pb.txt", "all", "See what drops which Pocketboss");
+	bot::help($MODULE_NAME, "symb", "symb.txt", "all", "See symbs are available");
 ?>
