8640bdf9592bb4deeb6326e303f4e859fa35f3b4
Tyrence <bigwheels16@gmail.com>
2011-02-12 19:20:13 +0000

    added !svn update dryrun and !svn status commands

--- modules/SVN_MODULE/svn.php
+++ modules/SVN_MODULE/svn.php
@@ -1,6 +1,21 @@
 <?php
 
-if (preg_match("/^svn update/i", $message)) {
+if (preg_match("/^svn update dryrun/i", $message)) {
+	$command = "svn update --dry-run"
+	$output = array();
+	$return_var = '';
+	exec($command, $output, $return_var);
+	
+	$window = "<header> :::::: SVN UPDATE --dry-run output :::::: <end>\n\n";
+	$window .= $command . "\n\n";
+	forEach ($output as $line) {
+		$window .= $line . "\n";
+	}
+	
+	$msg = Text::make_link('svn update --dry-run output', $window);
+	
+	$chatBot->send($msg, $sendto);
+} else if (preg_match("/^svn update/i", $message)) {
 	$command = "svn update --accept " . $this->settings['svnconflict'];
 	$output = array();
 	$return_var = '';
@@ -28,6 +43,21 @@ if (preg_match("/^svn update/i", $message)) {
 	}
 	
 	$msg = Text::make_link('svn info output', $window);
+	
+	$chatBot->send($msg, $sendto);
+} else if (preg_match("/^svn status/i", $message)) {
+	$command = "svn status";
+	$output = array();
+	$return_var = '';
+	exec($command, $output, $return_var);
+	
+	$window = "::: SVN STATUS output :::\n\n";
+	$window .= $command . "\n\n";
+	forEach ($output as $line) {
+		$window .= $line . "\n";
+	}
+	
+	$msg = Text::make_link('svn status output', $window);
 	
 	$chatBot->send($msg, $sendto);
 } else {
