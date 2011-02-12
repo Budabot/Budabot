e042cc23f0cbbf9f76389fa1c11517b239b949da
Tyrence <bigwheels16@gmail.com>
2011-02-12 19:47:52 +0000

    added !svn changes

--- modules/SVN_MODULE/svn.php
+++ modules/SVN_MODULE/svn.php
@@ -1,6 +1,21 @@
 <?php
 
-if (preg_match("/^svn update/i", $message)) {
+if (preg_match("/^svn changes/i", $message)) {
+	$command = "svn merge –-dry-run -r BASE:HEAD .";
+	$output = array();
+	$return_var = '';
+	exec($command, $output, $return_var);
+	
+	$window = "::: SVN MERGE --dry-run :::\n\n";
+	$window .= $command . "\n\n";
+	forEach ($output as $line) {
+		$window .= $line . "\n";
+	}
+	
+	$msg = Text::make_link('svn merge –-dry-run output', $window);
+	
+	$chatBot->send($msg, $sendto);
+} else if (preg_match("/^svn update/i", $message)) {
 	$command = "svn update --accept " . $this->settings['svnconflict'];
 	$output = array();
 	$return_var = '';
