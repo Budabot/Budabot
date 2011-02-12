7622f406460e7e513c35c97786d304624782d0d0
Tyrence <bigwheels16@gmail.com>
2011-02-12 19:52:23 +0000

    fixed regex for some of the !svn commands

--- modules/SVN_MODULE/svn.php
+++ modules/SVN_MODULE/svn.php
@@ -1,6 +1,6 @@
 <?php
 
-if (preg_match("/^svn dry/i", $message)) {
+if (preg_match("/^svn dry$/i", $message)) {
 	$command = "svn merge –-dry-run -r BASE:HEAD .";
 	$output = array();
 	$return_var = '';
@@ -15,7 +15,7 @@ if (preg_match("/^svn dry/i", $message)) {
 	$msg = Text::make_link('svn merge –-dry-run output', $window);
 	
 	$chatBot->send($msg, $sendto);
-} else if (preg_match("/^svn update/i", $message)) {
+} else if (preg_match("/^svn update$/i", $message)) {
 	$command = "svn update --accept " . $this->settings['svnconflict'];
 	$output = array();
 	$return_var = '';
@@ -30,7 +30,7 @@ if (preg_match("/^svn dry/i", $message)) {
 	$msg = Text::make_link('svn update output', $window);
 	
 	$chatBot->send($msg, $sendto);
-} else if (preg_match("/^svn info/i", $message)) {
+} else if (preg_match("/^svn info$/i", $message)) {
 	$command = "svn info";
 	$output = array();
 	$return_var = '';
@@ -45,7 +45,7 @@ if (preg_match("/^svn dry/i", $message)) {
 	$msg = Text::make_link('svn info output', $window);
 	
 	$chatBot->send($msg, $sendto);
-} else if (preg_match("/^svn status/i", $message) || preg_match("/^svn status (.*)/i", $message, $arr)) {
+} else if (preg_match("/^svn status$/i", $message) || preg_match("/^svn status (.*)$/i", $message, $arr)) {
 	$command = "svn status $arr[1]";
 	$output = array();
 	$return_var = '';
