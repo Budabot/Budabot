f3a32548db50b7ba5b9d722699f081d13464fe4d
Tyrence <bigwheels16@gmail.com>
2011-02-19 21:50:18 +0000

    updated PRIVATE_CHANNEL_MODULE to use $chatBot->data instead of $chatBot->vars for stateful session data

--- modules/PRIVATE_CHANNEL_MODULE/kickall.php
+++ modules/PRIVATE_CHANNEL_MODULE/kickall.php
@@ -32,7 +32,7 @@
 if (preg_match("/^kickall$/", $message)) {
   	$msg = "Everyone will be kicked from this channel in 10 seconds. [by <highlight>$sender<end>]";
   	$chatBot->send($msg, 'priv');
-  	$chatBot->vars["priv_kickall"] = time() + 10;
+  	$chatBot->data["priv_kickall"] = time() + 10;
 	Event::activate("2sec", "PRIVATE_CHANNEL_MODULE/kickall_event.php");
 } else {
 	$syntax_error = true;
