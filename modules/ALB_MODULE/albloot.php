ae672419f8bc8dc7b276d5045df90446e53147be
Tyrence <bigwheels16@gmail.com>
2011-02-19 19:39:17 +0000

    added ALB_MODULE

---
+++ modules/ALB_MODULE/albloot.php
@@ -0,0 +1,213 @@
+<?php
+
+require_once('func.php');
+/*
+   ** Author: Dare2005 (RK2), based on code for dbloot module by Chachy (RK2)
+   ** Description: Albtraum Loot Module
+   ** Version: 0.5
+   **
+   ** Developed for: Budabot(http://sourceforge.net/projects/budabot)
+   **
+   ** Date(created): 18.02.2011
+   ** Date(last modified): 18.02.2011
+   **
+   ** This file is part of Budabot.
+   **
+   ** Budabot is free software; you can redistribute it and/or modify
+   ** it under the terms of the GNU General Public License as published by
+   ** the Free Software Foundation; either version 2 of the License, or
+   ** (at your option) any later version.
+   **
+   ** Budabot is distributed in the hope that it will be useful,
+   ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+   ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   ** GNU General Public License for more details.
+   **
+   ** You should have received a copy of the GNU General Public License
+   ** along with Budabot; if not, write to the Free Software
+   ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+//:::: Alb Crystals and Crystalised Memories Loot ::::
+$albloot[1]["name"] = "Inert Knowledge Crystal";
+$albloot[1]["img"] = "151030";
+$albloot[1]["ref"] = "267737";
+$albloot[2]["name"] = "Energy Infused Crystal";
+$albloot[2]["img"] = "156567";
+$albloot[2]["ref"] = "267749";
+$albloot[3]["name"] = "Crystalised Memories of a Sniper";
+$albloot[3]["img"] = "72771";
+$albloot[3]["ref"] = "267704";
+$albloot[4]["name"] = "Crystalised Memories of a Defender";
+$albloot[4]["img"] = "72771";
+$albloot[4]["ref"] = "267711";
+$albloot[5]["name"] = "Crystalised Memories of a Technician";
+$albloot[5]["img"] = "72770";
+$albloot[5]["ref"] = "267698";
+$albloot[6]["name"] = "Crystalised Memories of a Mechanic";
+$albloot[6]["img"] = "72771";
+$albloot[6]["ref"] = "267701";
+$albloot[7]["name"] = "Crystalised Memories of a Surgeon";
+$albloot[7]["img"] = "72771";
+$albloot[7]["ref"] = "267728";
+$albloot[8]["name"] = "Crystalised Memories of a Engineer";
+$albloot[8]["img"] = "72771";
+$albloot[8]["ref"] = "267714";
+$albloot[9]["name"] = "Crystalised Memories of an Instructor";
+$albloot[9]["img"] = "72771";
+$albloot[9]["ref"] = "267710";
+$albloot[10]["name"] = "Crystalised Memories of a Doctor";
+$albloot[10]["img"] = "72771";
+$albloot[10]["ref"] = "267726";
+$albloot[11]["name"] = "Crystalised Memories of a Warrior";
+$albloot[11]["img"] = "72771";
+$albloot[11]["ref"] = "267697";
+//:::: Alb Sample Loot ::::
+$albloot[12]["name"] = "Radioactive Gland Sample";
+$albloot[12]["img"] = "144705";
+$albloot[12]["ref"] = "267744";
+$albloot[13]["name"] = "Venom Gland Sample";
+$albloot[13]["img"] = "253010";
+$albloot[13]["ref"] = "267745";
+$albloot[14]["name"] = "Frost Gland Sample";
+$albloot[14]["img"] = "144702";
+$albloot[14]["ref"] = "267742";
+$albloot[15]["name"] = "Acid Gland Sample";
+$albloot[15]["img"] = "144703";
+$albloot[15]["ref"] = "267743";
+$albloot[16]["name"] = "Fire Gland Sample";
+$albloot[16]["img"] = "144705";
+$albloot[16]["ref"] = "267741";
+//:::: Alb PB Loot ::::
+$albloot[17]["name"] = "Xan Essence Crystal - Summoned Terror";
+$albloot[17]["img"] = "235354";
+$albloot[17]["ref"] = "267892";
+$albloot[18]["name"] = "Xan Essence Crystal - Gruesome Misery";
+$albloot[18]["img"] = "235354";
+$albloot[18]["ref"] = "267904";
+$albloot[19]["name"] = "Xan Essence Crystal - Sister Pestilence";
+$albloot[19]["img"] = "235354";
+$albloot[19]["ref"] = "267880";
+$albloot[20]["name"] = "Xan Essence Crystal - Sister Merciless";
+$albloot[20]["img"] = "235354";
+$albloot[20]["ref"] = "267799";
+$albloot[21]["name"] = "Xan Essence Crystal - Divided Loyalty";
+$albloot[21]["img"] = "235354";
+$albloot[21]["ref"] = "267800";
+//:::: Alb Rings and Preservation Units Loot ::::
+$albloot[22]["name"] = "Ancient Speed Preservation Unit";
+$albloot[22]["img"] = "218753";
+$albloot[22]["ref"] = "267625";
+$albloot[23]["name"] = "Ancient Vision Preservation Unit";
+$albloot[23]["img"] = "218752";
+$albloot[23]["ref"] = "267626";
+$albloot[24]["name"] = "Ring of Divided Loyalty";
+$albloot[24]["img"] = "84067";
+$albloot[24]["ref"] = "267905";
+$albloot[25]["name"] = "Ring of Gruesome Misery";
+$albloot[25]["img"] = "84067";
+$albloot[25]["ref"] = "267906";
+$albloot[26]["name"] = "Ring of Sister Pestilence";
+$albloot[26]["img"] = "84067";
+$albloot[26]["ref"] = "267909";
+$albloot[27]["name"] = "Ring of Sister Merciless";
+$albloot[27]["img"] = "84067";
+$albloot[27]["ref"] = "267907";
+$albloot[28]["name"] = "Ring of Summoned Terror";
+$albloot[28]["img"] = "84067";
+$albloot[28]["ref"] = "267911";
+//:::: Alb Ancient loot list ::::
+//$albloot[30]["ql"] = "250";
+$albloot[29]["name"] = "Dormant Ancient Circuit";
+$albloot[29]["img"] = "158233";
+$albloot[29]["ref"] = "267794";
+$albloot[30]["name"] = "Empty Ancient Device";
+$albloot[30]["img"] = "218753";
+$albloot[30]["ref"] = "267709";
+$albloot[31]["name"] = "Inactive Ancient Bracer";
+$albloot[31]["img"] = "84048";
+$albloot[31]["ref"] = "267746";
+$albloot[32]["name"] = "Ancient Scrap of Spirit Knowledge";
+$albloot[32]["img"] = "163575";
+$albloot[32]["ref"] = "267677";
+$albloot[33]["name"] = "Ancient Scrap of Saturated Spirit Knowledge";
+$albloot[33]["img"] = "163575";
+$albloot[33]["ref"] = "267679";
+$albloot[34]["name"] = "Ancient Damage Generation Device";
+$albloot[34]["img"] = "218770";
+$albloot[34]["ref"] = "267725";
+$albloot[35]["name"] = "Inactive Ancient Medical Device";
+$albloot[35]["img"] = "218774";
+$albloot[35]["ref"] = "267735";
+$albloot[36]["name"] = "Inactive Ancient Engineering Device";
+$albloot[36]["img"] = "156094";
+$albloot[36]["ref"] = "267748";
+
+global $loot;
+global $residual;
+global $raidloot;
+global $vote;
+
+//error_reporting(E_ERROR |  E_PARSE | E_NOTICE);
+if (preg_match("/^albloot ([0-9]+)$/i", $message, $arr)) {
+
+	$val = $arr[1];
+	$itemname = $albloot[$val]["name"];
+	$dontadd = 0;
+	forEach ($loot as $key => $item) {
+		if ($item["name"] == $itemname) {
+			$loot[$key]["multiloot"] = $item["multiloot"]+1;
+			$total = $item["multiloot"]+1;
+			$dontadd = 1;
+			$slot = $key;
+		}
+	}
+
+	if ($dontadd == 0) {
+		if (is_array($loot)) {
+			if (count($loot) < 31) {
+				$nextloot = count($loot) + 1;
+			} else {
+				$chatBot->send("You can only roll 30 items max at one time!", $sendto);
+				return;
+			}
+		} else {
+			$nextloot = 1;
+		}
+		
+		$loot[$nextloot]["name"] = $albloot[$val]["name"];
+		$loot[$nextloot]["linky"] = Text::make_item($albloot[$val]["ref"], $albloot[$val]["ref"], 250, $albloot[$val]["name"]);
+		$loot[$nextloot]["icon"] = $albloot[$val]["img"];
+		$loot[$nextloot]["multiloot"] = 1;
+		$chatBot->send("<highlight>".$itemname."<end> will be rolled in Slot <highlight>#".$nextloot, 'priv');
+	} else {
+		$chatBot->send("<highlight>".$itemname."<end> will be rolled in Slot <highlight>#".$slot."<end> as multiloot. Total: <yellow>".$total."<end>", 'priv');
+	}
+	$chatBot->send("To add use !add ".$nextloot.", or !add 0 to remove yourself", 'priv');
+} else if (preg_match("/^alb$/i", $message)) {
+	
+	$loot1 = loot_func("<header>::::: Crystals & Crystalised Memories :::::<end>\n\n\n", 1, 11, $albloot);
+	$msg1 = Text::make_link("Crystals & Crystalised Memories", $loot1, 'blob');
+	$chatBot->send($msg1, $sendto);
+	
+	$loot2 = loot_func("<header>::::: Ancient Loots :::::<end>\n\n\n", 29, 36, $albloot);
+	$msg2 = Text::make_link("Ancient Loots", $loot2, 'blob');
+	$chatBot->send($msg2, $sendto);
+	
+	$loot3 = loot_func("<header>::::: Samples Loot :::::<end>\n\n\n", 12, 16, $albloot);
+	$msg3 = Text::make_link("Samples", $loot3, 'blob');
+	$chatBot->send($msg3, $sendto);
+	
+	$loot4 = loot_func("<header>::::: Rings and Preservation Units Loot :::::<end>\n\n\n", 22, 28, $albloot);
+	$msg4 = Text::make_link("Rings and Preservation Units", $loot4, 'blob');
+	$chatBot->send($msg4, $sendto);
+	
+	$loot5 = loot_func("<header>::::: Albtraum Pocket Boss Loot :::::<end>\n\n\n", 17, 21, $albloot);
+	$msg5 = Text::make_link("Pocket Boss Crystals", $loot5, 'blob');
+	$chatBot->send($msg5, $sendto);
+
+} else {
+	$syntax_error = true;
+}
+  
+?>
