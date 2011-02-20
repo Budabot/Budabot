f636e6a0fb68c8cb5160d8620c7d0d3021d03f51
Tyrence <bigwheels16@gmail.com>
2011-02-20 02:52:26 +0000

    updated !searchcmd to include command aliases in the search

--- core/CONFIG/searchcmd.php
+++ core/CONFIG/searchcmd.php
@@ -8,33 +8,29 @@
 	 * Date: 21.05.2010
 	 */
 
-if (preg_match("/^searchcmd (.*)/i", $message, $arr))
-{
-	$sqlquery = "SELECT DISTINCT module FROM cmdcfg_<myname> WHERE `cmd` = '".strtolower($arr[1])."' ;";
+if (preg_match("/^searchcmd (.*)/i", $message, $arr)) {
+	$cmd = strtolower($arr[1]);
+
+	$sqlquery = "SELECT DISTINCT module FROM cmdcfg_<myname> WHERE `cmd` = '{$cmd}' OR `cmd` IN (SELECT cmd FROM cmd_alias_<myname> WHERE `alias` = '{$cmd}')";
 	$db->query($sqlquery);
 	
-	if ( 0 == $db->numrows())
-	{
-		$msg = "<highlight>".strtolower($arr[1])."<end> could not be found.";
-		$chatBot->send($msg,$sendto);
+	if (0 == $db->numrows()) {
+		$msg = "<highlight>{$cmd}<end> could not be found.";
+		$chatBot->send($msg, $sendto);
 		return;
 	}
 	
 	$data = $db->fObject("all");
-	$blob = '';
-	$msg = '';
-	foreach ($data as $row)
-	{
+	$blob = "<header> :::::: Command Search Results :::::: <end>\n\n";
+	forEach ($data as $row) {
 		$foundmodule = strtoupper($row->module);
 		$blob .= Text::make_link($foundmodule.' configuration', '/tell <myname> config '.$foundmodule, 'chatcmd') . "\n";
 	}
-	if (count($data) == 0)
-	{
+
+	if (count($data) == 0) {
 		$msg = "No results found.";
-	}
-	else
-	{
-		$msg = Text::make_link(count($data) . ' results found.', $blob);
+	} else {
+		$msg = Text::make_link(count($data) . ' results found', $blob, 'blob');
 	}
 	$chatBot->send($msg, $sendto);
 }
