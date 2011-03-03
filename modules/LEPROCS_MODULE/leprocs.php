3825ff92bd508349f7768f7f610b86a9697b0db7
Tyrence <bigwheels16@gmail.com>
2011-03-03 00:50:06 +0000

    added long descriptions to leprocs db

--- modules/LEPROCS_MODULE/leprocs.php
+++ modules/LEPROCS_MODULE/leprocs.php
@@ -13,15 +13,19 @@ if (preg_match("/^leprocs (.+)$/i", $message, $arr)) {
 	if ($db->numrows() == 0) {
 	    $msg = "No procs found for profession '$profession'.";
 	} else {
-		$blob = "<header> :::::: LE Procs '$profession' :::::: <end>\n\n";
+		$blob = "<header> :::::: $profession LE Procs :::::: <end>\n\n";
 		$type = '';
 		forEach ($data as $row) {
 			if ($type != $row->proc_type) {
 				$type = $row->proc_type;
-				$blob .= "\n<tab>$type\n";
+				$blob .= "\n<tab><yellow>$type<end>\n";
 			}
-			$blob .= "<yellow>$row->name<end> $row->duration <orange>$row->modifiers<end>\n";
+			
+			$proc_trigger = "<green>" . substr($row->proc_trigger, 0, 3) . ".<end>";
+			$blob .= "$row->name <orange>$row->modifiers<end> $proc_trigger $row->duration\n";
 		}
+		
+		$blob .= "\n\nNote: Offensive procs have a 5% chance of firing every time you attack; Defensive procs have a 10% chance of firing every time something attacks you";
 		
 		$blob .= "\n\nProc info provided by Wolfbiter (RK1), Gatester (RK2)";
 
