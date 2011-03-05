35f6b7aba33228d268190280ff535a1272fb2f5e
Tyrence <bigwheels16@gmail.com>
2011-03-05 00:59:05 +0000

    fix so that duplicates can't be added to the online list

--- modules/ONLINE_MODULE/online.sql
+++ modules/ONLINE_MODULE/online.sql
@@ -1,2 +1,2 @@
-CREATE TABLE IF NOT EXISTS online (`name` CHAR(25) NOT NULL, `afk` VARCHAR(255) DEFAULT '', `channel` CHAR(50), `channel_type` CHAR(10) NOT NULL, `added_by` CHAR(25) NOT NULL, `dt` INT NOT NULL);
+CREATE TABLE IF NOT EXISTS online (`name` CHAR(25) NOT NULL, `afk` VARCHAR(255) DEFAULT '', `channel` CHAR(50), `channel_type` CHAR(10) NOT NULL, `added_by` CHAR(25) NOT NULL, `dt` INT NOT NULL, UNIQUE(name, channel_type, added_by));
 DELETE FROM online WHERE `added_by` = '<myname>';
