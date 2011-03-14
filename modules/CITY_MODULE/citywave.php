8e6902d6f353e08aa8b4225dcdf2cf018f49a468
Tyrence <bigwheels16@gmail.com>
2011-03-14 16:40:37 +0000

    fix for !citywave

--- modules/CITY_MODULE/citywave.php
+++ modules/CITY_MODULE/citywave.php
@@ -1,10 +1,12 @@
 <?php
 
 if (preg_match("/^citywave$/i", $message)) {
-	if (isset($chatBot->data["CITY_WAVE"])) {
-		$msg = "Current wave: " . $chatBot->data["CITY_WAVE"]['wave'];
-	} else {
+	if (!isset($chatBot->data["CITY_WAVE"])) {
 		$msg = "There is no raid in progress at this time.";
+	} else if ($chatBot->data["CITY_WAVE"]['wave'] == 1) {
+		$msg = "Waiting for the first wave.";
+	} else {
+		$msg = "Current wave: " . ($chatBot->data["CITY_WAVE"]['wave'] - 1);
 	}
 	$chatBot->send($msg, $sendto);
 } else {
