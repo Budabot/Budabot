ed14b13672912210d8e8b54a5a6b0b0e53a6b72a
Tyrence <bigwheels16@gmail.com>
2011-04-09 22:57:53 +0000

    fix for topic not sending to people in guild on logon

--- modules/BASIC_CHAT_MODULE/topic_logon.php
+++ modules/BASIC_CHAT_MODULE/topic_logon.php
@@ -4,7 +4,7 @@ if (Setting::get('topic') == '') {
 	return;
 }
 
-if ($type == 'joinPriv' || ($type == 'logon' && isset($chatBot->guildmembers[$sender]) && $chatBot->is_ready())) {
+if ($type == 'joinPriv' || ($type == 'logOn' && isset($chatBot->guildmembers[$sender]) && $chatBot->is_ready())) {
 	$date_string = Util::unixtime_to_readable(time() - $chatBot->settings["topic_time"], false);
 	$msg = "<highlight>Topic:<end> {$chatBot->settings["topic"]} [set by <highlight>{$chatBot->settings["topic_setby"]}<end>][<highlight>{$date_string} ago<end>]";
     $chatBot->send($msg, $sender);
