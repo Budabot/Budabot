b5d7df0179d6b98b96df58407977526c87589294
Tyrence <bigwheels16@gmail.com>
2011-04-11 23:27:59 +0000

    fix for bot not using org roster xml files from the cache directory when fc servers are down

--- core/PLAYER_LOOKUP/Guild.class.php
+++ core/PLAYER_LOOKUP/Guild.class.php
@@ -5,30 +5,29 @@ class Guild {
     public $errorCode = 0;
     public $errorInfo;
 
-    //the organisation lookup function
 	public static function get_by_id($guild_id, $rk_num = 0, $force_update = false) {
 		global $chatBot;
 
 		$data_found = false;
 		$data_save = false;
 	
-		//if no server number is specified use the one on which the bot is logged in
+		// if no server number is specified use the one on which the bot is logged in
 		if ($rk_num == 0) {
 			$rk_num = $chatBot->vars["dimension"];
 		}
 
 		$cache = $chatBot->vars["cachefolder"];
 
-		//Making sure that the cache folder exists
+		// making sure that the cache folder exists
         if (!dir($cache)) {
 	        @mkdir($cache, 0777, true);
 		}
 		
-		//Check if a xml file of the person exists and if it is uptodate
+		// check if a xml file of the person exists and if it is uptodate
 		if (!force_update && file_exists("$cache/$guild_id.$rk_num.xml")) {
 	        $mins = (time() - filemtime("$cache/$guild_id.$rk_num.xml")) / 60;
             $hours = floor($mins/60);
-            //if the file is not older then 24hrs and it is not the roster of the bot guild then use the cache one, when it the xml file from the org bot guild and not older then 6hrs use it
+            // if the file is not older then 24hrs and it is not the roster of the bot guild then use the cache one, when it the xml file from the org bot guild and not older then 6hrs use it
             if (($hours < 24 && $chatBot->vars["my_guild_id"] != $guild_id) || ($hours < 6 && $chatBot->vars["my_guild_id"] == $guild_id)) {
              	$orgxml = file_get_contents("$cache/$guild_id.$rk_num.xml");
 				if (xml::spliceData($orgxml, '<id>', '</id>') == $guild_id) {
@@ -41,7 +40,7 @@ class Guild {
 			}
         }
 
-        //If no file was found or it is outdated try to update it from anarchyonline.com
+        // if no file was found or it is outdated try to update it from anarchyonline.com
         if (!$data_found) {
 			$orgxml = xml::getUrl("http://people.anarchy-online.com/org/stats/d/$rk_num/name/$guild_id/basicstats.xml", 30);
 			if (xml::spliceData($orgxml, '<id>', '</id>') == $guild_id) {
@@ -53,10 +52,10 @@ class Guild {
 			}
 		}
 		
-		//If the site was not responding or the data was invalid and a xml file exists get that one
+		// if the site was not responding or the data was invalid and a xml file exists get that one
 		if (!$data_found && file_exists("$cache/$guild_id.$rk_num.xml")) {
 			$orgxml = file_get_contents("$cache/$guild_id.$rk_num.xml");
-			if (xml::spliceData($orgxml, '<id>', '</id>') == $name) {
+			if (xml::spliceData($orgxml, '<id>', '</id>') == $guild_id) {
 				$data_found = true;
 			} else {
 				$data_found = false;
@@ -68,12 +67,12 @@ class Guild {
 		$guild = new Guild();
 		$guild->guild_id = $guild_id;
 		
-		//if there is still no valid data available give an error back
+		// if there is still no valid data available give an error back
 		if (!$data_found) {
            	return null;
 		}
 
-		//parsing of the memberdata
+		// parsing of the memberdata
 		$members = xml::splicemultidata($orgxml, "<member>", "</member>");
         $guild->orgname	= xml::spliceData($orgxml, "<name>", "</name>");
         $guild->orgside	= xml::spliceData($orgxml, "<side>", "</side");
@@ -115,6 +114,7 @@ class Guild {
 		}
 		
 		// this is done separately from the loop above to prevent nested transaction errors from occuring
+		// when looking up charids for characters
 		if ($data_save) {
 			$db = DB::get_instance();
 			$db->beginTransaction();
