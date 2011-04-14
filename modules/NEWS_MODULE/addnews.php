6f082e21e737edc9672d5af560dad017ef94228d
Tyrence <bigwheels16@gmail.com>
2011-04-14 18:07:34 +0000

    !addnews can now accept newlines in the message

--- modules/NEWS_MODULE/addnews.php
+++ modules/NEWS_MODULE/addnews.php
@@ -1,6 +1,6 @@
 <?php
 
-if (preg_match("/^addnews (.+)$/i", $message, $arr)) {
+if (preg_match("/^addnews (.+)$/si", $message, $arr)) {
 	$news = str_replace("'", "''", $arr[1]);
 	$db->exec("INSERT INTO news (`time`, `name`, `news`) VALUES (".time().", '".$sender."', '$news')"); 
 	$msg = "News has been added successfully.";
