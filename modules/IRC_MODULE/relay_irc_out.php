97c1916979a57c57028606f2e610e11d96829abd
Teeko <bigamehunter12@gmail.com>
2011-05-01 03:42:08 +0000

    Fix for displaying ' through IRC.

--- modules/IRC_MODULE/relay_irc_out.php
+++ modules/IRC_MODULE/relay_irc_out.php
@@ -18,13 +18,15 @@ if ("1" == Setting::get('irc_status')) {
 
 		$replaces = array(
 		  '\3 (http://auno.org/ao/db.php?id=\1)',
-		  '\4 (http://auno.org/ao/db.php?id=\1&ql=\3)',
-		);
+ 		  '\4 (http://auno.org/ao/db.php?id=\1&ql=\3)',
+ 		);
+ 
+-		$message = htmlspecialchars_decode(preg_replace($patterns, $replaces, $message));
++		$message = htmlspecialchars_decode(preg_replace($patterns, $replaces, $message), ENT_QUOTES);
+ 
+ 		fputs($socket, "PRIVMSG ".Setting::get('irc_channel')." :[{$chatBot->vars['my_guild']}] $sender: $message\n");
+ 		if (Setting::get('irc_debug_messages') == 1) {
 
-		$message = htmlspecialchars_decode(preg_replace($patterns, $replaces, $message));
-
-		fputs($socket, "PRIVMSG ".Setting::get('irc_channel')." :[{$chatBot->vars['my_guild']}] $sender: $message\n");
-		if (Setting::get('irc_debug_messages') == 1) {
 			Logger::log_chat("Out. IRC Msg.", $sender, $message);
 		}
 	}
