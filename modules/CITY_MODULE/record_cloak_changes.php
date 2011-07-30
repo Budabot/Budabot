3c8532dcebc834cd4b878259c1bd96d4cba83217
Tyrence <bigwheels16@gmail.com>
2011-07-30 05:08:26 +0000

    temp fix for recording cloak changes on linux x64

--- modules/CITY_MODULE/record_cloak_changes.php
+++ modules/CITY_MODULE/record_cloak_changes.php
@@ -1,35 +1,8 @@
 <?php
-   /*
-   ** Author: Derroylo (RK2)
-   ** Description: Shows the status of city cloak
-   ** Version: 0.2
-   **
-   ** Developed for: Budabot(http://sourceforge.net/projects/budabot)
-   **
-   ** Date(created): 04.12.2005
-   ** Date(last modified): 26.11.2006
-   ** 
-   ** Copyright (C) 2005, 2006 Carsten Lohmann
-   **
-   ** Licence Infos: 
-   ** This file is part of Budabot.
-   **
-   ** Budabot is free software; you can redistribute it and/or modify
-   ** it under the terms of the GNU General Public License as published by
-   ** the Free Software Foundation; either version 2 of the License, or
-   ** (at your option) any later version.
-   **
-   ** Budabot is distributed in the hope that it will be useful,
-   ** but WITHOUT ANY WARRANTY; without even the implied warranty of
-   ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   ** GNU General Public License for more details.
-   **
-   ** You should have received a copy of the GNU General Public License
-   ** along with Budabot; if not, write to the Free Software
-   ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-   */
 
-if (-1 == $sender && preg_match("/^(.+) turned the cloaking device in your city (on|off).$/i", $message, $arr)) {
+// this breaks in debian_x64
+//if (-1 == $sender && preg_match("/^(.+) turned the cloaking device in your city (on|off).$/i", $message, $arr)) {
+if (preg_match("/^(.+) turned the cloaking device in your city (on|off).$/i", $message, $arr)) {
 	$db->exec("INSERT INTO org_city_<myname> (`time`, `action`, `player`) VALUES ('".time()."', '".$arr[2]."', '".$arr[1]."')");
 }
 
