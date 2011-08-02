c255522c1a86f31fb30cda1f2d885792d3b605e8
Tyrence <bigwheels16@gmail.com>
2011-08-02 06:12:46 +0000

    modified the config file comments and formatting

--- config.template.php
+++ config.template.php
@@ -1,23 +1,18 @@
 <?php
-/////////////////////////////////////////////////
 /* ********************************************	*/
-/* Config file for the bot.                     */
-/* To change this settings you can use the      */
-/* Ingame Commands(config/settings) for it      */
-/* but change these file only when you          */
-/* know what you are doing.                     */
+/* Config file for Budabot.                     */
 /* ********************************************	*/
 
-// Enter your Account info here
+// Account info
 $vars['login']      = "";
 $vars['password']   = "";
 $vars['name']       = "";
 $vars['my_guild']   = "";
 
-// Enter 1 for Atlantean, 2 for Rimor, 3 for Die Nueue Welt, 4 for Test
+// 1 for Atlantean, 2 for Rimor, 3 for Die Nueue Welt, 4 for Test
 $vars['dimension']  = 1;
 
-// Insert the Administrator name here
+// Administrator/Owner name
 $vars['SuperAdmin'] = "";
 
 // Database Information
@@ -27,7 +22,7 @@ $vars['DB Host'] = "./data/";	// Hostname or File location.
 $vars['DB username'] = "";		// Mysql User name
 $vars['DB password'] = "";		// Mysql Password
 
-// Logging options.  1 for enabled, 0 for disabled
+// Logging options. 1 for enabled, 0 for disabled
 $vars['error_console'] = 1;
 $vars['error_file'] = 1;
 
@@ -43,21 +38,18 @@ $vars['debug_file'] = 0;
 $vars['chat_console'] = 1;
 $vars['chat_file'] = 1;
 
-// Show aoml markup (formatting and blobs) in logs/console
+// Show aoml markup in logs/console. 1 for enabled, 0 for disabled
 $vars['show_aoml_markup'] = 0;
 
 // Cache folder for storing org xml files
 $vars['cachefolder'] = "./cache/";
 
-// Default Status for modules
-// 0 = Modules are disabled by default
-// 1 = Modules are enabled by default
+// Default Status for modules. 1 for enabled, 0 for disabled
 $settings['default_module_status'] = 0;
 
-// AO Chat Proxy
-// 1 for enabled, 0 for disabled
+// AO Chat Proxy. 1 for enabled, 0 for disabled
 $vars['use_proxy'] = 0;
 $vars['proxy_server'] = "127.0.0.1";
 $vars['proxy_port'] = 9993;
-////////////////////////////////////////////////
+
 ?>
