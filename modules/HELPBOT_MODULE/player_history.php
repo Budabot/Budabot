a9766fded937e44958dc5a69cd3f16c52a0fd304
Raging <norbby42@gmail.com>
2011-08-07 23:36:32 +0000

    Refactor: Updated to use new pagewise blob system. Refactor: Updated sided coloring to be configurable.

--- modules/HELPBOT_MODULE/player_history.php
+++ modules/HELPBOT_MODULE/player_history.php
@@ -36,21 +36,23 @@ if (preg_match("/^history (.+)$/i", $message, $arr)) {
 	} else {
 	  	$msg = "Getting History of player <highlight>$name<end>. Please standby.";
         $chatBot->send($msg, $sendto);
-
+		
+		$link = array();
 		$history = new history($name);
 		if ($history->errorCode != 0) {
 			$msg = $history->errorInfo;
 		} else {
-			$link  = "<header>::::: History from $name ::::::<end>\n\n";
-			$link .= "<highlight>Options:<end>\n";
-			$link .= "<tab><tab><a href='chatcmd:///start $url_orig'>Show History in your browser</a>\n";
-			$link .= "<tab><tab><a href='chatcmd:///tell <myname> alts $name'>Show Alts</a> \n";
-			$link .= "<tab><tab><a href='chatcmd:///tell <myname> whois $name'>Whois</a>\n";
-			$link .= "<tab><tab><a href='chatcmd:///cc addbuddy $name'>Add to your friendslist</a>\n";
-			$link .= "<tab><tab><a href='chatcmd:///cc rembuddy $name'>Remove from your friendslist</a>\n\n";
+			$link[] = "<header>::::: History of $name ::::::<end>\n\n";
+			$lh = "<highlight>Options:<end>\n";
+			$lh .= "<tab><tab><a href='chatcmd:///start $url_orig'>Show History in your browser</a>\n";
+			$lh .= "<tab><tab><a href='chatcmd:///tell <myname> alts $name'>Show Alts</a> \n";
+			$lh .= "<tab><tab><a href='chatcmd:///tell <myname> whois $name'>Whois</a>\n";
+			$lh .= "<tab><tab><a href='chatcmd:///cc addbuddy $name'>Add to your friendslist</a>\n";
+			$lh .= "<tab><tab><a href='chatcmd:///cc rembuddy $name'>Remove from your friendslist</a>\n\n";
 			
-		    $link .= "Date           Level    AI     Faction      Guild(rank) \n";
-		    $link .= "________________________________________________ \n";
+		    $lh .= "Date           Level    AI     Faction      Guild(rank) \n";
+		    $lh .= "________________________________________________ \n";
+			$l = "";
 		    forEach ($history->data as $key => $data) {
 		      	$level = $data["level"];
 		      	
@@ -61,11 +63,11 @@ if (preg_match("/^history (.+)$/i", $message, $arr)) {
 				}
 				
 				if ($data["faction"] == "Omni") {
-			      	$faction = "<blue>Omni<end>";
+			      	$faction = "<omni>Omni<end>";
 			    } else if ($data["faction"] == "Clan") {
-			      	$faction = "<red>Clan<end>";
+			      	$faction = "<clan>Clan<end>";
 			    } else {
-			      	$faction = "<white>Neutral<end>";
+			      	$faction = "<neutral>Neutral<end>";
 				}
 	
 				if ($data["guild"] == "") {
@@ -74,8 +76,9 @@ if (preg_match("/^history (.+)$/i", $message, $arr)) {
 		      		$guild = $data["guild"]."(".$data["rank"].")";
 				}
 	
-			  	$link .= "$key |  $level  | $ailevel | $faction | $guild\n";
+			  	$l .= "$key |  $level  | $ailevel | $faction | $guild\n";
 			}
+			$link[] = array("header" => $lh, "content" => $l);
 			$msg = Text::make_link("History of $name", $link);
 		}
 	}
