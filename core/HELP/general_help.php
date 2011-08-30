812d9567594a2538de6e3a79a775410c3167a182
Tyrence <bigwheels16@gmail.com>
2011-08-30 04:37:13 +0000

    for for help topics not showing under the correct module

--- core/HELP/general_help.php
+++ core/HELP/general_help.php
@@ -1,33 +1,4 @@
 <?php
-   /*
-   ** Author: Sebuda (RK2)
-   ** Description: General Help/Shows all helpfiles
-   ** Version: 0.3
-   **
-   ** Developed for: Budabot(http://sourceforge.net/projects/budabot)
-   **
-   ** Date(created): 01.10.2005
-   ** Date(last modified): 21.11.2006
-   **
-   ** Copyright (C) 2005, 2006 J. Gracik
-   **
-   ** Licence Infos:
-   ** This file is part of Budabot.
-   **
-   ** Budabot is free software; you can redistribute it and/or modify
-   ** it under the terms of the GNU General Public License as published by
-   ** the Free Software Foundation; either version 2 of the License, or
-   ** (at your option) any later version.
-   **
-   ** Budabot is distributed in the hope that it will be useful,
-   ** but WITHOUT ANY WARRANTY; without even the implied warranty of
-   ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   ** GNU General Public License for more details.
-   **
-   ** You should have received a copy of the GNU General Public License
-   ** along with Budabot; if not, write to the Free Software
-   ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-   */
 
 if (preg_match("/^about$/i", $message) || preg_match("/^help about$/i", $message)) {
 	global $version;
@@ -52,24 +23,18 @@ if (preg_match("/^about$/i", $message) || preg_match("/^help about$/i", $message
 	if (count($help_array) == 0) {
 		$msg = "<orange>No Helpfiles found.<end>";
 	} else {
-		$blob = array(array("content" => "<header> :::: Help Files for Budabot {$version} ::: <end>\n\n"));
+		$blob = "<header> :::: Help Files for Budabot {$version} ::: <end>\n\n";
 		$current_module = '';
-		$current_content = '';
 		forEach ($help_array as $row) {
 			if ($current_module != $row->module) {
-				
-				if ($current_module != '') {
-					$blob[] = array("header" => "<highlight><u>{$row->module}:</u><end>\n", "content" => $current_content, "footer" => "\n");
-				}
+				$blob .= "\n<pagebreak><highlight><u>{$row->module}:</u><end>\n";
 				$current_module = $row->module;
-				$current_content = '';
 			}
 			
-			$current_content .= "  *{$row->name}: {$row->description} <a href='chatcmd:///tell <myname> help {$row->name}'>Click here</a>\n";
+			$blob .= "  *{$row->name}: {$row->description} <a href='chatcmd:///tell <myname> help {$row->name}'>Click here</a>\n";
 		}
 		
-		$blob[] = array("header" => "<highlight><u>{$row->module}:</u><end>\n", "content" => $current_content);
-		$msg = Text::make_structured_blob("Help(main)", $blob);
+		$msg = Text::make_blob("Help(main)", $blob);
 	}
 
 	$chatBot->send($msg, $sendto);
--- mainloop.php
+++ mainloop.php
@@ -1,41 +1,12 @@
 <?php
-   /*
-   ** Author: Derroylo (both RK2)
-   ** Description: Main Loop of the bot
-   ** Version: 0.1
-   **
-   ** Developed for: Budabot(http://sourceforge.net/projects/budabot)
-   **
-   ** Date(created): 28.04.2005	
-   ** Date(last modified): 12.01.2007
-   ** 
-   ** Copyright (C) 2006 Carsten Lohmann
-   **
-   ** Licence Infos: 
-   ** This file is part of Budabot.
-   **
-   ** Budabot is free software; you can redistribute it and/or modify
-   ** it under the terms of the GNU General Public License as published by
-   ** the Free Software Foundation; either version 2 of the License, or
-   ** (at your option) any later version.
-   **
-   ** Budabot is distributed in the hope that it will be useful,
-   ** but WITHOUT ANY WARRANTY; without even the implied warranty of
-   ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   ** GNU General Public License for more details.
-   **
-   ** You should have received a copy of the GNU General Public License
-   ** along with Budabot; if not, write to the Free Software
-   ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-   */ 
 
 /* 
-* If the bot is running under Windows, use php.exe 
-* and the Windows-specific php-win.ini, else use 
-* php and the system default php.ini, if any, or a 
-* local custom php.ini if it exists (hence the new 
-* name for the Windows-specific ini-file.) 
-*/ 
+ * If the bot is running under Windows, use php.exe 
+ * and the Windows-specific php-win.ini, else use 
+ * php and the system default php.ini, if any, or a 
+ * local custom php.ini if it exists (hence the new 
+ * name for the Windows-specific ini-file.) 
+ */
 if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') { 
     $php_exec = ".\win32\php.exe -c php-win.ini"; 
 } else { 
@@ -46,7 +17,7 @@ $php_file = "main.php";
 $config_file = $argv[1];
 
 while (true) { 
-    $last_line = system("$php_exec -f $php_file -- $config_file"); 
+    $last_line = system("$php_exec -f $php_file -- $config_file");
     if (preg_match("/^The bot is shutting down.$/i", $last_line)) {
         die();
 	}
