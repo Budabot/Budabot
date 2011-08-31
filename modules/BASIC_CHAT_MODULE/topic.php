1d551517d60a3cd09106c4754e608e1b218fc173
Tyrence <bigwheels16@gmail.com>
2011-08-31 23:03:04 +0000

    code cleanup for topic

--- modules/BASIC_CHAT_MODULE/topic.php
+++ modules/BASIC_CHAT_MODULE/topic.php
@@ -1,13 +1,16 @@
 <?php
 
 if (preg_match("/^topic$/i", $message, $arr)) {
-	$date_string = Util::unixtime_to_readable(time() - Setting::get('topic_time'), false);
 	if (Setting::get('topic') == '') {
-		$topic = 'No topic set';
+		$msg = 'No topic set.';
 	} else {
+		$date_string = Util::unixtime_to_readable(time() - Setting::get('topic_time'), false);
 		$topic = Setting::get('topic');
+		$set_by = Setting::get('topic_setby');
+		
+		$msg = "<highlight>Topic:<end> {$topic} [set by <highlight>{$set_by}<end>][<highlight>{$date_string} ago<end>]";
 	}
-	$msg = "<highlight>Topic:<end> {$topic} [set by <highlight>{$chatBot->settings["topic_setby"]}<end>][<highlight>{$date_string} ago<end>]";
+
     $chatBot->send($msg, $sendto);
 } else {
 	$syntax_error = true;
--- modules/BASIC_CHAT_MODULE/topic_logon.php
+++ modules/BASIC_CHAT_MODULE/topic_logon.php
@@ -1,12 +1,15 @@
 <?php
-   
+
 if (Setting::get('topic') == '') {
 	return;
 }
 
 if ($type == 'joinPriv' || ($type == 'logOn' && isset($chatBot->guildmembers[$sender]) && $chatBot->is_ready())) {
-	$date_string = Util::unixtime_to_readable(time() - $chatBot->settings["topic_time"], false);
-	$msg = "<highlight>Topic:<end> {$chatBot->settings["topic"]} [set by <highlight>{$chatBot->settings["topic_setby"]}<end>][<highlight>{$date_string} ago<end>]";
+	$date_string = Util::unixtime_to_readable(time() - Setting::get('topic_time'), false);
+	$topic = Setting::get('topic');
+	$set_by = Setting::get('topic_setby');
+		
+	$msg = "<highlight>Topic:<end> {$topic} [set by <highlight>{$set_by}<end>][<highlight>{$date_string} ago<end>]";
     $chatBot->send($msg, $sender);
 }
 
