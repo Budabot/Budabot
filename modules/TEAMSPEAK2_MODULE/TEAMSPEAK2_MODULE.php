2724d5e3425786c3aca6b7668e8c206893ab8116
Tyrence <bigwheels16@gmail.com>
2011-09-01 04:33:09 +0000

    renamed TEAMSPEAK_MODULE to TEAMSPEAK2_MODULE

--- modules/TEAMSPEAK2_MODULE/TEAMSPEAK2_MODULE.php
+++ modules/TEAMSPEAK2_MODULE/TEAMSPEAK2_MODULE.php
@@ -1,7 +1,7 @@
 <?php
-	$MODULE_NAME = "TEAMSPEAK_MODULE";
+	$MODULE_NAME = "TEAMSPEAK2_MODULE";
 
-	Command::register($MODULE_NAME, "", "teamspeak_server.php", "ts", "all", "Show Status of the Teamspeak Server");
+	Command::register($MODULE_NAME, "", "teamspeak_server.php", "ts", "all", "Show Status of the Teamspeak2 Server");
 
 	Setting::add($MODULE_NAME, "ts_ip", "IP from the TS Server", "edit", "text", "Not set yet.", "", '', "mod");	
 	Setting::add($MODULE_NAME, "ts_queryport", "Queryport for the TS Server", "edit", "number", "51234", "", '', "mod");
@@ -9,5 +9,5 @@
 	Setting::add($MODULE_NAME, "ts_servername", "Name of the TS Server", "edit", "text", "Not set yet.", "", '', "mod");
 
 	//Help files
-    Help::register($MODULE_NAME, "ts", "ts.txt", "guild", "Using the Teamspeak plugin");
+    Help::register($MODULE_NAME, "ts", "ts.txt", "guild", "Using the Teamspeak2 plugin");
 ?>
