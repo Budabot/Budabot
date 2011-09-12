db9de08a8af8563349a49bcd4b7b633491e90278
Tyrence <bigwheels16@gmail.com>
2011-09-12 08:21:50 +0000

    fixed !dyna to return correct results; updated dyna.sql to reference playfields.sql

--- modules/HELPBOT_MODULE/dyna.php
+++ modules/HELPBOT_MODULE/dyna.php
@@ -11,44 +11,45 @@
 
 $links = array("Help" => "/tell <myname> help dyna");
 
-$dynacamps = '';
-if (preg_match ("/^dyna ([0-2]?[0-9]?[0-9])$/i", $message, $arr)) {
-	$search = str_replace(" ", "%", $arr[1]);
+if (preg_match ("/^dyna ([\\d]+)$/i", $message, $arr)) {
+	$search = $arr[1];
 	$range1 = $search - 25;
 	$range2 = $search + 25;
-	$db->query("SELECT * FROM dynadb Where minQl > $range1 AND minQl < $range2 GROUP BY `zone` ORDER BY `minQl`");
-	$dyna_found = $db->numrows();
+	$db->query("SELECT * FROM dynadb d JOIN playfields p ON d.playfield_id = p.id WHERE minQl > $range1 AND minQl < $range2 ORDER BY `minQl`");
+	echo $db->getLastQuery();
+	$count = $db->numrows();
 
 	$dynacamps = Text::make_header("Results Of Dynacamp Search For $search", $links);
 
-	$dynacamps .= "There are $dyna_found locations matching your query\n\n";
+	$dynacamps .= "There are $count locations matching your query\n\n";
 	$data = $db->fObject("all");
-	foreach($data as $row) {
-		$dynacamps .="<yellow>$row->zone:  Co-ordinates <blue>$row->cX<yellow>x<blue>$row->cY<end>\n";
+	forEach($data as $row) {
+		$coordLink = Text::make_chatcmd("{$row->cX}x{$row->cY} {$row->long_name}", "/waypoint $row->cX $row->cY $row->playfield_id");
+		$dynacamps .="<yellow>$row->long_name:  Co-ordinates $coordLink\n";
 		$dynacamps .="<green>Mob Type:  $row->mob\n";
 		$dynacamps .="<blue>Level:  $row->minQl<yellow>-<blue>$row->maxQl\n\n";
 	}
 	
-	$dynacamps = Text::make_blob("Dynacamps", $dynacamps);
+	$dynacamps = Text::make_blob("Dynacamps ($count found)", $dynacamps);
 	$chatBot->send($dynacamps, $sendto);
-} elseif (preg_match ("/^dyna (.+)$/i", $message, $arr)) {
+} else if (preg_match ("/^dyna (.+)$/i", $message, $arr)) {
 	$search = str_replace(" ", "%", $arr[1]);
-	$search = ucfirst(strtolower($search));
 	$search = str_replace("'", "''", $arr[1]);
-	$db->query("SELECT * FROM dynadb Where zone like '%$search%' OR mob = '$search' ORDER BY `minQl`");
-	$dyna_found = $db->numrows();
+	$db->query("SELECT * FROM dynadb d JOIN playfields p ON d.playfield_id = p.id WHERE long_name LIKE '%$search%' OR short_name LIKE '%$search%' OR mob LIKE '%$search%' ORDER BY `minQl`");
+	$count = $db->numrows();
 
-	$dynacamps = Text::make_header("Results Of Dynacamp Search For $search", $links);
+	$dynacamps = Text::make_header("Results Of Dynacamp Search For '$search'", $links);
 
-	$dynacamps .= "There are $dyna_found locations matching your query\n\n";
+	$dynacamps .= "There are $count locations matching your query\n\n";
 	$data = $db->fObject("all");
-	foreach($data as $row) {
-		$dynacamps .="<yellow>$row->zone:  Co-ordinates <blue>$row->cX<yellow>x<blue>$row->cY<end>\n";
+	forEach($data as $row) {
+		$coordLink = Text::make_chatcmd("{$row->cX}x{$row->cY} {$row->long_name}", "/waypoint $row->cX $row->cY $row->playfield_id");
+		$dynacamps .="<yellow>$row->long_name:  Co-ordinates $coordLink\n";
 		$dynacamps .="<green>Mob Type:  $row->mob\n";
 		$dynacamps .="<blue>Level: $row->minQl<yellow>-<blue>$row->maxQl\n\n";
 	}
 	
-	$dynacamps = Text::make_blob("Dynacamps", $dynacamps);
+	$dynacamps = Text::make_blob("Dynacamps ($count found)", $dynacamps);
 	$chatBot->send($dynacamps, $sendto);
 } else {
 	$syntax_error = true;
