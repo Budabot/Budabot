fd40f2da111cf97fcbad57b5446e1f6c6dab6849
Tyrence <bigwheels16@gmail.com>
2011-09-12 04:53:47 +0000

    expanded TEAMSPEAK3_MODULE issue #88

--- modules/TEAMSPEAK3_MODULE/TEAMSPEAK3_MODULE.php
+++ modules/TEAMSPEAK3_MODULE/TEAMSPEAK3_MODULE.php
@@ -6,10 +6,12 @@
 	Command::register($MODULE_NAME, "", "ts.php", "ts", "guild", "Show users connected to Teamspeak3 server");
 	Command::register($MODULE_NAME, "", "aospeak.php", "aospeak", "guild", "Show org members connected to AOSpeak server");
 
-	Setting::add($MODULE_NAME, "ts_username", "Username for TS Server", "edit", "text", 'serveradmin', 'serveradmin');	
-	Setting::add($MODULE_NAME, "ts_password", "Password for TS Server", "edit", "text", 'password');
-	Setting::add($MODULE_NAME, "ts_port", "Server port for the TS Server", "edit", "number", "10011", '10011');
-	Setting::add($MODULE_NAME, "ts_server", "IP/Domain name of the TS Server", "edit", "text", '127.0.0.1', '127.0.0.1');
+	Setting::add($MODULE_NAME, "ts_username", "Username for TS server", "edit", "text", 'serveradmin', 'serveradmin');	
+	Setting::add($MODULE_NAME, "ts_password", "Password for TS server", "edit", "text", 'password');
+	Setting::add($MODULE_NAME, "ts_queryport", "ServerQuery port for the TS server", "edit", "number", '10011', '10011');
+	Setting::add($MODULE_NAME, "ts_clientport", "Client port for the TS server", "edit", "number", '9987', '9987');
+	Setting::add($MODULE_NAME, "ts_description", "Description for TS server", "edit", "text", 'Teamspeak 3 Server');
+	Setting::add($MODULE_NAME, "ts_server", "IP/Domain name of the TS server", "edit", "text", '127.0.0.1', '127.0.0.1');
 
 	//Help files
 	Help::register($MODULE_NAME, "ts", "ts.txt", "guild", "How to use teamspeak");
