047805c3e02f187a61972dc56b9016ed6580296b
Tyrence <bigwheels16@gmail.com>
2011-09-19 23:00:09 +0000

    made !friendlist, !addbuddy, !rembuddy, and !rembuddyall register as normal commands and assigned help topic friendlist to them

--- core/FRIENDLIST/FRIENDLIST.php
+++ core/FRIENDLIST/FRIENDLIST.php
@@ -1,23 +1,10 @@
 <?php
 	$MODULE_NAME = "FRIENDLIST";
 
-	Command::activate("msg", "$MODULE_NAME/friendlist_cmd.php", "friendlist", "mod");
-	Command::activate("priv", "$MODULE_NAME/friendlist_cmd.php", "friendlist", "mod");
-	Command::activate("guild", "$MODULE_NAME/friendlist_cmd.php", "friendlist", "mod");
+	Command::register($MODULE_NAME, "", "friendlist_cmd.php", "friendlist", "mod", "Show buddies on bot's buddylist");
+	Command::register($MODULE_NAME, "", "addbuddy.php", "addbuddy", "mod", "Add a buddy to bot's buddylist", 'friendlist');
+	Command::register($MODULE_NAME, "", "rembuddy.php", "rembuddy", "mod", "Remove a buddy from bot's buddylist", 'friendlist');
+	Command::register($MODULE_NAME, "", "rembuddyall.php", "rembuddyall", "mod", "Remove all buddies from bot's buddylist", 'friendlist');
 	
-	Command::activate("msg", "$MODULE_NAME/rembuddy.php", "rembuddy", "mod");
-	Command::activate("priv", "$MODULE_NAME/rembuddy.php", "rembuddy", "mod");
-	Command::activate("guild", "$MODULE_NAME/rembuddy.php", "rembuddy", "mod");
-	
-	Command::activate("msg", "$MODULE_NAME/rembuddyall.php", "rembuddyall", "mod");
-	Command::activate("priv", "$MODULE_NAME/rembuddyall.php", "rembuddyall", "mod");
-	Command::activate("guild", "$MODULE_NAME/rembuddyall.php", "rembuddyall", "mod");
-	
-	Command::activate("msg", "$MODULE_NAME/addbuddy.php", "addbuddy", "mod");
-	Command::activate("priv", "$MODULE_NAME/addbuddy.php", "addbuddy", "mod");
-	Command::activate("guild", "$MODULE_NAME/addbuddy.php", "addbuddy", "mod");
-	
-	// Help files
 	Help::register($MODULE_NAME, "friendlist", "friendlist.txt", "mod", "Commands for viewing and manually changing the friend list");
-	// TODO add help for rembuddy/addbuddy/rembuddyall
 ?>
