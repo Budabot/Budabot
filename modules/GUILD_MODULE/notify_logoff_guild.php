7b5c766c987cd1cbca90f91309ae46ce13099303
Rageballs <wow@wowrelated.com>
2011-09-25 21:23:08 +0000

    Updated Logoff messages to not show full info.

--- modules/GUILD_MODULE/notify_logoff_guild.php
+++ modules/GUILD_MODULE/notify_logoff_guild.php
@@ -2,7 +2,7 @@
 
 if (isset($chatBot->guildmembers[$sender]) && $chatBot->is_ready()) {
 	if (Setting::get('first_and_last_alt_only') == 1) {
-		// if at least one alt/main is already online, don't show logon message
+		// if at least one alt/main is already online, don't show logoff message
 		$altInfo = Alts::get_alt_info($sender);
 		if (count($altInfo->get_online_alts()) > 0) {
 			return;
@@ -12,20 +12,6 @@ if (isset($chatBot->guildmembers[$sender]) && $chatBot->is_ready()) {
 	if (Setting::get('extended_logoff_display') == 0) {
 		$msg = "$sender logged off.";
 	} else {
-		$whois = Player::get_by_name($sender);
-
-		$msg = '';
-		if ($whois === null) {
-			$msg = "$sender logged off.";
-		} else {
-			$msg = Player::get_info($whois);
-
-			$msg .= " logged off.";
-
-			$altInfo = Alts::get_alt_info($sender);
-			if (count($altInfo->alts) > 0) {
-				$msg .= " " . $altInfo->get_alts_blob();
-			}
 
 			$sql = "SELECT logoff_msg FROM org_members_<myname> WHERE name = '{$sender}'";
 			$db->query($sql);
@@ -33,7 +19,6 @@ if (isset($chatBot->guildmembers[$sender]) && $chatBot->is_ready()) {
 			if ($row !== null && $row->logoff_msg != '') {
 				$msg .= " - " . $row->logoff_msg;
 			}
-		}
 	}
 
 	$chatBot->send($msg, "guild", true);
