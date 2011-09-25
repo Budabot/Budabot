9e1fc152b85fe8f643848225bb3bf69a841daf0e
Tyrence <bigwheels16@gmail.com>
2011-09-25 05:06:04 +0000

    code cleanup for !spirits

--- modules/SPIRITS_MODULE/spirits.php
+++ modules/SPIRITS_MODULE/spirits.php
@@ -118,7 +118,7 @@ else if (preg_match("/^spirits ([^0-9]+),([^0-9]+)$/i", $message, $arr)) {
 else if (preg_match("/^spirits ([0-9]+)$/i", $message, $arr)) {
 	$ql = $arr[1];
     if ($ql <= 1 OR $ql >= 300) {
-        $msg = "<red>No valid Ql specified(1-300)";
+        $msg = "Invalid Ql specified(1-300)";
 		$chatBot->send($msg, $sendto);
         return;
     }
@@ -150,7 +150,7 @@ else if (preg_match("/^spirits ([0-9]+)-([0-9]+)$/i", $message, $arr)) {
 	$qllorange = $arr[1];
 	$qlhirange = $arr[2];
 	if ($qllorange < 1 OR $qlhirange > 219 OR $qllorange >= $qlhirange) {
-		$msg = "<red>Invalid Ql range specified(1-219)";
+		$msg = "Invalid Ql range specified(1-219)";
         $chatBot->send($msg, $sendto);
         return;
 	}
@@ -183,7 +183,7 @@ else if (preg_match("/^spirits ([0-9]+) (.+)$/i", $message, $arr)) {
 	$name = $arr[2];
 	$name = ucwords(strtolower($name));
     if ($ql < 1 OR $ql > 300) {
-        $msg = "<red>No valid Ql specified(1-300)";
+        $msg = "Invalid Ql specified(1-300)";
         $chatBot->send($msg, $sendto);
         return;
     } else if (preg_match("/[^chest|ear|eye|feet|head|larm|legs|lhand|lwrist|rarm|rhand|rwrist|waist]/i", $name)) {
@@ -239,7 +239,7 @@ else if (preg_match("/^spirits ([0-9]+)-([0-9]+) (.+)$/i", $message, $arr)) {
 	$name = $arr[3];
 	$name = ucwords(strtolower($name));
 	if ($qllorange < 1 OR $qlhirange > 300 OR $qllorange >= $qlhirange) {
-		$msg = "<red>Invalid Ql range specified(1-300)";
+		$msg = "Invalid Ql range specified(1-300)";
 		$chatBot->send($msg, $sendto);
         return;
     } else if (preg_match("/[^chest|ear|eye|feet|head|larm|legs|lhand|lwrist|rarm|rhand|rwrist|waist]/i",$name)) {
@@ -292,7 +292,7 @@ else if (preg_match("/^spirits ([0-9]+)-([0-9]+) (.+)$/i", $message, $arr)) {
 else if (preg_match ("/^spiritslvl ([0-9]+)$/i", $message, $arr)) {
 	$lvl = $arr[1];
     if ($lvl < 1 OR $lvl > 219) {
-        $msg = "<red>No valid Level specified(1-219)";
+        $msg = "Invalid Level specified(1-219)";
         $chatBot->send($msg, $sendto);
         return;
     }
@@ -325,7 +325,7 @@ else if (preg_match("/^spiritslvl ([0-9]+)-([0-9]+)$/i", $message, $arr)) {
 	$lvllorange = $arr[1];
 	$lvlhirange = $arr[2];
 	if ($lvllorange < 1 OR $lvlhirange > 219 OR $lvllorange >= $lvlhirange) {
-		$msg = "<red>Invalid Level range specified(1-219)";
+		$msg = "Invalid Level range specified(1-219)";
         $chatBot->send($msg, $sendto);
         return;
 	}
@@ -353,12 +353,12 @@ else if (preg_match("/^spiritslvl ([0-9]+)-([0-9]+)$/i", $message, $arr)) {
 	}
 }
 	// If searched by minimum level and slot
-else if (ereg ("/^spiritslvl ([0-9]+) (.+)$/i", $message, $arr)) {
+else if (preg_match ("/^spiritslvl ([0-9]+) (.+)$/i", $message, $arr)) {
 	$lvl = $arr[1];
 	$name = $arr[2];
 	$name = ucwords(strtolower($name));
     if ($lvl < 1 OR $lvl > 219) {
-        $msg = "<red>No valid Level specified(1-219)";
+        $msg = "Invalid Level specified(1-219)";
         $chatBot->send($msg, $sendto);
         return;
     } else if (preg_match("/[^chest|ear|eye|feet|head|larm|legs|lhand|lwrist|rarm|rhand|rwrist|waist]/i",$name)) {
@@ -415,7 +415,7 @@ else if (preg_match("/^spiritslvl ([0-9]+)-([0-9]+) (.+)$/i", $message, $arr)) {
 	$name = $arr[3];
 	$name = ucwords(strtolower($name));
 	if ($lvllorange < 1 OR $lvlhirange > 219 OR $lvllorange >= $lvlhirange) {
-		$msg = "<red>Invalid Level range specified(1-219)";
+		$msg = "Invalid Level range specified(1-219)";
         $chatBot->send($msg, $sendto);
         return;
     } else if (preg_match("/[^chest|ear|eye|feet|head|larm|legs|lhand|lwrist|rarm|rhand|rwrist|waist]/i",$name)) {
@@ -465,8 +465,13 @@ else if (preg_match("/^spiritslvl ([0-9]+)-([0-9]+) (.+)$/i", $message, $arr)) {
 	}
 }
 	//Search by Agility
-else if (preg_match ("/^spiritsagi ([0-9,]+)$/i", $message, $arr)) {
+else if (preg_match ("/^spiritsagi ([0-9]+)$/i", $message, $arr)) {
 	$agility = $arr[1];
+	if ($agility < 1) {
+        $msg = "Invalid Agility specified(1-1276)";
+		$chatBot->send($msg, $sendto);
+        return;
+    }
 	$loagility = $agility - 10;
 	$spirits = "<header> :::::: Search Spirits Database for Agility Requirement of $agility :::::: <end>\n\n";
 	$db->query("SELECT * FROM spiritsdb WHERE agility < $agility AND agility > $loagility ORDER BY level");
@@ -499,11 +504,10 @@ else if (preg_match ("/^spiritsagi ([0-9,]+)$/i", $message, $arr)) {
 else if (preg_match ("/^spiritsagi ([0-9]+) (.+)$/i", $message, $arr)) {
 	$agility = $arr[1];
 	$loagility = $agility - 10;
-	var_dump($loagility,$agility);
 	$name = $arr[2];
 	$name = ucwords(strtolower($name));
-    if ($agility < 1 OR $agility > 1276) {
-        $msg = "<red><red>No valid Agility specified(1-1276)";
+    if ($agility < 1) {
+        $msg = "Invalid Agility specified(1-1276)";
 		$chatBot->send($msg, $sendto);
         return;
     }
@@ -554,8 +558,13 @@ else if (preg_match ("/^spiritsagi ([0-9]+) (.+)$/i", $message, $arr)) {
 	}
 }
 	//Search By Sense
-else if (preg_match ("/^spiritssen ([0-9,]+)$/i", $message, $arr)) {
+else if (preg_match ("/^spiritssen ([0-9]+)$/i", $message, $arr)) {
 	$sense = $arr[1];
+	if ($sense < 1) {
+        $msg = "Invalid Sense specified(1-1276)";
+        $chatBot->send($msg, $sendto);
+        return;
+    }
 	$losense = $sense - 10;
 	$spirits = "<header>::::Search Spirits Database for Sense Requirement of $sense::::<end>\n\n";
 	$db->query("SELECT * FROM spiritsdb WHERE sense < $sense AND sense > $losense ORDER BY level");
@@ -590,8 +599,8 @@ else if (preg_match ("/^spiritssen ([0-9]+) (.+)$/i", $message, $arr)) {
 	$losense = $sense - 10;
 	$name = $arr[2];
 	$name = ucwords(strtolower($name));
-    if ($sense < 1 OR $sense > 1276) {
-        $msg = "<red>No valid Sense specified(1-1276)";
+    if ($sense < 1) {
+        $msg = "Invalid Sense specified(1-1276)";
         $chatBot->send($msg, $sendto);
         return;
     } else if (preg_match("/[^chest|ear|eye|feet|head|larm|legs|lhand|lwrist|rarm|rhand|rwrist|waist]/i",$name)) {
