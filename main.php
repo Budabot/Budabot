0c8f35675497fd003bde87b654de20a5b4d79098
Tyrence <bigwheels16@gmail.com>
2011-09-26 20:46:42 +0000

    updated patchnotes.txt and version for 2.3_RC2

--- main.php
+++ main.php
@@ -29,7 +29,7 @@
    ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
    */  
 
-$version = "2.3_RC1";
+$version = "2.3_RC2";
 
 echo "\n\n\n\n\n
 **************************************************
