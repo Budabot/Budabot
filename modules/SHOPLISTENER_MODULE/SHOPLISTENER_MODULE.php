d8e85b8fb8cbb8308f4cc863101acba3e52a0dbf
Tyrence <bigwheels16@gmail.com>
2011-09-27 23:48:42 +0000

    added event to remove old shopping messages from the database

--- modules/SHOPLISTENER_MODULE/SHOPLISTENER_MODULE.php
+++ modules/SHOPLISTENER_MODULE/SHOPLISTENER_MODULE.php
@@ -5,6 +5,9 @@
 	DB::loadSQLFile($MODULE_NAME, 'shopping_items');
 
 	Event::register($MODULE_NAME, "allpackets", "capture.php", "none", "Capture messages from shopping channel");
+	Event::register($MODULE_NAME, "24hrs", "remove_old_messages.php", "none", "Remove old shopping messages from the database");
+	
+	Setting::add($MODULE_NAME, "shop_message_age", "The number of days to keep shopping messages", "edit", "options", "10", "1;2;5;10;15;20");
 	
 	Command::register($MODULE_NAME, "", "shop.php", "shop", "member", "Searches shopping messages");
 ?>
---
+++ modules/SHOPLISTENER_MODULE/remove_old_messages.php
@@ -0,0 +1,15 @@
+<?php
+
+$dt = time() - (86400 * Setting::get('shop_message_age'));
+
+$db->begin_transaction();
+
+$sql = "DELETE FROM shopping_messages WHERE dt < {$dt}";
+$db->exec($sql);
+
+$sql = "DELETE FROM shoppping_items WHERE message_id NOT IN (SELECT id FROM shopping_messages)";
+$db->exec($sql);
+
+$db->commit();
+
+?>
