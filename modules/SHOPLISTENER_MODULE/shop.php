93dd267f574869a0b1b8a343cbb6e8bc4d544182
Tyrence <bigwheels16@gmail.com>
2011-10-02 07:44:18 +0000

    limit max number of shopping results to 40

--- modules/SHOPLISTENER_MODULE/shop.php
+++ modules/SHOPLISTENER_MODULE/shop.php
@@ -16,19 +16,25 @@ if (preg_match("/^shop (.+)$/i", $message, $arr)) {
 			AND s1.name LIKE '%" . str_replace("'", "''", $search) . "%'
 		GROUP BY
 			sender,
-			message";
+			message
+		LIMIT
+			40";
 	$db->query($sql);
 	$data = $db->fObject('all');
 	
-	$blob = "<header> :::::: Shopping Results for '$search' :::::: <end>\n\n";
-	forEach ($data as $row) {
-		$senderLink = Text::make_userlink($row->sender);
-		$timeString = Util::unixtime_to_readable(time()- $row->dt, false);
-		$post = preg_replace('/<a href="itemref:\/\/(\d+)\/(\d+)\/(\d+)">([^<]+)<\/a>/', "<a href='itemref://\\1/\\2/\\3'>\\4</a>", $row->message);
-		$blob .= "[$senderLink]: {$post} - <highlight>($timeString ago)<end>\n\n";
+	if (count($data) > 0) {
+		$blob = "<header> :::::: Shopping Results for '$search' :::::: <end>\n\n";
+		forEach ($data as $row) {
+			$senderLink = Text::make_userlink($row->sender);
+			$timeString = Util::unixtime_to_readable(time()- $row->dt, false);
+			$post = preg_replace('/<a href="itemref:\/\/(\d+)\/(\d+)\/(\d+)">([^<]+)<\/a>/', "<a href='itemref://\\1/\\2/\\3'>\\4</a>", $row->message);
+			$blob .= "[$senderLink]: {$post} - <highlight>($timeString ago)<end>\n\n";
+		}
+		
+		$msg = Text::make_blob("Shopping Results for '$search'", $blob);
+	} else {
+		$msg = "No shopping results found for '$search'.";
 	}
-	
-	$msg = Text::make_blob("Shopping Results for '$search'", $blob);
 	$chatBot->send($msg, $sendto);
 } else {
 	$syntax_error = true;
