5e42cd5b9626f0cb5b8e92e1401d411184363686
Tyrence <bigwheels16@gmail.com>
2011-10-05 16:07:13 +0000

    added subcommands for adding, removing, and editing events

--- modules/EVENTS_MODULE/events.php
+++ modules/EVENTS_MODULE/events.php
@@ -101,25 +101,6 @@ if (preg_match("/^events$/i", $message, $arr)) {
 	}
 	
 	$chatBot->send($msg, $sendto);
-} else if (preg_match("/^events add (.+)$/i", $message, $arr)) {
-	$db->exec("INSERT INTO events (`time_submitted`, `submitter_name`, `event_name`) VALUES (".time().", '".$sender."', '".addslashes($arr[1])."')");
-	$event_id = $db->lastInsertId();
-	$msg = "Event: '$arr[1]' was submitted [Event ID $event_id].";
-	$chatBot->send($msg, $sendto);
-} else if (preg_match("/^events (rem|remove|del|delete) ([0-9]+)$/i", $message, $arr)) {
-	$db->exec("DELETE FROM events WHERE `id` = '$arr[2]'");
-	$msg = "Event Deleted.";
-	$chatBot->send($msg, $sendto);
-} else if (preg_match("/^events setdesc ([0-9]+) (.+)$/i", $message, $arr)) {
-	$db->exec("UPDATE events SET `event_desc` = '".addslashes($arr[2])."' WHERE `id` = '$arr[1]'");
-	$msg = "Description Updated.";
-	$chatBot->send($msg, $sendto);
-} else if (preg_match("/^events setdate ([0-9]+) ([0-9]{4})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01]) ([0-1][0-9]|[2][0-3]):([0-5][0-9]):([0-5][0-9])$/i", $message, $arr)) {
-	// yyyy-dd-mm hh:mm:ss GMT
-	$eventDate = gmmktime($arr[5], $arr[6], 0, $arr[3], $arr[4], $arr[2]);
-	$db->exec("UPDATE events SET `event_date` = '$eventDate' WHERE `id` = '$arr[1]'");
-	$msg = "Date/Time Updated.";
-	$chatBot->send($msg, $sendto);
 } else {
 	$syntax_error = true;
 }
