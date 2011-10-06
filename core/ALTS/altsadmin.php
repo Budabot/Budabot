97222e0d7313c0a3b6ef3333bbea5492d230f6b5
Tyrence <bigwheels16@gmail.com>
2011-10-06 18:13:42 +0000

    removed !altsadmin export and !altsadmin import

--- core/ALTS/altsadmin.php
+++ core/ALTS/altsadmin.php
@@ -59,64 +59,6 @@ if (preg_match("/^altsadmin add (.+) (.+)$/i", $message, $names)) {
 		$msg = "<highlight>$name_alt<end> has been deleted from the alt list of <highlight>$name_main.<end>";
 	}
 	$chatBot->send($msg, $sendto);
-} else if (preg_match("/^altsadmin export (.+)$/i", $message, $arr)) {
-	/* the file may only be stored under the current directory */
-	$file_name = "./".basename($arr[1]);
-	/* do not overwrite existing files */
-	if (file_exists($file_name)) {
-		$msg = "<highlight>File already exists, please specify another one.<end>";
-		$chatBot->send($msg, $sendto);
-		return;
-	}
-
-	/* get the complete alts list */
-	$db->query("SELECT * FROM alts");
-	$alts_table = $db->fObject("all");
-
-	/* write it to a file */
-	$file = fopen($file_name, 'w');
-	fwrite($file, "alt main\n");
-	forEach ($alts_table as $row) {
-		fwrite($file, $row->alt.' '.$row->main."\n");
-	}
-	fclose($file);
-
-	$msg = "Export completed into '$file_name'";
-	$chatBot->send($msg, $sendto);
-	return;
-} else if (preg_match("/^altsadmin import (.+)/i", $message, $arr)) {
-	/* the file may only be stored under the current directory */
-	$file_name = "./".basename($arr[1]);
-	/* check for existing file */
-	if (!file_exists($file_name)) {
-		$msg = "<highlight>File '$file_name' does not exist.<end>";
-		$chatBot->send($msg, $sendto);
-		return;
-	}
-
-	/* open the file */
-	$file = fopen($file_name, 'r');
-
-	/* get first line and check for "alt main" */
-	$firstline = fgets($file);
-	if (stripos($firstline, "alt main") === false) {
-		$msg = "File didn't start with expected 'alt main', aborting import.";
-		$chatBot->send($msg, $sendto);
-		return;
-	}
-	
-	$altcounter = 0;
-	while (!feof($file)) {
-		$line = fgets($file);
-		$explodeline = explode(' ', $line);
-		$name_alt = $explodeline[0];
-		$name_main = $explodeline[1];
-		$db->exec("INSERT INTO alts (`alt`, `main`) VALUES ('$name_alt', '$name_main')");
-		++$altcounter;
-	}
-	$msg = "Succesfully added $altcounter entries into the alts table.";
-	$chatBot->send($msg, $sendto);
-	return;
 } else {
 	$syntax_error = true;
 }
