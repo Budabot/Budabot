36dcff332700618c7378317d6dddf3d12db8612d
Tyrence <bigwheels16@gmail.com>
2011-10-07 20:56:32 +0000

    updated USAGE to version 1.1

--- core/USAGE/Usage.class.php
+++ core/USAGE/Usage.class.php
@@ -1,21 +1,36 @@
 <?php
 
 class Usage {
-	public function record($type, $cmd, $sender) {
+	public static function record($type, $cmd, $sender) {
 		$db = DB::get_instance();
 
 		$sql = "INSERT INTO usage_<myname> (type, command, sender, dt) VALUES ('$type', '$cmd', '$sender', " . time() . ")";
 		$db->exec($sql);
 	}
 
-	public function submitUsage() {
-		global $chatBot;
-		$db = DB::get_instance();
-
+	public static function submitUsage($debug = false) {
 		$time = time();
 		$settingName = 'last_submitted_stats';
 		$lastSubmittedStats = Setting::get($settingName);
-		
+
+		$postArray['stats'] = json_encode(Usage::getUsageInfo($lastSubmittedStats, $debug));
+
+		$url = 'stats.jkbff.com/submitUsage.php';
+		$mycurl = new MyCurl($url);
+		$mycurl->setPost($postArray);
+		$mycurl->createCurl();
+		if ($debug) {
+			echo $mycurl->__toString() . "\n";
+		}
+
+		Setting::save($settingName, $time);
+	}
+	
+	public static function getUsageInfo($lastSubmittedStats, $debug = false) {
+		global $chatBot;
+		$db = DB::get_instance();
+		global $version;
+
 		$botid = Setting::get('botid');
 		if ($botid == '') {
 			$botid = Util::genRandomString(20);
@@ -26,22 +41,28 @@ class Usage {
 		$db->query($sql);
 		$data = $db->fObject('all');
 
-		if (count($data) > 0) {
-			$obj = new stdClass;
-			$obj->id = sha1($botid . $chatBot->vars['name'] . $chatBot->vars['dimension']);
-			$obj->dimension = $chatBot->vars['dimension'];
-			$obj->commands = $data;
-			$obj->version = "1.0";
+		$settings = array();
+		$settings['dimension'] = $chatBot->vars['dimension'];
+		$settings['is_guild_bot'] = ($chatBot->vars['my_guild'] == '' ? '0' : '1');
+		$settings['guildsize'] = count($chatBot->guildmembers);
+		$settings['using_chat_proxy'] = $chatBot->vars['use_proxy'];
+		$settings['symbol'] = Setting::get('symbol');
+		$settings['spam_protection'] = Setting::get('spam_protection');
+		$settings['db_type'] = $db->get_type();
+		$settings['bot_version'] = $version;
+		$settings['using_svn'] = (file_exists("./modules/SVN_MODULE/svn.php") === true ? '1' : '0');
+		$settings['os'] = (isWindows() === true ? 'Windows' : 'Other');
+		$settings['relay_enabled'] = (Setting::get('relaybot') == 'Off' ? '0' : '1');
+		$settings['relay_type'] = Setting::get('relaytype');
 
-			$postArray['stats'] = json_encode($obj);
+		$obj = new stdClass;
+		$obj->id = sha1($botid . $chatBot->vars['name'] . $chatBot->vars['dimension']);
+		$obj->version = "1.1";
+		$obj->debug = ($debug == true ? '1' : '0');
+		$obj->commands = $data;
+		$obj->settings = $settings;
 
-			$url = 'stats.jkbff.com/submitUsage.php';
-			$mycurl = new MyCurl($url);
-			$mycurl->setPost($postArray);
-			$mycurl->createCurl();
-
-			Setting::save($settingName, $time);
-		}
+		return $obj;
 	}
 }
 
