1688381e044ba030e857b6c425cc90e9a1e50155
Tyrence <bigwheels16@gmail.com>
2011-11-01 20:23:56 +0000

    updated version and releasenotes.txt for 2.3_RC4

--- main.php
+++ main.php
@@ -29,7 +29,7 @@
    ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
    */  
 
-$version = "2.3_RC3";
+$version = "2.3_RC4";
 
 echo "\n\n\n\n\n
 **************************************************
