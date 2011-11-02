d4a103a487c0b4098c9447b30c73da207491d40c
Tyrence <bigwheels16@gmail.com>
2011-11-02 18:51:07 +0000

    fix so that !g and !b don't remain silent when being called in some cases

--- modules/BIOR_GUARDIAN_MODULE/cast_bior.php
+++ modules/BIOR_GUARDIAN_MODULE/cast_bior.php
@@ -1,11 +1,9 @@
 <?php
 
 if (preg_match("/^b$/i", $message)) {
-	if ($chatBot->data['bior'][$sender]["b"] == "ready") {
-		$msg = "<blue>$sender casted Bio Regrowth on tank. 30sec remaining.<end>";
-		$chatBot->data['bior'][$sender]["b"] = time() + 330;
-		$chatBot->send($msg, $sendto);
-	}
+	$msg = "<blue>$sender casted Bio Regrowth on tank. 30sec remaining.<end>";
+	$chatBot->data['bior'][$sender]["b"] = time() + 330;
+	$chatBot->send($msg, $sendto);
 } else {
 	$syntax_error = true;
 }
--- modules/BIOR_GUARDIAN_MODULE/cast_guard.php
+++ modules/BIOR_GUARDIAN_MODULE/cast_guard.php
@@ -1,11 +1,9 @@
 <?php
 
 if (preg_match("/^g$/i", $message)) {
-	if ($chatBot->data['guard'][$sender]["g"] == "ready") {
-		$msg = "<blue>$sender casted Guardian on tank. 40sec remaining.<end>";
-		$chatBot->data['guard'][$sender]["g"] = time() + 340;
-		$chatBot->send($msg, $sendto);
-	}
+	$msg = "<blue>$sender casted Guardian on tank. 40sec remaining.<end>";
+	$chatBot->data['guard'][$sender]["g"] = time() + 340;
+	$chatBot->send($msg, $sendto);
 } else {
 	$syntax_error = true;
 }
