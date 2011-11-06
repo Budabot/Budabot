ac7c64046f2b99529cd0ccc0081e122685fe8102
Tyrence <bigwheels16@gmail.com>
2011-11-06 21:50:42 +0000

    code cleanup for !orgranks and !aigen

--- modules/ALIEN_MODULE/aigen.php
+++ modules/ALIEN_MODULE/aigen.php
@@ -28,40 +28,33 @@
    ** along with Budabot; if not, write to the Free Software
    ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
    */
-//////////////// IGN bot plugin ::aigan:: by Ruby V1.0 //////////////
-//|help| 
-//This is a script to know the type of viralbots a boss can drop, what type bio, and what to expect when you fight these bosses.
-//Type (command) bossname <-- for results.
-//Valid bosses are: ilari, rimah, jaax, xoch or cha, ankari.
-//|help| 
-////////////////;
 
 if (preg_match("/^aigen (ankari|ilari|rimah|jaax|xoch|cha)$/i", $message, $arr)) {
 	$gen = ucfirst(strtolower($arr[1]));
 	
-	$msg = "<header>::::: Info about General $gen :::::<end>\n\n";
+	$blob = "<header>::::: Info about General $gen :::::<end>\n\n";
 	switch ($gen) {
 		case "Ankari":
-			$msg .= "<red>Low Evade/Dodge,low AR, casting Viral/Virral nukes.<blue> \nBoss of this type drops:\n\n<img src=rdb://100337></img>\n<a href='itemref://247145/247145/300'>Arithmetic Lead Viralbots</a>.\n<orange>(Nanoskill / Tradeskill)<blue>\n<img src=rdb://255705></img>\n<a href='itemref://247684/247684/300'>Kyr'Ozch Bio-Material - Type 1</a>\n<img src=rdb://255705></img>\n<a href='itemref://247685/247685/300'>Kyr'Ozch Bio-Material - Type 2</a>\n<img src=rdb://255705></img>\n<a href='itemref://288672/288673/300'>Kyr'Ozch Bio-Material - Type 48</a>";
+			$blob .= "<red>Low Evade/Dodge,low AR, casting Viral/Virral nukes.<blue> \nBoss of this type drops:\n\n<img src=rdb://100337></img>\n<a href='itemref://247145/247145/300'>Arithmetic Lead Viralbots</a>.\n<orange>(Nanoskill / Tradeskill)<blue>\n<img src=rdb://255705></img>\n<a href='itemref://247684/247684/300'>Kyr'Ozch Bio-Material - Type 1</a>\n<img src=rdb://255705></img>\n<a href='itemref://247685/247685/300'>Kyr'Ozch Bio-Material - Type 2</a>\n<img src=rdb://255705></img>\n<a href='itemref://288672/288673/300'>Kyr'Ozch Bio-Material - Type 48</a>";
 			break;
 		case "Ilari":
-			$msg .= "<red>Low Evade/Dodge.<blue>\nBoss of this type drops:\n\n<img src=rdb://100337></img>\n<a href='itemref://247146/247146/300'>Spiritual Lead Viralbots</a>.<orange>\n(Nanocost / Nanopool / Max Nano)<blue>\n<img src=rdb://255705></img>\n<a href='itemref://247681/247681/300'>Kyr'Ozch Bio-Material - Type 992</a>\n<img src=rdb://255705></img>\n<a href='itemref://247679/247679/300'>Kyr'Ozch Bio-Material - Type 880</a>";
+			$blob .= "<red>Low Evade/Dodge.<blue>\nBoss of this type drops:\n\n<img src=rdb://100337></img>\n<a href='itemref://247146/247146/300'>Spiritual Lead Viralbots</a>.<orange>\n(Nanocost / Nanopool / Max Nano)<blue>\n<img src=rdb://255705></img>\n<a href='itemref://247681/247681/300'>Kyr'Ozch Bio-Material - Type 992</a>\n<img src=rdb://255705></img>\n<a href='itemref://247679/247679/300'>Kyr'Ozch Bio-Material - Type 880</a>";
 			break;
 		case "Rimah":
-			$msg .= "<red>Low Evade/Dodge.<blue>\nBoss of this type drops:\n\n<img src=rdb://100337></img>\n<a href='itemref://247143/247143/300'>Observant Lead Viralbots</a>.<orange>\n(Init / Evades)<blue>\n<img src=rdb://255705></img>\n<a href='itemref://247675/247675/300'>Kyr'Ozch Bio-Material - Type 112</a>\n<img src=rdb://255705></img>\n<a href='itemref://247678/247678/300'>Kyr'Ozch Bio-Material - Type 240</a>";
+			$blob .= "<red>Low Evade/Dodge.<blue>\nBoss of this type drops:\n\n<img src=rdb://100337></img>\n<a href='itemref://247143/247143/300'>Observant Lead Viralbots</a>.<orange>\n(Init / Evades)<blue>\n<img src=rdb://255705></img>\n<a href='itemref://247675/247675/300'>Kyr'Ozch Bio-Material - Type 112</a>\n<img src=rdb://255705></img>\n<a href='itemref://247678/247678/300'>Kyr'Ozch Bio-Material - Type 240</a>";
 			break;
 		case "Jaax":
-			$msg .= "<red>High Evade, Low Dodge.<blue>\nBoss of this type drops:\n\n<img src=rdb://100337></img>\n<a href='itemref://247139/247139/300'>Strong Lead Viralbots</a>.<orange>\n(Melee / Spec Melee / Add All Def / Add Damage)\n<blue><img src=rdb://255705></img>\n<a href='itemref://247694/247694/300'>Kyr'Ozch Bio-Material - Type 3</a>\n<img src=rdb://255705></img>\n<a href='itemref://247688/247688/300'>Kyr'Ozch Bio-Material - Type 4</a>";
+			$blob .= "<red>High Evade, Low Dodge.<blue>\nBoss of this type drops:\n\n<img src=rdb://100337></img>\n<a href='itemref://247139/247139/300'>Strong Lead Viralbots</a>.<orange>\n(Melee / Spec Melee / Add All Def / Add Damage)\n<blue><img src=rdb://255705></img>\n<a href='itemref://247694/247694/300'>Kyr'Ozch Bio-Material - Type 3</a>\n<img src=rdb://255705></img>\n<a href='itemref://247688/247688/300'>Kyr'Ozch Bio-Material - Type 4</a>";
 			break;
 		case "Xoch":
-			$msg .= "<red>High Evade/Dodge, casting Ilari Biorejuvenation heals.<blue>\nBoss of this type drops:\n\n<img src=rdb://100337></img>\n<a href='itemref://247137/247137/300'>Enduring Lead Viralbots</a>.<orange>\n(Max Health / Body Dev)<blue>\n<img src=rdb://255705></img>\n<a href='itemref://247690/247690/300'>Kyr'Ozch Bio-Material - Type 5</a>\n<img src=rdb://255705></img>\n<a href='itemref://247692/247692/300'>Kyr'Ozch Bio-Material - Type 12</a>";
+			$blob .= "<red>High Evade/Dodge, casting Ilari Biorejuvenation heals.<blue>\nBoss of this type drops:\n\n<img src=rdb://100337></img>\n<a href='itemref://247137/247137/300'>Enduring Lead Viralbots</a>.<orange>\n(Max Health / Body Dev)<blue>\n<img src=rdb://255705></img>\n<a href='itemref://247690/247690/300'>Kyr'Ozch Bio-Material - Type 5</a>\n<img src=rdb://255705></img>\n<a href='itemref://247692/247692/300'>Kyr'Ozch Bio-Material - Type 12</a>";
 			break;
 		case "Cha":
-			$msg .= "<red>High Evade/NR, Low Dodge.<blue>\nBoss of this type drops:\n\n<img src=rdb://100337></img>\n<a href='itemref://247141/247141/300'>Supple Lead Viralbots</a>.<orange>\n(Ranged / Spec Ranged / Add All Off)\n<img src=rdb://255705></img>\n<a href='itemref://247696/247696/300'>Kyr'Ozch Bio-Material - Type 13</a>\n<img src=rdb://255705></img>\n<a href='itemref://247674/247674/300'>Kyr'Ozch Bio-Material - Type 76</a>";
+			$blob .= "<red>High Evade/NR, Low Dodge.<blue>\nBoss of this type drops:\n\n<img src=rdb://100337></img>\n<a href='itemref://247141/247141/300'>Supple Lead Viralbots</a>.<orange>\n(Ranged / Spec Ranged / Add All Off)\n<img src=rdb://255705></img>\n<a href='itemref://247696/247696/300'>Kyr'Ozch Bio-Material - Type 13</a>\n<img src=rdb://255705></img>\n<a href='itemref://247674/247674/300'>Kyr'Ozch Bio-Material - Type 76</a>";
 			break;
 	}
 	
-	$msg = Text::make_blob("Info for General ".ucfirst($gen), $msg);
+	$msg = Text::make_blob("Info about General $gen", $blob);
 	$chatBot->send($msg, $sendto);
 } else {
 	$syntax_error = true;
--- modules/ORGLIST_MODULE/orgranks.php
+++ modules/ORGLIST_MODULE/orgranks.php
@@ -1,76 +1,61 @@
 <?php
 
-if (preg_match("/^orgranks$/i", $message)) {
-	if ($chatBot->vars["my_guild_id"] == "") {
-	  	$msg = "The Bot needs to be in a org to show the orgmembers.";
-        $chatBot->send($msg, $sendto);
-	}
-	
-	$db->query("SELECT * FROM org_members_<myname> o LEFT JOIN players p ON (o.name = p.name AND p.dimension = '<dim>') WHERE `mode` != 'del' ORDER BY `guild_rank_id`");
-	$members = $db->numrows();
-  	if ($members == 0) {
-	  	$msg = "No members recorded.";
-        $chatBot->send($msg, $sendto);
-	}
-
-	$msg = "Getting guild info. Please wait...";
-    $chatBot->send($msg, $sendto);
-       	
-	$list = array("<header> :::::: Members of <myguild> (Sorted by org rank) :::::: <end>\n\n");
-	$l = "";
-	$data = $db->fObject('all');
-	forEach ($data as $row) {
-		if (Buddylist::is_online($row->name) == 1) {
-			$logged_off = "<green>Online<end>";
-        } else if ($row->logged_off != "0") {
-	        $logged_off = date("l F d, Y - H:i", $row->logged_off)."(GMT)";
-	    } else {
-	    	$logged_off = "<orange>Unknown<end>";
-		}
-	    
-	  	$l .= "<tab><highlight>$row->name<end> (Lvl $row->level/<green>$row->ai_level<end> $row->profession) (<highlight>$row->guild_rank<end>) <highlight>::<end> Last logoff: $logged_off\n";
-	}
-	
-	$list[] = $l;
-	$msg = Text::make_structured_blob("<myguild> members by org rank ($members)", $list);
-    $chatBot->send($msg, $sendto);
-} else if (preg_match("/^orgranks ([0-9]+)$/i", $message, $arr1) || preg_match("/^orgranks ([a-z0-9-]+)$/i", $message, $arr2)) {
-	if ($arr2) {
-		// Someone's name.  Doing a whois to get an orgID.
-		$name = ucfirst(strtolower($arr2[1]));
-		$whois = Player::get_by_name($name);
-
-		if ($whois === null) {
-			$msg = "Could not find character info for $name.";
+if (preg_match("/^orgranks$/i", $message, $arr) || preg_match("/^orgranks ([0-9]+)$/i", $message, $arr1) || preg_match("/^orgranks ([a-z0-9-]+)$/i", $message, $arr2)) {
+	if (isset($arr[0])) {
+		if ($chatBot->vars["my_guild_id"] == "") {
+			$msg = "The bot does not belong to an org.";
 			$chatBot->send($msg, $sendto);
-			return;
-		} else if (!$whois->guild_id) {
-			$msg = "Player <highlight>$name<end> does not seem to be in any org.";
-			$chatBot->send($msg, $sendto);
-			return;
-		} else {
-			$guild_id = $whois->guild_id;
 		}
+		
+		$sql = "SELECT * FROM org_members_<myname> o LEFT JOIN players p ON (o.name = p.name AND p.dimension = '<dim>') WHERE `mode` != 'del' ORDER BY `guild_rank_id` ASC, o.name ASC";
+		$db->query($sql);
+		$orgname = "<myguild>";
 	} else {
-		$guild_id = $arr1[1];
+		if (isset($arr2[0])) {
+			// Someone's name.  Doing a whois to get an orgID.
+			$name = ucfirst(strtolower($arr2[1]));
+			$whois = Player::get_by_name($name);
+
+			if ($whois === null) {
+				$msg = "Could not find character info for $name.";
+				$chatBot->send($msg, $sendto);
+				return;
+			} else if (!$whois->guild_id) {
+				$msg = "Player <highlight>$name<end> does not seem to be in any org.";
+				$chatBot->send($msg, $sendto);
+				return;
+			} else {
+				$guild_id = $whois->guild_id;
+			}
+		} else if (isset($arr1[0])) {
+			$guild_id = $arr1[1];
+		}
+
+		$msg = "Getting guild info. Please wait...";
+		$chatBot->send($msg, $sendto);
+		
+		$org = Guild::get_by_id($guild_id);
+		if ($org === null) {
+			$msg = "Error in getting the Org info. Either org does not exist or AO's server was too slow to respond.";
+			$chatBot->send($msg, $sendto);
+			return;
+		}
+		
+		$sql = "SELECT * FROM players WHERE guild_id = {$guild_id} AND dimension = '<dim>' ORDER BY guild_rank_id ASC, name ASC";
+		$db->query($sql);
+		$orgname = $org->orgname;
 	}
 
-  	$msg = "Getting guild info. Please wait...";
-    $chatBot->send($msg, $sendto);
-	
-    $org = Guild::get_by_id($guild_id);
-	if ($org === null) {
-		$msg = "Error in getting the Org info. Either org does not exist or AO's server was too slow to respond.";
-		$chatBot->send($msg, $sendto);
+	$data = $db->fObject('all');	
+	$count = count($data);
+	if ($count == 0) {
+	  	$msg = "No org members found.";
+        $chatBot->send($msg, $sendto);
 		return;
 	}
 	
-	$sql = "SELECT * FROM players WHERE guild_id = {$guild_id} AND dimension = '<dim>' ORDER BY guild_rank_id ASC, name ASC";
-	$db->query($sql);
+	$blob = array("{$orgname} has {$count} members.\n\n");
 	
-	$blob = array("{$org->orgname} has {$db->numrows()} members.\n\n");
-	
-	$data = $db->fObject('all');
 	$current_rank_id = '';
 	$l = "";
 	$lh = "";
@@ -89,14 +74,28 @@ if (preg_match("/^orgranks$/i", $message)) {
 		if ($row->ai_level > 0) {
 			$l .= "<green>/{$row->ai_level}<end>";
 		}
-		$l .= ", {$row->gender} {$row->breed} {$row->profession})<end>\n";
+		$l .= ", {$row->gender} {$row->breed} {$row->profession})<end>";
+		
+		if (isset($row->logged_off)) {
+			if (Buddylist::is_online($row->name) == 1) {
+				$logged_off = "<green>Online<end>";
+			} else if ($row->logged_off != "0") {
+				$logged_off = date("l F d, Y - H:i", $row->logged_off)."(GMT)";
+			} else {
+				$logged_off = "<orange>Unknown<end>";
+			}
+			$l .= " :: Last logoff: $logged_off";
+		}
+		
+		$l .= "\n";
 	}
 	
 	$blob[] = array('header' => $lh, 'content' => $l);
 	
-	$msg = Text::make_structured_blob("Org ranks for '$org->orgname'", $blob);
+	$msg = Text::make_structured_blob("Org ranks for '$orgname' ($count)", $blob);
 	$chatBot->send($msg, $sendto);
 } else {
 	$syntax_error = true;
 }
+
 ?>
