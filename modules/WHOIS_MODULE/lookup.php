930a07d3c5c844ce7da9532ff1e2cd3202a45333
Tyrence <bigwheels16@gmail.com>
2011-11-17 21:07:41 +0000

    merged !charid into !lookup

--- modules/WHOIS_MODULE/lookup.php
+++ modules/WHOIS_MODULE/lookup.php
@@ -1,6 +1,23 @@
 <?php
 
-if (preg_match("/^lookup (.*)$/i", $message, $arr)) {
+if (preg_match("/^lookup (\\d+)$/i", $message, $arr)) {
+	$charid = $arr[1];
+	$db->query("SELECT * FROM name_history WHERE charid = $charid AND dimension = <dim> ORDER BY dt DESC");
+	$data = $db->fObject('all');
+	$count = count($data);
+
+	$blob = "<header> :::::: Name History for $charid ($count) :::::: <end>\n\n";
+	if ($count > 0) {
+		forEach ($data as $row) {
+			$blob .= "<green>{$row->name}<end> " . date("M j, Y, G:i", $row->dt) . "\n";
+		}
+		$msg = Text::make_blob("Name History for $charid ($count)", $blob);
+	} else {
+		$msg = "No name history available.";
+	}
+	
+	$chatBot->send($msg, $sendto);
+} else if (preg_match("/^lookup (.*)$/i", $message, $arr)) {
 	$name = ucfirst(strtolower($arr[1]));
 	$uid = $chatBot->get_uid($name);
 
