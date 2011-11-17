6ef645403324ee76b6fed504feb5e9e7f8560088
Tyrence <bigwheels16@gmail.com>
2011-11-17 23:53:06 +0000

    fix for ONLINE_MODULE showing people online from last restart

--- modules/ONLINE_MODULE/online_check.php
+++ modules/ONLINE_MODULE/online_check.php
@@ -3,18 +3,25 @@
 if ($chatBot->is_ready()) {
 	$db->begin_transaction();
 	   
-	$sql = "SELECT name FROM `online`";
+	$sql = "SELECT name, channel_type FROM `online`";
 	$db->query($sql);
 	$data = $db->fObject('all');
-	$array = array();
+	$guildArray = array();
+	$privArray = array();
 	forEach ($data as $row) {
-		$array []= $row->name;
+		if ($row->channel_type == 'guild') {
+			$guildArray []= $row->name;
+		} else if ($row->channel_type == 'priv') {
+			$privArray []= $row->name;
+		} else {
+			Logger::log("WARN", "ONLINE_MODULE", "Unknown channel type: '$row->channel_type'. Expected: 'guild' or 'priv'");
+		}
 	}
 
 	$time = time();
 	forEach ($chatBot->guildmembers as $name => $rank) {
 		if (Buddylist::is_online($name)) {
-			if (in_array($name, $array)) {
+			if (in_array($name, $guildArray)) {
 				$db->exec("UPDATE `online` SET `dt` = " . $time . " WHERE `name` = '$name' AND added_by = '<myname>' AND channel_type = 'guild'");
 			} else {
 				$db->exec("INSERT INTO `online` (`name`, `channel`,  `channel_type`, `added_by`, `dt`) VALUES ('$name', '<myguild>', 'guild', '<myname>', " . $time . ")");
@@ -23,15 +30,18 @@ if ($chatBot->is_ready()) {
 	}
 
 	forEach ($chatBot->chatlist as $name => $value) {
-		if (in_array($name, $array)) {
+		if (in_array($name, $privArray)) {
 			$db->exec("UPDATE `online` SET `dt` = " . $time . " WHERE `name` = '$name' AND added_by = '<myname>' AND channel_type = 'priv'");
 		} else {
 			$db->exec("INSERT INTO `online` (`name`, `channel`,  `channel_type`, `added_by`, `dt`) VALUES ('$name', '<myguild> Guest', 'priv', '<myname>', " . $time . ")");
 		}
 	}
+	
+	$sql = "DELETE FROM `online` WHERE ";
+	$db->exec($sql);
 
 	$time_to_expire = ($time - Setting::get('online_expire'));
-	$sql = "DELETE FROM `online` WHERE `dt` < {$time_to_expire}";
+	$sql = "DELETE FROM `online` WHERE (`dt` < {$time} AND added_by = '<myname>') OR (`dt` < {$time_to_expire})";
 	$db->exec($sql);
 
 	$db->commit();
