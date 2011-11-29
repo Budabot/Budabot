85069d4a6b49615241daf07a5385ab0bf0a0493e
Tyrence <bigwheels16@gmail.com>
2011-11-29 21:46:48 +0000

    add 'irc_ignore' setting to allow the bot to ignore users in the irc channel

--- modules/IRC_MODULE/IRC_MODULE.php
+++ modules/IRC_MODULE/IRC_MODULE.php
@@ -58,7 +58,7 @@
 	Setting::add($MODULE_NAME, 'irc_guild_message_color', "Color of messages from other bots in the IRC channel", 'edit', "color", "<font color='#FFFFFF'>");
 	Setting::add($MODULE_NAME, 'irc_guild_name_color', "Color of guild names from other bots in the IRC channel", 'edit', "color", "<font color='#FFFFFF'>");
 	Setting::add($MODULE_NAME, 'irc_message_color', "Color of messages from users in the IRC channel", 'edit', "color", "<font color='#FFFFFF'>");
-	Setting::add($MODULE_NAME, 'irc_ignore', "Defines which characters to ignore", 'edit', "text");
+	Setting::add($MODULE_NAME, 'irc_ignore', "Defines which characters to ignore", 'edit', "text", 'none', 'none');
 	
 	// Helpfiles
 	Help::register($MODULE_NAME, "irc", "irc_help.txt", "all", "How to use the IRC plugin");
