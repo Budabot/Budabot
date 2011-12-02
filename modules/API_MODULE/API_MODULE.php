5a524f0ae104ae98385ac427ab6f5e054c9402ca
Tyrence <bigwheels16@gmail.com>
2011-12-02 19:51:44 +0000

    removed !testapi

--- modules/API_MODULE/API_MODULE.php
+++ modules/API_MODULE/API_MODULE.php
@@ -8,7 +8,5 @@
 	
 	Setting::add($MODULE_NAME, "api_port", "Port number to listen for API requests", "edit", "number", '5250', '5250');
 
-	Command::register($MODULE_NAME, "", "testapi.php", "testapi", "mod", "Test API MODULE");
 	Command::register($MODULE_NAME, "", "apipassword.php", "apipassword", "mod", "Set your API password");
-
 ?>
