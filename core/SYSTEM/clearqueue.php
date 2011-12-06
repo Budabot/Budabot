82ac34b0c78bd485d6ffe93385234bb432048c56
Tyrence <bigwheels16@gmail.com>
2011-12-06 08:10:48 +0000

    !clearqueue now displays how many messages were cleared

--- core/SYSTEM/clearqueue.php
+++ core/SYSTEM/clearqueue.php
@@ -1,9 +1,13 @@
 <?php
 
 if (preg_match("/^clearqueue$/i", $message)) {
+	$num = 0;
+	forEach ($chatBot->chatqueue->queue as $priority) {
+		$num += count($priority);
+	}
 	$chatBot->chatqueue->queue = array();
 
-	$chatBot->send('Chat queue has been cleared.', $sendto);
+	$chatBot->send("Chat queue has been cleared of $num messages.", $sendto);
 }
 
 ?>
