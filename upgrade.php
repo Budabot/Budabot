d309f15ceb87fc56b8dfc05bb4536d8b902e6117
Tyrence <bigwheels16@gmail.com>
2011-12-23 18:05:45 +0000

    updated upgrade.php to not cause errors

--- upgrade.php
+++ upgrade.php
@@ -1,25 +1,24 @@
 <?php
 
-// disable error logging
-$error_file = $vars['error_file'];
-$error_console = $vars['error_console'];
+function checkIfColumnExists($table, $column) {
+	$db = DB::get_instance();
 
-//$vars['error_file'] = 0;
-//$vars['error_console'] = 0;
-require_once 'core/PREFERENCES/Preferences.class.php';
-$data = $db->query("SELECT name, logon_msg, logoff_msg FROM org_members_<myname>");
-forEach ($data as $row) {
-	if ($row->logon_msg != '') {
-		Preferences::save($row->name, 'logon_msg', $row->logon_msg);
-	}
-	if ($row->logoff_msg != '') {
-		Preferences::save($row->name, 'logoff_msg', $row->logoff_msg);
-	}
+	$data = $db->query("SELECT * FROM org_members_<myname>");
+	return property_exists($data[0], 'logon_msg');
 }
-$db->exec("UPDATE org_members_<myname> SET logon_msg = '', logoff_msg = ''");
 
-// re-enable error logging
-$vars['error_file'] = $error_file;
-$vars['error_console'] = $error_console;
+require_once 'core/PREFERENCES/Preferences.class.php';
+$data = $db->query("SELECT * FROM org_members_<myname>");
+if (property_exists($data[0], 'logon_msg') || property_exists($data[0], 'logoff_msg')) {
+	forEach ($data as $row) {
+		if (isset($row->logon_msg) && $row->logon_msg != '') {
+			Preferences::save($row->name, 'logon_msg', $row->logon_msg);
+		}
+		if (isset($row->logoff_msg) && $row->logoff_msg != '') {
+			Preferences::save($row->name, 'logoff_msg', $row->logoff_msg);
+		}
+	}
+	$db->exec("UPDATE org_members_<myname> SET logon_msg = '', logoff_msg = ''");
+}
 
 ?>
