5d96134599c6243303bd50a49a7995f1ec5cf2e0
Tyrence <bigwheels16@gmail.com>
2011-12-23 21:08:11 +0000

    API_MODULE defaults to disabled

--- core/API_MODULE/API_MODULE.php
+++ core/API_MODULE/API_MODULE.php
@@ -3,8 +3,8 @@
 	require_once 'APIRequest.class.php';
 	require_once 'APIResponse.class.php';
 	
-	Event::register($MODULE_NAME, "connect", "connect.php", "Opens a socket to listen for API requests");
-	Event::register($MODULE_NAME, "2sec", "listen.php", "Checks for and processes API requests");
+	Event::register($MODULE_NAME, "connect", "connect.php", "Opens a socket to listen for API requests", '', 0);
+	Event::register($MODULE_NAME, "2sec", "listen.php", "Checks for and processes API requests", '', 0);
 	
 	Setting::add($MODULE_NAME, "api_port", "Port number to listen for API requests", "edit", "number", '5250', '5250');
 
