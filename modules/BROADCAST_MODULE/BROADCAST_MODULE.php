da430eae2d7e5a42c0adaf2dadca6a74a30fb950
Tyrence <bigwheels16@gmail.com>
2011-12-24 20:45:04 +0000

    removed explicit neutnet support from BROADCAST_MODULE

--- modules/BROADCAST_MODULE/BROADCAST_MODULE.php
+++ modules/BROADCAST_MODULE/BROADCAST_MODULE.php
@@ -7,14 +7,12 @@
 	Event::register($MODULE_NAME, "extPriv", "incoming_broadcast.php", 'Relays incoming messages to the guild/private channel');
 	
 	Command::register($MODULE_NAME, "", "broadcast.php", "broadcast", "mod", "View/edit the broadcast bots list");
-	Command::register($MODULE_NAME, "", "neutnet.php", "neutnet", "mod", "Enable/disable Neutnet support (RK 2 only)");
 	Command::register($MODULE_NAME, "", "dnet.php", "dnet", "mod", "Enable/disable Dnet support (RK 1 only)");
 	
 	Setting::add($MODULE_NAME, "broadcast_to_guild", "Send broadcast message to guild channel", "edit", "options", "1", "true;false", "1;0");
 	Setting::add($MODULE_NAME, "broadcast_to_privchan", "Send broadcast message to private channel", "edit", "options", "0", "true;false", "1;0");
 	Setting::add($MODULE_NAME, "dnet_status", "Enable Dnet support", "noedit", "options", "0", "true;false", "1;0");
 	
-	Help::register($MODULE_NAME, "neutnet", "neutnet.txt", "mod", "How to enable Neutnet support");
 	Help::register($MODULE_NAME, "dnet", "dnet.txt", "mod", "How to enable Dnet support");
 	Help::register($MODULE_NAME, "broadcast", "broadcast.txt", "all", "How to manage the broadcast list");
 ?>
