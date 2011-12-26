cb1cdf511382bba361e15bfcac8c38c6e33a1195
Tyrence <bigwheels16@gmail.com>
2011-12-26 22:27:17 +0000

    default access level for !orglist reduced from 'rl' to 'guild'

--- modules/ORGLIST_MODULE/ORGLIST_MODULE.php
+++ modules/ORGLIST_MODULE/ORGLIST_MODULE.php
@@ -6,14 +6,14 @@
 	Command::register($MODULE_NAME, "", "whoisorg.php", "whoisorg", "all", "Display org info");
 	Command::register($MODULE_NAME, "", "findorg.php", "findorg", "all", "Find orgs by name");
 	Command::register($MODULE_NAME, "", "is_online.php", "is", "all", "Checks if a player is online");
-	Command::register($MODULE_NAME, "", "orglist.php", "orglist", "rl", "Check someones org roster");
+	Command::register($MODULE_NAME, "", "orglist.php", "orglist", "guild", "Check someones org roster");
 
 	Event::register($MODULE_NAME, "logOn", "orglist_check.php", "Gets online status of org members");
 	Event::register($MODULE_NAME, "logOff", "orglist_check.php", "Gets offline status of org members");
 	Event::register($MODULE_NAME, "logOn", "is_online_check.php", "Gets online status of player");
 	Event::register($MODULE_NAME, "logOff", "is_online_check.php", "Gets offline status of player");
 
-	Help::register($MODULE_NAME, "orglist", "orglist.txt", "all", "See who is online from someones org");
+	Help::register($MODULE_NAME, "orglist", "orglist.txt", "guild", "See who is online from someones org");
 	Help::register($MODULE_NAME, "findorg", "findorg.txt", "all", "Find an org by name");
 	Help::register($MODULE_NAME, "is", "isonline.txt", "guild", "Checking if a player is online");
 	Help::register($MODULE_NAME, "orgmembers", "orgmembers.txt", "guild", "How to use orgmembers");
