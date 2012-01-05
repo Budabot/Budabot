8ac68dcf9378b4c3949ecfd394318f7f3aeeffc7
Tyrence <bigwheels16@gmail.com>
2012-01-05 21:30:01 +0000

    fix for !banlist.php

--- core/BAN/banlist.php
+++ core/BAN/banlist.php
@@ -1,15 +1,15 @@
 <?php
 
-$banInstance = Registry::getInstance('ban');
+$banlist = Registry::getInstance('ban')->getBanlist();
 
 if (preg_match("/^banlist$/i", $message)) {
-  	if (count($banInstance = Registry::getInstance('ban')->getBanlist()) == 0) {
+  	if (count($banlist) == 0) {
 	    $chatBot->send("No one is currently banned from this bot.", $sendto);
 	    return;
 	}
 	
 	$list .= "<header> :::::: Banlist :::::: <end>\n\n";
-	forEach ($banInstance = Registry::getInstance('ban')->getBanlist() as $ban) {
+	forEach ($banlist as $ban) {
 		$list .= "<highlight>Name:<end> {$ban->name}\n";
 		$list .= "<highlight><tab>Date:<end> ".date(DATE_RFC850, $ban->time)."\n";
 		$list .= "<highlight><tab>By:<end> {$ban->admin}\n";
