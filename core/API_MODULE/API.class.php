c61d26656d911bb57d922f7f5c66ccb838bd694a
Tyrence <bigwheels16@gmail.com>
2012-01-06 23:04:08 +0000

    core API_MODULE now uses new OO method

---
+++ core/API_MODULE/API.class.php
@@ -0,0 +1,97 @@
+<?php
+
+class API {
+
+	/** @Inject */
+	public $command;
+	
+	/** @Inject */
+	public $preferences;
+	
+	/** @Inject */
+	public $chatBot;
+
+	/**
+	 * @Setting("api_port")
+	 * @Description("Port number to listen for API requests")
+	 * @Visibility("edit")
+	 * @Type("number")
+	 * @Options("5250")
+	 */
+	public $defaultAPIPort = "5250";
+	
+	private $apisocket;
+
+	/**
+	 * @Event("connect")
+	 * @Description("Opens a socket to listen for API requests")
+	 * @DefaultStatus("0")
+	 */
+	public function connect($eventObj) {
+		// bind to port 5250 on any address
+		$address = '0.0.0.0';
+		$port = $setting->get('api_port');
+
+		// Create a TCP Stream socket
+		$this->apisocket = socket_create(AF_INET, SOCK_STREAM, 0);
+		socket_bind($this->apisocket, $address, $port);
+		$errno = socket_last_error();
+		if ($errno == 0) {
+			Logger::log('INFO', 'API_MODULE', 'API socket bound successfully');
+		} else {
+			Logger::log('ERROR', 'API_MODULE', socket_strerror($errno));
+		}
+		socket_listen($this->apisocket);
+		socket_set_nonblock($this->apisocket);
+	}
+	
+	/**
+	 * @Event("2sec")
+	 * @Description("Checks for and processes API requests")
+	 * @DefaultStatus("0")
+	 */
+	public function listen($eventObj) {
+		/* Accept incoming requests and handle them as child processes */
+		$client = socket_accept($this->apisocket);
+		if ($client !== false) {
+			$clientHandler = new ClientHandler($client);
+
+			// Read the input from the client
+			$apiRequest = $clientHandler->readPacket();
+			if ($apiRequest->version != API_VERSION) {
+				$clientHandler->writePacket(new APIResponse(API_FAILURE, "API version must be: " . API_VERSION));
+			}
+			
+			$password = $this->preferences->get($apiRequest->username, 'apipassword');
+			if ($password === false) {
+				$clientHandler->writePacket(new APIResponse(API_FAILURE, "Password has not been set for this user."));
+			} else if ($password != $apiRequest->password) {
+				$clientHandler->writePacket(new APIResponse(API_FAILURE, "Password was incorrect."));
+			} else {
+				if ($apiRequest->type == API_SIMPLE_MSG) {
+					$type = 'msg';
+				} else if ($apiRequest->type == API_ADVANCED_MSG) {
+					$type = 'api';
+				} else {
+					$clientHandler->writePacket(new APIResponse(API_FAILURE, "Invalid request type."));
+					return;
+				}
+
+				$this->command->command($type, $apiRequest->command, $apiRequest->username, $clientHandler);
+			}
+		}
+	}
+	
+	/**
+	 * @Command("apipassword")
+	 * @AccessLevel("mod")
+	 * @Description("Set your api password")
+	 * @Matches("/^apipassword (.*)$/i")
+	 */
+	public function apipasswordCommand($message, $channel, $sender, $sendto, $arr) {
+		$this->preferences->save($sender, 'apipassword', $password, $arr[1]);
+		$this->chatBot->send("Your API password has been updated successfully.", $sendto);
+	}
+}
+
+?>
--- core/API_MODULE/API_MODULE.php
+++ core/API_MODULE/API_MODULE.php
@@ -2,11 +2,7 @@
 	require_once 'ClientHandler.class.php';
 	require_once 'APIRequest.class.php';
 	require_once 'APIResponse.class.php';
+	require_once 'API.class.php';
 	
-	$event->register($MODULE_NAME, "connect", "connect.php", "Opens a socket to listen for API requests", '', 0);
-	$event->register($MODULE_NAME, "2sec", "listen.php", "Checks for and processes API requests", '', 0);
-	
-	$setting->add($MODULE_NAME, "api_port", "Port number to listen for API requests", "edit", "number", '5250', '5250');
-
-	$command->register($MODULE_NAME, "", "apipassword.php", "apipassword", "mod", "Set your API password");
+	$chatBot->registerInstance($MODULE_NAME, 'API', new API);
 ?>
