5bb097223ce9edf7da7f15432306e7adf7aec368
Tyrence <bigwheels16@gmail.com>
2012-01-08 06:56:15 +0000

    fix for !flip and !roll

--- modules/HELPBOT_MODULE/roll.php
+++ modules/HELPBOT_MODULE/roll.php
@@ -1,37 +1,8 @@
 <?php
-   /*
-   ** Author: Derroylo (RK2)
-   ** Description: Does a random flip or a roll
-   ** Version: 0.1
-   **
-   ** Developed for: Budabot(http://sourceforge.net/projects/budabot)
-   **
-   ** Date(created): 15.01.2006
-   ** Date(last modified): 21.11.2006
-   ** 
-   ** Copyright (C) 2006 Carsten Lohmann
-   **
-   ** Licence Infos: 
-   ** This file is part of Budabot.
-   **
-   ** Budabot is free software; you can redistribute it and/or modify
-   ** it under the terms of the GNU General Public License as published by
-   ** the Free Software Foundation; either version 2 of the License, or
-   ** (at your option) any later version.
-   **
-   ** Budabot is distributed in the hope that it will be useful,
-   ** but WITHOUT ANY WARRANTY; without even the implied warranty of
-   ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   ** GNU General Public License for more details.
-   **
-   ** You should have received a copy of the GNU General Public License
-   ** along with Budabot; if not, write to the Free Software
-   ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-   */
 
 if (preg_match("/^flip$/i", $message)) {
 	$row = $db->queryRow("SELECT * FROM roll WHERE `name` = ? AND `time` >= ? LIMIT 1", $sender, time() - 30);
-	if ($row !== null) {
+	if ($row === null) {
 	  	$flip = rand(1, 2);
 		$db->exec("INSERT INTO roll (`time`, `name`, `type`, `result`) VALUES (?, ?, ?, ?)", time(), $sender, 0, $flip);
 		$ver_num = $db->lastInsertId();
@@ -50,7 +21,7 @@ if (preg_match("/^flip$/i", $message)) {
 		$msg = "The maximum number that the roll number can be is <highlight>".getrandmax()."<end>";
 	} else {
 		$row = $db->queryRow("SELECT * FROM roll WHERE `name` = ? AND `time` >= ? LIMIT 1", $sender, time() - 30);
-		if ($row !== null) {
+		if ($row === null) {
 		  	$num = rand(1, $arr[1]);
 			$db->exec("INSERT INTO roll (`time`, `name`, `type`, `start`, `end`, `result`) VALUES (?, ?, ?, ?, ?, ?)", time(), $sender, 1, 1, $arr[1], $num);
 		  	$ver_num = $db->lastInsertId();
@@ -68,7 +39,7 @@ if (preg_match("/^flip$/i", $message)) {
 		$msg = "The first number can't be higher than or equal to the second number.";
 	} else {
 		$row = $db->queryRow("SELECT * FROM roll WHERE `name` = ? AND `time` >= ? LIMIT 1", $sender, time() - 30);
-		if ($row !== null) {
+		if ($row === null) {
 		  	$num = rand($arr[1], $arr[2]);
 			$db->exec("INSERT INTO roll (`time`, `name`, `type`, `start`, `end`, `result`) VALUES (?, ?, ?, ?, ?, ?)", time(), $sender, 1, $arr[1], $arr[2], $num);
 			$ver_num = $db->lastInsertId();
