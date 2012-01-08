cf56e487ac2483605d65166cfe231f6e2664140f
MDK <mdk00420@gmail.com>
2012-01-08 10:20:50 +0000

    The correct test command for buffitems

--- modules/HELPBOT_MODULE/buffs2.php
+++ modules/HELPBOT_MODULE/buffs2.php
@@ -1,60 +1,60 @@
 <?php
 
+// Makes main selection screen
 if (preg_match("/^bufftest$/i", $message, $arr)) {
-	$blob = "<header> :::::: Buff item ability selection :::::: <end>\n\n";
-	$blob .= Text::make_chatcmd("Agility", "/tell buffitems agility") " - ";
-	$blob .= Text::make_chatcmd("Intelligence", "/tell buffitems Intelligence") " - ";
-	$blob .= Text::make_chatcmd("Psychic", "/tell buffitems Psychic") " - ";
-	$blob .= Text::make_chatcmd("Sense", "/tell buffitems Sense") " - ";
-	$blob .= Text::make_chatcmd("Stamina", "/tell buffitems Stamina") " - ";
-	$blob .= Text::make_chatcmd("Strength", "/tell buffitems Strength") " - ";
-	$chatBot->send($blob, $sendto);
+	$blob = "<header> :::::: Buff item skill selection :::::: <end>\n\n";
+	$blob .= "<highlight>Ability Listings:<end>\n";
+	$blob .= Text::make_chatcmd("Agility", "/tell <myname> buffitems Agility") . " - ";
+	$blob .= Text::make_chatcmd("Intelligence", "/tell <myname> buffitems Intelligence") . " - ";
+	$blob .= Text::make_chatcmd("Psychic", "/tell <myname> buffitems Psychic") . " - ";
+	$blob .= Text::make_chatcmd("Sense", "/tell <myname> buffitems Sense") . " - ";
+	$blob .= Text::make_chatcmd("Stamina", "/tell <myname> buffitems Stamina") . " - ";
+	$blob .= Text::make_chatcmd("Strength", "/tell <myname> buffitems Strength");
+	// Add additional skills over time
+	$blob .= "\n\n<highlight>Skill Listings:<end>\n";
+	$blob .= Text::make_chatcmd("Treatment", "/tell <myname> buffitems Treatment");
 
-if (preg_match("/^buffitems (.+)$/i", $message, $arr)) {
-	$name = $arr[1];	
-	//$data = db->query(SELECT * FROM buffitems WHERE level = ?;
-	$results = array();
-	$found = 0;
-	// search item line database
-	forEach ($buffitems as $key => $value) {
-		unset($info);
-		if (matches($key, $name)) {
-			$found++;
-			$info =	make_info($key, $value);
-			array_unshift($results, array($key, $info));
-		}
-	}
-	// search  item alias database
-	forEach ($aliases as $key => $values) {
-		unset($info);
-		if (contains($values, $name) && !(duplicate($key, $results))) {
-			$found++;
-			$buffitem = $buffitems[$key];
-			$alias = get_alias($values, $name);
-			$info =	"Item <green>$alias<end>\nbelongs into the line of ";
-			$info .= make_info($key, $buffitem);
-			array_unshift($results, array($key, $info, $alias));
-		}
-	}
-	
-	if ($found == 0) {
-		$chatBot->send("No matches, sorry.", $sendto);
+	$msg = Text::make_blob("test", $blob);
+	$chatBot->send($msg, $sendto);
+// Creates the list of items
+} else if (preg_match("/^bufftest (.+)$/i", $message, $arr)) {
+	$name = $arr[1];
+	$ability = Util::get_ability($arr[1], true);
+	// Allows only valid searches. Must add additional skills in the get_ability function to bypass check
+	if (!$ability){
+		$syntax_error = true;
 		return;
-	} else {
-		$blob = "<header>::::: Buff item info :::::<end>\n\n";
-		$blob .= "Your query of <yellow>".$name."<end> returned the following item line(s):\n\n";
-		if ($found == 1) {
-			$blob .= $results[0][1]."\n\n";
-		} else {
-			forEach ($results as $result) {
-				$blob .= "- <a href='chatcmd:///tell <myname> <symbol>buffitem ".$result[0]."'>".$result[0]."</a>".
-						   (sizeof($result) == 3 ? " (".$result[2].")" : "")."\n";
-			}
-			$blob .= "\n".sizeof($results)." results found, please pick one by clicking it";
-		}
-		$blob .= "\n\nby Imoutochan, RK1";
-		$msg = Text::make_blob("Buff item search results (<highlight>$found<end>)", $blob);
 	}
+	$data = $db->query("SELECT * FROM buffitems WHERE skill = ? ORDER BY type, buffed DESC", $ability);
+	$blob .=  "<header>:::::: $ability Buffing Items ::::::<end>\n";
+	$weap = 1;
+	$armor = 1;
+	$util = 1;
+	forEach ($data as $row) {
+		//print_r($row);
+		if ($row->type == "weap") {
+			if ($weap == 1) {
+				$blob .= "\n<highlight>Weapon Buff List<end>\n\n";
+				$weap = 0;
+			}
+			$blob .= Text::make_item($row->lowid, $row->highid, $row->minql, $row->name) . " Buff Amount: <highlight>$row->buffed<end> - $row->info\n";
+		}
+		if ($row->type == "armor") {
+			if ($armor == 1) {
+				$blob .= "\n<highlight>Armor Buff List<end>\n\n";
+				$armor = 0;
+			}
+			$blob .= Text::make_item($row->lowid, $row->highid, $row->minql, $row->name) . " Buff Amount: <highlight>$row->buffed<end> - $row->info\n";
+		}
+		if ($row->type == "util") {
+			if ($util == 1) {
+				$blob .= "\n<highlight>Utility Buff List<end>\n\n";
+				$util = 0;
+			}
+			$blob .= Text::make_item($row->lowid, $row->highid, $row->minql, $row->name) . " Buff Amount: <highlight>$row->buffed<end> - $row->info\n";
+		}
+	}
+	$msg = Text::make_blob("Buff Item list ($ability)", $blob);
 	$chatBot->send($msg, $sendto);
 } else {
 	$syntax_error = true;
