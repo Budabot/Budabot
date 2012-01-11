30ad1f2e24b38335121981b8e5b811951f660a75
Tyrence <bigwheels16@gmail.com>
2012-01-11 19:15:31 +0000

    removed logging from config template since that is now handled by ./conf/log4php.xml

--- conf/config.template.php
+++ conf/config.template.php
@@ -22,22 +22,6 @@ $vars['DB Host'] = "./data/";		// Hostname or file location
 $vars['DB username'] = "";		// MySQL username
 $vars['DB password'] = "";		// MySQL password
 
-// Logging options. 1 for enabled, 0 for disabled.
-$vars['error_console'] = 1;
-$vars['error_file'] = 1;
-
-$vars['info_console'] = 1;
-$vars['info_file'] = 1;
-
-$vars['query_console'] = 0;
-$vars['query_file'] = 0;
-
-$vars['debug_console'] = 0;
-$vars['debug_file'] = 0;
-
-$vars['chat_console'] = 1;
-$vars['chat_file'] = 1;
-
 // Show aoml markup in logs/console. 1 for enabled, 0 for disabled.
 $vars['show_aoml_markup'] = 0;
 
