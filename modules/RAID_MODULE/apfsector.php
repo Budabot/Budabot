61ce052a77a2cf6c56fd4b3517a478570e8126ee
Tyrence <bigwheels16@gmail.com>
2012-01-15 01:10:00 +0000

    removed unneeded code from RAID_MODULE (points, raidlist, etc)

--- modules/RAID_MODULE/apfsector.php
+++ modules/RAID_MODULE/apfsector.php
@@ -5,10 +5,9 @@ if (preg_match("/^(13|28|35)$/i", $message, $arr)) {
 	$sector = $arr[1];
 
 	// adding apf stuff
-	if (Raid::add_raid_to_loot_list('APF', "Sector $sector")) {
-		$msg = "Sector $sector loot table was added to the loot list.";
-		$chatBot->send($msg, 'priv');
-	}
+	Raid::add_raid_to_loot_list('APF', "Sector $sector");
+	$msg = "Sector $sector loot table was added to the loot list.";
+	$chatBot->send($msg, 'priv');
 
 	$msg = Raid::get_current_loot_list();
 	$chatBot->send($msg, 'priv');
--- modules/RAID_MODULE/rem.php
+++ modules/RAID_MODULE/rem.php
@@ -1,50 +1,9 @@
 <?php
-   /*
-   ** Author: Derroylo (RK2)
-   ** Description: Lets a player remove from his choosen loot
-   ** Version: 0.1
-   **
-   ** Developed for: Budabot(http://sourceforge.net/projects/budabot)
-   **
-   ** Date(created): 10.10.2006
-   ** Date(last modified): 11.10.2006
-   ** 
-   ** Copyright (C) 2006 Carsten Lohmann
-   **
-   ** Licence Infos: 
-   ** This file is part of Budabot.
-   **
-   ** Budabot is free software; you can redistribute it and/or modify
-   ** it under the terms of the GNU General Public License as published by
-   ** the Free Software Foundation; either version 2 of the License, or
-   ** (at your option) any later version.
-   **
-   ** Budabot is distributed in the hope that it will be useful,
-   ** but WITHOUT ANY WARRANTY; without even the implied warranty of
-   ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   ** GNU General Public License for more details.
-   **
-   ** You should have received a copy of the GNU General Public License
-   ** along with Budabot; if not, write to the Free Software
-   ** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-   */
 
 global $loot;
-global $raidloot;
 
 if (preg_match("/^rem$/i", $message)) {
-	if ($chatBot->vars["raid_status"] != "" && $chatBot->vars["raid_pts"] == 0) {
-	  	forEach ($raidloot as $key => $value) {
-			forEach ($value as $key1 => $value1) {
-				if ($raidloot[$key][$key1]["users"][$sender] == true) {
-					unset($raidloot[$key][$key1]["users"][$sender]);
-				}
-			}
-		}
-	
-		$msg = "$sender has been removed from all rolls.";
-	  	$chatBot->send($msg, 'priv');
-	} else if (count($loot) > 0) {
+	if (count($loot) > 0) {
 	  	forEach ($loot as $key => $item) {
 			if ($loot[$key]["users"][$sender] == true) {
 				unset($loot[$key]["users"][$sender]);
