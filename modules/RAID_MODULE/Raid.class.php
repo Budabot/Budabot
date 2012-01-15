62ee0a004903370178128f656355e82b105a6110
Tyrence <bigwheels16@gmail.com>
2012-01-15 05:32:15 +0000

    removed unneeded code from RAID_MODULE (points, raidlist, etc)

--- modules/RAID_MODULE/Raid.class.php
+++ modules/RAID_MODULE/Raid.class.php
@@ -4,89 +4,51 @@ class Raid {
 	public static function get_current_loot_list() {
 		$chatBot = Registry::getInstance('chatBot');
 		global $loot;
-		global $raidloot;
 	
-		if ($chatBot->vars["raid_status"] == "") {
-			if (is_array($loot)) {
-				$list = "<header>::::: Loot List :::::<end>\n\nUse <symbol>flatroll to roll.\n\n";
-				forEach ($loot as $key => $item) {
-					$add = Text::make_chatcmd("Add", "/tell <myname> add $key");
-					$rem = Text::make_chatcmd("Remove", "/tell <myname> rem");
-					$added_players = count($item["users"]);
-		
-					$list .= "<u>Slot #<font color='#FF00AA'>$key</font></u>\n";
-					if ($item["icon"] != "") {
-						$list .= "<img src=rdb://{$item["icon"]}>\n";
-					}
+		if (is_array($loot)) {
+			$list = "<header>::::: Loot List :::::<end>\n\nUse <symbol>flatroll to roll.\n\n";
+			forEach ($loot as $key => $item) {
+				$add = Text::make_chatcmd("Add", "/tell <myname> add $key");
+				$rem = Text::make_chatcmd("Remove", "/tell <myname> rem");
+				$added_players = count($item["users"]);
+	
+				$list .= "<u>Slot #<font color='#FF00AA'>$key</font></u>\n";
+				if ($item["icon"] != "") {
+					$list .= "<img src=rdb://{$item["icon"]}>\n";
+				}
 
-					if ($item["multiloot"] > 1) {
-						$ml = " <yellow>(x".$item["multiloot"].")<end>";
-					} else {
-						$ml = "";
-					}
-					
-					if ($item["linky"]) {
-						$itmnm = $item["linky"];
-					} else {
-						$itmnm = $item["name"];
-					}
-		
-					$list .= "Item: <orange>$itmnm<end>".$ml."\n";
-					if ($item["minlvl"] != "") {
-						$list .= "MinLvl set to <highlight>{$item["minlvl"]}<end>\n";
-					}
-									
-					$list .= "<highlight>$added_players<end> Total ($add/$rem)\n";
-					$list .= "Players added:";
-					if (count($item["users"]) > 0) {
-						forEach ($item["users"] as $key => $value) {
-							$list .= " [<yellow>$key<end>]";
-						}
-					} else {
-						$list .= " None added yet.";
-					}
-					
-					$list .= "\n\n";
+				if ($item["multiloot"] > 1) {
+					$ml = " <yellow>(x".$item["multiloot"].")<end>";
+				} else {
+					$ml = "";
 				}
-				$msg = Text::make_blob("Loot List", $list);
-			} else {
-				$msg = "No List exists yet.";
-			}
-		} else if ($chatBot->vars["raid_status"] != "" && $chatBot->vars["raid_loot_pts"] == 0) {
-			if (is_array($raidloot)) {
-				$list = "<header>::::: Raidloot List :::::<end>\n\n";
-				forEach ($raidloot as $key => $item) {
-					$add = Text::make_chatcmd("Add", "/tell <myname> add $key");
-					$rem = Text::make_chatcmd("Remove", "/tell <myname> rem");
-					$added_players = count($item["users"]);
-		
-					$list .= "<u>Slot #$key</u>\n";
-					if ($item["icon"] != "") {
-						$list .= "<img src=rdb://{$item["icon"]}>\n";
-					}
-		
-					$list .= "Item: <highlight>{$item["name"]}<end>\n";
-					if ($item["minlvl"] != "") {
-						$list .= "MinLvl set to <highlight>{$item["minlvl"]}<end>\n";
-					}
-					$list .= "<highlight>$added_players<end> Total ($add/$rem)\n";
-					$list .= "Players added:";
-					if (count($item["users"]) > 0) {
-						forEach ($item["users"] as $key => $value) {
-							$list .= " [<highlight>$key<end>]";
-						}
-					} else {
-						$list .= " None added yet.";
-					}
-					
-					$list .= "\n\n";
+				
+				if ($item["linky"]) {
+					$itmnm = $item["linky"];
+				} else {
+					$itmnm = $item["name"];
 				}
-				$msg = Text::make_blob("Raidloot List", $list);
-			} else {
-				$msg = "No List exists yet.";
+	
+				$list .= "Item: <orange>$itmnm<end>".$ml."\n";
+				if ($item["minlvl"] != "") {
+					$list .= "MinLvl set to <highlight>{$item["minlvl"]}<end>\n";
+				}
+								
+				$list .= "<highlight>$added_players<end> Total ($add/$rem)\n";
+				$list .= "Players added:";
+				if (count($item["users"]) > 0) {
+					forEach ($item["users"] as $key => $value) {
+						$list .= " [<yellow>$key<end>]";
+					}
+				} else {
+					$list .= " None added yet.";
+				}
+				
+				$list .= "\n\n";
 			}
+			$msg = Text::make_blob("Loot List", $list);
 		} else {
-			$msg = "No list available!";
+			$msg = "No List exists yet.";
 		}
 		
 		return $msg;
