f9e5a2d32a848f9441d820e875b0219e996dafd5
Tyrence <bigwheels16@gmail.com>
2012-01-16 05:30:49 +0000

    fix for mysql when calling query() for a query that returned no result set

--- core/BAN/setup.php
+++ core/BAN/setup.php
@@ -1,6 +1,6 @@
 <?php
 
-$db->query("CREATE TABLE IF NOT EXISTS banlist_<myname> (name VARCHAR(25) NOT NULL PRIMARY KEY, admin VARCHAR(25), time INT, reason TEXT, banend INT)");
+$db->exec("CREATE TABLE IF NOT EXISTS banlist_<myname> (name VARCHAR(25) NOT NULL PRIMARY KEY, admin VARCHAR(25), time INT, reason TEXT, banend INT)");
 
 $ban = Registry::getInstance('ban');
 $ban->upload_banlist();
--- modules/VOTE_MODULE/setup.php
+++ modules/VOTE_MODULE/setup.php
@@ -1,6 +1,6 @@
 <?php
 
-$db->query("CREATE TABLE IF NOT EXISTS vote_<myname> (`question` TEXT(500), `author` TEXT (80), `started` INT (10), `duration` INT (10), `answer` TEXT(500), `status` INT (1))");
+$db->exec("CREATE TABLE IF NOT EXISTS vote_<myname> (`question` TEXT(500), `author` TEXT (80), `started` INT (10), `duration` INT (10), `answer` TEXT(500), `status` INT (1))");
 
 if (!isset($chatBot->data["Vote"])) {
 	// Upload to memory votes that are still running
