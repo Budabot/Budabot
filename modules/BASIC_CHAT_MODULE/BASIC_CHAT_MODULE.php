6c0bffb3f95eb158bc623dcd3944ff9b60ed8b0a
Tyrence <bigwheels16@gmail.com>
2012-01-18 18:12:39 +0000

    added !rally

--- modules/BASIC_CHAT_MODULE/BASIC_CHAT_MODULE.php
+++ modules/BASIC_CHAT_MODULE/BASIC_CHAT_MODULE.php
@@ -1,6 +1,10 @@
 <?php
+	require_once 'Rally.class.php';
+	
+	$chatBot->registerInstance($MODULE_NAME, 'Rally', new Rally);
+
 	// Check macros
-	$command->register($MODULE_NAME, "", "check.php", "check", "rl", "Checks who of the raidgroup is in the area");
+	$command->register($MODULE_NAME, "", "check.php", "check", "all", "Checks who of the raidgroup is in the area");
 
 	// Topic set/show
 	$event->register($MODULE_NAME, "joinPriv", "topic_logon.php", "Show topic when someone joins the private channel");
@@ -11,7 +15,7 @@
 	$setting->add($MODULE_NAME, "topic_setby", "Character who set the topic", "noedit", "text", '');
 	$setting->add($MODULE_NAME, "topic_time", "Time the topic was set", "noedit", "text", '');
 
-	// Leader
+	// Leader/Leader echo
 	$command->register($MODULE_NAME, "priv", "leader.php", "leader", "all", "Sets the Leader of the raid");
 	$subcommand->register($MODULE_NAME, "priv", "leader_set.php", "leader (.+)", "rl", "leader", "Set a specific Leader");
 	$command->register($MODULE_NAME, "", "leaderecho_cmd.php", "leaderecho", "rl", "Set if the text of the leader will be repeated", 'leader');
@@ -24,6 +28,8 @@
 	$command->register($MODULE_NAME, "", "assist.php", "assist", "all", "Shows an Assist macro");
 	$commandAlias->register($MODULE_NAME, "assist", "callers");
 	$subcommand->register($MODULE_NAME, "", "assist_set.php", "assist (.+)", "rl", "assist", "Set a new assist");
+	
+	// Heal Assist
 	$command->register($MODULE_NAME, "", "healassist.php", "heal", "all", "Creates/showes an Doc Assist macro");
 	$subcommand->register($MODULE_NAME, "", "healassist_set.php", "heal (.+)", "rl", "heal", "Set a new Doc assist");
 	$commandAlias->register($MODULE_NAME, "heal", "healassist");
@@ -40,4 +46,5 @@
 	$help->register($MODULE_NAME, "tell", "tell.txt", "rl", "How to use tell");
 	$help->register($MODULE_NAME, "topic", "topic.txt", "all", "Set the Topic of the raid");
 	$help->register($MODULE_NAME, "cmd", "cmd.txt", "rl", "How to use cmd");
+	$help->register($MODULE_NAME, "rally", "rally.txt", "rl", "How to set or view the rally");
 ?>
