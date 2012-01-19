b9460635330e32a0c40aa05422d8500f0a97e371
Tyrence <bigwheels16@gmail.com>
2012-01-19 07:55:48 +0000

    removed !adminnews and !privnews

--- modules/NEWS_MODULE/NEWS_MODULE.php
+++ modules/NEWS_MODULE/NEWS_MODULE.php
@@ -6,11 +6,6 @@
 	$db->add_table_replace('#__news', 'news');
 	$db->loadSQLFile($MODULE_NAME, 'news');
 
-	// Set admin and user news
-	$command->register($MODULE_NAME, "", "set_news.php", "privnews", "mod", "Set news shown on private channel join");
-	$command->register($MODULE_NAME, "", "set_news.php", "adminnews", "mod", "Set news shown to admins on private channel join", 'privnews');
-
 	// Help files
 	$help->register($MODULE_NAME, "news", "news.txt", "guild", "How to use news");
-	$help->register($MODULE_NAME, "privnews", "privnews.txt", "mod", "How to set private channel and admin news entries");
 ?>
