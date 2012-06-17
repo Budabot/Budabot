5f400b01b9525712abd8afbdc0bbca320a047194
Patrick Godschalk <patrick.godschalk@me.com>
2012-06-17 16:12:37 +0000

    Vhab joins the fun!

--- modules/FUN_MODULE/Fun.class.php
+++ modules/FUN_MODULE/Fun.class.php
@@ -47,6 +47,7 @@ class Fun {
     $fc[] = "alot of players of video games dont see the big picture... heheh -- ShadowGod";
     $fc[] = "you are a poor liar. -- Anarrina";
     $fc[] = "\"ShadowGod, Anarrina, the !fc command needed a new batch, thanks =)\" --Argure ~ \"yeah.. i'm done with this now --ShadowGod\" You were banned from #anarchyonline by ShadowGod (find other people to troll)";
+    $fc[] = "Argure: after your insensitive jackass stunt last night in #ao you're no longer welcome in my channel at least. 4chan may be more suited for your childish behavior. --Vhab";
 		
 		$dmg = rand(100,999);
 		$cred = rand(10000,9999999);
