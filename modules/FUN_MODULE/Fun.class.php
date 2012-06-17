6ecd1850ce958dae47ac27e8e7d235aaed4c47bb
Patrick Godschalk <patrick.godschalk@me.com>
2012-06-17 04:44:24 +0000

    Moar annanananr

--- modules/FUN_MODULE/Fun.class.php
+++ modules/FUN_MODULE/Fun.class.php
@@ -38,7 +38,14 @@ class Fun {
 		$fc[] = "This sort of non-constructive trolling is not necessary to begin a thread. Should you have an issue with a particular policy, it is within forum rules to state so in a more polite manner. I have edited the objectional content in the post, you may edit it to include a calmer and more constructive criticism. --Anarrina";
     $fc[] = "they're generated, but not in a way you guys can make sense of em :) -- Vhab [talking about how FC calculates values in AO]";
     $fc[] = "Last edited by Anarrina; Today at 18:32:45.. Reason: constantly mistyping someone else's name in an attempt to belittle them in harassment";
-     $fc[] = "\"I'm sorry my reality doesn't match the arguments you'd love to see in your hypothetical situation --Vhab\" ~ \"apology accepted. :) --Argure\"";
+    $fc[] = "\"I'm sorry my reality doesn't match the arguments you'd love to see in your hypothetical situation --Vhab\" ~ \"apology accepted. :) --Argure\"";
+    $fc[] = "no, it doesn\'t, Argure.  For someone espousing logic you are lacking some, there. -- Anarrina";
+    $fc[] = "god, Argure, honestly.  If you're so confident you can manage teh business, please apply for a damned job. -- Anarrina";
+    $fc[] = "you are not at all impressing anyone with your \"logic and reason\" which comes across much more as \"adolescent petulance\" -- Anarrina";
+    $fc[] = "then maybe you need to read the AO forums, specifically the MP forums.. and gain some education about my \"bias\"... -- ShadowGod";
+    $fc[] = "oh for god's sake. grow up -- Anarrina";
+    $fc[] = "alot of players of video games dont see the big picture... heheh -- ShadowGod";
+    $fc[] = "you are a poor liar. -- Anarrina";
 		
 		$dmg = rand(100,999);
 		$cred = rand(10000,9999999);
