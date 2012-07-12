6141779b5e0ab5fd2c36bf3bc2bea88550a52d01
Marvin Vek <laen@onedot.nl>
2012-07-12 22:20:01 +0000

    IRC_MODULE: Don't filter the command, but the blob
    The filtering was done the wrong way around, which was making the bot
    look as if it was talking by itself, and still resulting in an IRC
    excess flood.

--- modules/IRC_MODULE/relay_irc_out.php
+++ modules/IRC_MODULE/relay_irc_out.php
@@ -9,20 +9,16 @@
 
 global $ircSocket;
 if (IRC::isConnectionActive($ircSocket)) {
-	if ($args[2][0] != $setting->get("symbol")) {
+	$pattern = '/<a href="itemref:\/\/(\d+)\/(\d+)\/(\d+)">([^<]+)<\/a>/';
+	$replace = chr(3) . chr(3) . '\4' . chr(3) . ' ' . chr(3) . '(http://auno.org/ao/db.php?id=\1&id2=\2&ql=\3)' . chr(3) . chr(3);
 
-		$pattern = '/<a href="itemref:\/\/(\d+)\/(\d+)\/(\d+)">([^<]+)<\/a>/';
-		$replace = chr(3) . chr(3) . '\4' . chr(3) . ' ' . chr(3) . '(http://auno.org/ao/db.php?id=\1&id2=\2&ql=\3)' . chr(3) . chr(3);
+	$msg = strip_tags(htmlspecialchars_decode(preg_replace($pattern, $replace, $message)));
 
-		$msg = htmlspecialchars_decode(preg_replace($pattern, $replace, $message));
-		//$msg = htmlspecialchars_decode(preg_replace($patterns, $replaces, $message), ENT_QUOTES);
-
-		if (Util::isValidSender($sender)) {
-			$msg = "$sender: $msg";
-		}
-		IRC::send($ircSocket, $setting->get('irc_channel'), encodeGuildMessage(getGuildAbbreviation(), $msg));
-		LegacyLogger::log_chat("Out. IRC Msg.", $sender, $msg);
+	if (Util::isValidSender($sender)) {
+		$msg = "$sender: $msg";
 	}
+	IRC::send($ircSocket, $setting->get('irc_channel'), encodeGuildMessage(getGuildAbbreviation(), $msg));
+	LegacyLogger::log_chat("Out. IRC Msg.", $sender, $msg);
 }
 
 ?>
