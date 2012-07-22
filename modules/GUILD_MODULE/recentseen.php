fcbb6a1961f28c814da615242dac07a749497b70
JungleGeorge <JungleGeorge43@gmail.com>
2012-07-22 19:51:03 +0000

    recentseen.php updated to handle alts better Item DB updated to 18.05.01

--- modules/GUILD_MODULE/recentseen.php
+++ modules/GUILD_MODULE/recentseen.php
@@ -1,7 +1,7 @@
 <?php
    /*
-   ** Author: Junglegeorge (RK2) basesd in inacticemem by Legendadv (RK2)
-   ** Description: Lookup recent org members
+   ** Author: Junglegeorge (RK2) basesd on inactivemem by Legendadv (RK2)
+   ** Description: List org meembers who recently logged on
    */
 
 if (preg_match("/^recentseen ([a-z0-9]+)/i", $message, $arr)) {
@@ -21,7 +21,8 @@ if (preg_match("/^recentseen ([a-z0-9]+)/i", $message, $arr)) {
 	$timeString = Util::unixtime_to_readable($time, false);
 	$time = time() - $time;
 
-	$data = $db->query("SELECT * FROM org_members_<myname> o LEFT JOIN alts a ON o.name = a.alt WHERE `mode` != 'del' AND `logged_off` > ?  ORDER BY o.name", $time);
+	$data = $db->query("SELECT case when a.main is null then o.name else a.main end as main ,o.logged_off,o.name FROM org_members_<myname> o LEFT JOIN alts a ON o.name = a.alt WHERE `mode` != 'del'AND `logged_off` > ? ORDER BY 1, o.logged_off desc, o.name", $time); 
+
 
 	if (count($data) == 0) {
 	    $sendto->reply("No members recorded.");
@@ -30,42 +31,29 @@ if (preg_match("/^recentseen ([a-z0-9]+)/i", $message, $arr)) {
 
 	$numinactive = 0;
 	$highlight = 0;
-
+  
 	$blob = "Org members who have logged on since <highlight>{$timeString}<end>.\n\n";
 	
-
+  $prevtoon = '';
 	forEach ($data as $row) {
-		$logged = 0;
-		$main = $row->main;
-		if ($row->main != "") {
-			$data1 = $db->query("SELECT * FROM alts a JOIN org_members_<myname> o ON a.alt = o.name WHERE `main` = ?", $row->main);
-			forEach ($data1 as $row1) {
-				if ($row1->logged_off > $time) {
-					continue 2;
-				}
+		if ($row->main != $prevtoon) {
+      $prevtoon = $row->main;
+		  $numrecentcount++;
+		  $alts = Text::make_chatcmd("Alts", "/tell <myname> alts {$row->main}");
+      $logged = $row->logged_off;
+		  $lasttoon = $row->name;
 
-				if ($row1->logged_off > $logged) {
-					$logged = $row1->logged_off;
-					$lasttoon = $row1->name;
-				}
-			}
-		}
-
-		$numinactive++;
-		$alts = Text::make_chatcmd("Alts", "/tell <myname> alts {$row->name}");
-		$logged = $row->logged_off;
-		$lasttoon = $row->name;
-
-		$player = $row->name."; Main: $main; [{$alts}]\nLast seen on [$lasttoon] on ".date(Util::DATETIME, $logged)."\n\n";
-		if ($highlight == 1) {
-			$blob .= "<highlight>$player<end>";
-			$highlight = 0;
-		} else {
-			$blob .= $player;
-			$highlight = 1;
-		}
+		  $player = $row->main." [{$alts}]\nLast seen as [$lasttoon] on ".date(Util::DATETIME, $logged)."\n\n";
+		  if ($highlight == 1) {
+			 $blob .= "<highlight>$player<end>";
+			 $highlight = 0;
+		  } else {
+			 $blob .= $player;
+			 $highlight = 1;
+		  }
+    } 
 	}
-	$msg = Text::make_blob("$numinactive Recent seen Org Members", $blob);
+	$msg = Text::make_blob("$numrecentcount Recently seen Org Members", $blob);
 	$sendto->reply($msg);
 } else {
 	$syntax_error = true;
