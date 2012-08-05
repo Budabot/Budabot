874cfc7a2ab50cbc655525833983cd4fabdf1fdf
Marebone <joyer83@gmail.com>
2012-08-05 20:50:14 +0000

    Issue #139: Converted check command to new format.

--- modules/BASIC_CHAT_MODULE/BASIC_CHAT_MODULE.php
+++ modules/BASIC_CHAT_MODULE/BASIC_CHAT_MODULE.php
@@ -4,15 +4,14 @@
 	require_once 'ChatAssistController.class.php';
 	require_once 'ChatTopicController.class.php';
 	require_once 'ChatLeaderController.class.php';
+	require_once 'ChatCheckController.class.php';
 
 	$chatBot->registerInstance($MODULE_NAME, 'ChatRallyController', new ChatRallyController);
 	$chatBot->registerInstance($MODULE_NAME, 'ChatSayController', new ChatSayController);
 	$chatBot->registerInstance($MODULE_NAME, 'ChatAssistController', new ChatAssistController);
 	$chatBot->registerInstance($MODULE_NAME, 'ChatTopicController', new ChatTopicController);
 	$chatBot->registerInstance($MODULE_NAME, 'ChatLeaderController', new ChatLeaderController);
-
-	// Check macros
-	$command->register($MODULE_NAME, "", "check.php", "check", "all", "Checks who of the raidgroup is in the area", "check.txt");
+	$chatBot->registerInstance($MODULE_NAME, 'ChatCheckController', new ChatCheckController);
 
 	// Tell
 	$command->register($MODULE_NAME, "", "tell.php", "tell", "rl", "Repeats a message 3 times", "tell.txt");
---
+++ modules/BASIC_CHAT_MODULE/ChatCheckController.class.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * Commands this class contains:
+ *	@DefineCommand(
+ *		command     = 'check',
+ *		accessLevel = 'all',
+ *		description = 'Checks who of the raidgroup is in the area',
+ *      help        = 'check.txt'
+ *	)
+ */
+class ChatCheckController {
+
+	/** @Inject */
+	public $db;
+
+	/** @Inject */
+	public $text;
+
+	const CHANNEL_TYPE = "priv";
+
+	/**
+	 * This command handler checks who of the raidgroup is in the area.
+	 * @HandlesCommand("check")
+	 * @Matches("/^check$/i")
+	 * @Matches("/^check all$/i")
+	 */
+	public function checkAllCommand($message, $channel, $sender, $sendto, $args) {
+		$list = '';
+		$data = $this->db->query("SELECT name FROM online WHERE added_by = '<myname>' AND channel_type = ?", self::CHANNEL_TYPE);
+		forEach ($data as $row) {
+			$content .= " \\n /assist $row->name";
+		}
+
+		$list .= "<a href='chatcmd:///text AssistAll: $content'>Click here to check who is here</a>";
+		$msg = $this->text->make_blob("Check on all", $list);
+		$sendto->reply($msg);
+	}
+
+	/**
+	 * This command handler checks by profession of who of the raidgroup is in the area.
+	 * @HandlesCommand("check")
+	 * @Matches("/^check prof$/i")
+	 */
+	public function checkProfCommand($message, $channel, $sender, $sendto, $args) {
+		$list = '';
+		$data = $this->db->query("SELECT o.name, p.profession
+			FROM online o LEFT JOIN players p ON (o.name = o.name AND p.dimension = '<dim>')
+			WHERE added_by = '<myname>' AND channel_type = ? ORDER BY `profession` DESC", self::CHANNEL_TYPE);
+		forEach ($data as $row) {
+			$prof[$row->profession] .= " \\n /assist $row->name";
+		}
+
+		ksort($prof);
+
+		forEach ($prof as $key => $value) {
+			$list .= "<a href='chatcmd:///text Assist $key: $value'>Click here to check $key</a>\n";
+		}
+
+		$msg = $this->text->make_blob("Check by profession", $list);
+		$sendto->reply($msg);
+	}
+
+	/**
+	 * This command handler checks by organization of who of the raidgroup is in the area.
+	 * @HandlesCommand("check")
+	 * @Matches("/^check org$/i")
+	 */
+	public function checkOrgCommand($message, $channel, $sender, $sendto, $args) {
+		$list = '';
+		$data = $this->db->query("SELECT o.name, p.guild FROM online o LEFT JOIN
+			players p ON (o.name = p.name AND p.dimension = '<dim>')
+			WHERE added_by = '<myname>' AND channel_type = ? ORDER BY `guild` DESC", self::CHANNEL_TYPE);
+		forEach ($data as $row) {
+			if ($row->guild == "") {
+				$org["Non orged"] .= " \\n /assist $row->name";
+			} else {
+				$org[$row->guild] .= " \\n /assist $row->name";
+			}
+		}
+
+		ksort($org);
+
+		forEach ($org as $key => $value) {
+			$list .= "<a href='chatcmd:///text Assist $key: $value'>Click here to check $key</a>\n";
+		}
+
+		$msg = $this->text->make_blob("Check by Organization", $list);
+		$sendto->reply($msg);
+	}
+
+}
