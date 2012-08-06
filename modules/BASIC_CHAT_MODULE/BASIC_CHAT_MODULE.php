784b7d04ae963046397a4d1c00995653e7fb0638
Marebone <joyer83@gmail.com>
2012-08-06 18:26:53 +0000

    Moved cmd and tell commands to ChatSayController class.

--- modules/BASIC_CHAT_MODULE/BASIC_CHAT_MODULE.php
+++ modules/BASIC_CHAT_MODULE/BASIC_CHAT_MODULE.php
@@ -12,8 +12,4 @@
 	$chatBot->registerInstance($MODULE_NAME, 'ChatTopicController', new ChatTopicController);
 	$chatBot->registerInstance($MODULE_NAME, 'ChatLeaderController', new ChatLeaderController);
 	$chatBot->registerInstance($MODULE_NAME, 'ChatCheckController', new ChatCheckController);
-
-	// Tell
-	$command->register($MODULE_NAME, "", "tell.php", "tell", "rl", "Repeats a message 3 times", "tell.txt");
-	$command->register($MODULE_NAME, "", "cmd.php", "cmd", "rl", "Creates a highly visible messaage", "cmd.txt");
 ?>
--- modules/BASIC_CHAT_MODULE/ChatSayController.class.php
+++ modules/BASIC_CHAT_MODULE/ChatSayController.class.php
@@ -1,6 +1,11 @@
 <?php
 
 /**
+ * Authors:
+ *  - Legendadv (RK2)
+ *  - Derroylo (RK2)
+ *  - Marebone (RK2)
+ *
  * The ChatSayController class allows user to send messages to either org
  * channel or to private (guest) channel.
  *
@@ -9,6 +14,18 @@
  *		command     = 'say',
  *		accessLevel = 'all',
  *		description = 'Sends message to org chat or private chat'
+ *	)
+ *	@DefineCommand(
+ *		command     = 'tell',
+ *		accessLevel = 'rl',
+ *		description = 'Repeats a message 3 times',
+ *      help        = 'tell.txt'
+ *	)
+ *	@DefineCommand(
+ *		command     = 'cmd',
+ *		accessLevel = 'rl',
+ *		description = 'Creates a highly visible message',
+ *      help        = 'cmd.txt'
  *	)
  */
 class ChatSayController {
@@ -33,4 +50,40 @@ class ChatSayController {
 	public function sayPrivCommand($message, $channel, $sender, $sendto, $args) {
 		$this->chatBot->sendPrivate("$sender: $args[1]");
 	}
+
+	/**
+	 * This command handler creates a highly visible message.
+	 * @HandlesCommand("cmd")
+	 * @Matches("/^cmd (.+)$/i")
+	 */
+	public function cmdCommand($message, $channel, $sender, $sendto, $args) {
+		$msg = "\n<yellow>---------------------\n<red>$args[1]<end>\n<yellow>---------------------";
+
+		if ($channel == 'msg') {
+			$this->chatBot->sendGuild($msg, true);
+			$this->chatBot->sendPrivate($msg, true);
+		} else {
+			$sendto->reply($msg, true);
+		}
+	}
+
+	/**
+	 * This command handler repeats a message 3 times.
+	 * @HandlesCommand("tell")
+	 * @Matches("/^tell (.+)$/i")
+	 */
+	public function tellCommand($message, $channel, $sender, $sendto, $args) {
+		if ($channel == 'guild' || $channel == 'msg') {
+			$this->chatBot->sendGuild("<yellow>$args[1]<end>", true);
+			$this->chatBot->sendGuild("<yellow>$args[1]<end>", true);
+			$this->chatBot->sendGuild("<yellow>$args[1]<end>", true);
+		}
+
+		if ($channel == 'priv' || $channel == 'msg') {
+			$this->chatBot->sendPrivate("<yellow>$args[1]<end>", true);
+			$this->chatBot->sendPrivate("<yellow>$args[1]<end>", true);
+			$this->chatBot->sendPrivate("<yellow>$args[1]<end>", true);
+		}
+	}
+
 }
