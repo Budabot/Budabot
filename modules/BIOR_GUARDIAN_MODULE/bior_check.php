b3c0e9f1c62b22c75cf8aa28b9a7451dbaabcf3a
Marebone <joyer83@gmail.com>
2012-08-15 16:52:23 +0000

    Fixed event handlers sending info of Bio Regrowth and Guardian casts incorrectly.

--- modules/BIOR_GUARDIAN_MODULE/bior_check.php
+++ modules/BIOR_GUARDIAN_MODULE/bior_check.php
@@ -6,25 +6,25 @@ if (isset($chatBot->data['bior'])) {
 			$rem = $chatBot->data['bior'][$key]["b"] - time();
 			if ($rem >= 319 && $rem <= 321) {
 				$msg = "<blue>20sec remaining on Bio Regrowth.<end>";
-				$sendto->reply($msg);
+				$chatBot->sendPrivate($msg);
 			} else if ($rem >= 305 && $rem <= 307) {
 				$pos = array_search($key, $chatBot->data['blist']);
 				if (isset($chatBot->data['blist'][$pos + 1])) {
 					$next = " <yellow>Next is {$chatBot->data['blist'][$pos + 1]}<end>";
 				}
 				$msg = "<blue>6sec remaining on Bio Regrowth.$next<end>";
-				$sendto->reply($msg);
+				$chatBot->sendPrivate($msg);
 			} else if ($rem >= 299 && $rem <= 301) {
 				$pos = array_search($key, $chatBot->data['blist']);
 				if (isset($chatBot->data['blist'][$pos + 1])) {
 					$next = " <yellow>Next is {$chatBot->data['blist'][$pos + 1]}<end>";
 				}
 				$msg = "<blue>Bio Regrowth has terminated.$next<end>";
-				$sendto->reply($msg);
+				$chatBot->sendPrivate($msg);
 			} else if ($rem <= 0) {
 				$msg = "<blue>Bio Regrowth is ready on $key.<end>";
 				$chatBot->data['bior'][$key]["b"] = "ready";
-				$sendto->reply($msg);
+				$chatBot->sendPrivate($msg);
 			}
 		}
 	}
--- modules/BIOR_GUARDIAN_MODULE/guard_check.php
+++ modules/BIOR_GUARDIAN_MODULE/guard_check.php
@@ -5,23 +5,23 @@ if (isset($chatBot->data['guard'])) {
 			$rem = $chatBot->data['guard'][$key]["g"] - time();
 			if($rem >= 319 && $rem < 321) {
 				$msg = "<blue>20sec remaining on Guardian.<end>";
-				$sendto->reply($msg);
+				$chatBot->sendPrivate($msg);
 			} elseif($rem >= 305 && $rem <= 307) {
 				$pos = array_search($key, $chatBot->data['glist']);
 				if(isset($chatBot->data['glist'][$pos + 1]))
 					$next = " <yellow>Next is {$chatBot->data['glist'][$pos + 1]}<end>";
 				$msg = "<blue>6sec remaining on Guardian.$next<end>";
-				$sendto->reply($msg);
+				$chatBot->sendPrivate($msg);
 			} elseif($rem >= 299 && $rem <= 301) {
 				$pos = array_search($key, $chatBot->data['glist']);
 				if(isset($chatBot->data['glist'][$pos + 1]))
 					$next = " <yellow>Next is {$chatBot->data['glist'][$pos + 1]}<end>";
 				$msg = "<blue>Guardian has terminated.$next<end>";
-				$sendto->reply($msg);
+				$chatBot->sendPrivate($msg);
 			} elseif($rem <= 0) {
 				$msg = "<blue>Guardian is ready on $key.<end>";
 				$chatBot->data['guard'][$key]["g"] = "ready";
-				$sendto->reply($msg);
+				$chatBot->sendPrivate($msg);
 			}
 		}
 	}
