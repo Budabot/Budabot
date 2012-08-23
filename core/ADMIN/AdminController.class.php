ad274559e88b06d6f61d6148dfdaae3434370558
Marebone <joyer83@gmail.com>
2012-08-23 16:48:32 +0000

    Updated ADMIN module to new format.

---
+++ core/ADMIN/AdminController.class.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @Instance
+ */
+class AdminController {
+
+	/**
+	 * Name of the module.
+	 * Set automatically by module loader.
+	 */
+	public $moduleName;
+
+	/** @Inject */
+	public $db;
+
+	/** @Inject */
+	public $commandManager;
+
+	/** @Inject */
+	public $eventManager;
+
+	/** @Inject */
+	public $help;
+
+	/**
+	 * @Setup
+	 * This handler is called on bot startup.
+	 */
+	public function setup() {
+		$this->commandManager->activate("msg", "Admin.addCommand", "addadmin", "admin");
+		$this->commandManager->activate("priv", "Admin.addCommand", "addadmin", "admin");
+		$this->commandManager->activate("guild", "Admin.addCommand", "addadmin", "admin");
+
+		$this->commandManager->activate("msg", "Admin.removeCommand", "remadmin", "superadmin");
+		$this->commandManager->activate("priv", "Admin.removeCommand", "remadmin", "superadmin");
+		$this->commandManager->activate("guild", "Admin.removeCommand", "remadmin", "superadmin");
+
+		$this->commandManager->activate("msg", "Admin.addCommand", "addmod", "admin");
+		$this->commandManager->activate("priv", "Admin.addCommand", "addmod", "admin");
+		$this->commandManager->activate("guild", "Admin.addCommand", "addmod", "admin");
+
+		$this->commandManager->activate("msg", "Admin.removeCommand", "remmod", "admin");
+		$this->commandManager->activate("priv", "Admin.removeCommand", "remmod", "admin");
+		$this->commandManager->activate("guild", "Admin.removeCommand", "remmod", "admin");
+
+		$this->commandManager->activate("msg", "Admin.adminlistCommand", "adminlist", 'all');
+		$this->commandManager->activate("priv", "Admin.adminlistCommand", "adminlist", 'all');
+		$this->commandManager->activate("guild", "Admin.adminlistCommand", "adminlist", 'all');
+
+		$this->eventManager->activate("connect", "Admin.checkAdmins");
+		$this->eventManager->activate("setup", "Admin.uploadAdmins");
+
+		$this->help->register($this->moduleName, "admin", "admin.txt", "mod", "Mod/admin help file");
+	}
+}
