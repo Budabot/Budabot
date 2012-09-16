8aa0bb5c396bdbb9a7ebbb35a20c39b0fd835b14
Marebone <joyer83@gmail.com>
2012-09-16 15:23:43 +0000

    - Fixed Budabot::removeSocketNotifier() not actually removing anything. - Fixed some warnings and notices.

--- core/Budabot.class.php
+++ core/Budabot.class.php
@@ -59,6 +59,10 @@ class Budabot extends AOChat {
 
 		// Set startup time
 		$this->vars["startup"] = time();
+
+		$this->monitoredSocketsByType[SocketNotifier::ACTIVITY_READ] = array();
+		$this->monitoredSocketsByType[SocketNotifier::ACTIVITY_WRITE] = array();
+		$this->monitoredSocketsByType[SocketNotifier::ACTIVITY_ERROR] = array();
 	}
 
 	/**
@@ -111,19 +115,21 @@ class Budabot extends AOChat {
 				$read = $this->monitoredSocketsByType[SocketNotifier::ACTIVITY_READ];
 				$write = $this->monitoredSocketsByType[SocketNotifier::ACTIVITY_WRITE];
 				$except = $this->monitoredSocketsByType[SocketNotifier::ACTIVITY_ERROR];
-				if (0 < stream_select($read, $write, $except, 0)) {
-					foreach ($this->socketNotifiers as $notifier) {
-						$socket = $notifier->getSocket();
-						$type = $notifier->getType();
+				if ($read || $write || $except) {
+					if (0 < stream_select($read, $write, $except, 0)) {
+						foreach ($this->socketNotifiers as $notifier) {
+							$socket = $notifier->getSocket();
+							$type = $notifier->getType();
 
-						if (in_array($socket, $read) && $type & SocketNotifier::ACTIVITY_READ) {
-							$notifier->notify(SocketNotifier::ACTIVITY_READ);
-						}
-						if (in_array($socket, $write) && $type & SocketNotifier::ACTIVITY_WRITE) {
-							$notifier->notify(SocketNotifier::ACTIVITY_WRITE);
-						}
-						if (in_array($socket, $except) && $type & SocketNotifier::ACTIVITY_ERROR) {
-							$notifier->notify(SocketNotifier::ACTIVITY_ERROR);
+							if (in_array($socket, $read) && $type & SocketNotifier::ACTIVITY_READ) {
+								$notifier->notify(SocketNotifier::ACTIVITY_READ);
+							}
+							if (in_array($socket, $write) && $type & SocketNotifier::ACTIVITY_WRITE) {
+								$notifier->notify(SocketNotifier::ACTIVITY_WRITE);
+							}
+							if (in_array($socket, $except) && $type & SocketNotifier::ACTIVITY_ERROR) {
+								$notifier->notify(SocketNotifier::ACTIVITY_ERROR);
+							}
 						}
 					}
 				}
@@ -942,7 +948,7 @@ class Budabot extends AOChat {
 	 * Removes given socket notifier from list of sockets being monitored.
 	 */
 	public function removeSocketNotifier($socketNotifier) {
-		$removeOne = create_function('$array,$value', '$key = array_search($value, $array, true);'
+		$removeOne = create_function('&$array,$value', '$key = array_search($value, $array, true);'
 				   . 'if ($key !== false) {'
 				   . '	unset ($array[$key]);'
 				   . '}');
