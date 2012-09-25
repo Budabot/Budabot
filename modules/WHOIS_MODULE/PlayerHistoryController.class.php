ba59e28c689a430be7daf381fb03727591e6936a
Tyrence <bigwheels16@gmail.com>
2012-09-25 07:30:43 +0000

    converted !history to new 3.0 OO-style controller

---
+++ modules/WHOIS_MODULE/PlayerHistoryController.class.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Authors: 
+ *  - Tyrence (RK2)
+ *
+ * @Instance
+ *
+ * Commands this controller contains:
+ *	@DefineCommand(
+ *		command     = 'history',
+ *		accessLevel = 'all', 
+ *		description = 'Show history of a player', 
+ *		help        = 'history.txt'
+ *	)
+ */
+class PlayerHistoryController {
+
+	/**
+	 * Name of the module.
+	 * Set automatically by module loader.
+	 */
+	public $moduleName;
+
+	/** @Inject */
+	public $text;
+
+	/**
+	 * @HandlesCommand("history")
+	 * @Matches("/^history ([^ ]+) (\d)$/i")
+	 * @Matches("/^history ([^ ]+)$/i")
+	 */
+	public function playerHistoryCommand($message, $channel, $sender, $sendto, $args) {
+		$name = ucfirst(strtolower($args[1]));
+		if (count($args) == 3) {
+			$dimension = $args[2];
+		} else {
+			$dimension = $chatBot->vars['dimension'];
+		}
+
+		$history = new history($name, $dimension);
+		if ($history->errorCode != 0) {
+			$msg = $history->errorInfo;
+		} else {
+			$blob = "Date           Level    AI     Faction      Guild(rank) \n";
+			$blob .= "________________________________________________ \n";
+			forEach ($history->data as $key => $data) {
+				$level = $data["level"];
+
+				if ($data["ailevel"] == "") {
+					$ailevel = "<green>0<end>";
+				} else {
+					$ailevel = "<green>".$data["ailevel"]."<end>";
+				}
+
+				if ($data["faction"] == "Omni") {
+					$faction = "<omni>Omni<end>";
+				} else if ($data["faction"] == "Clan") {
+					$faction = "<clan>Clan<end>";
+				} else {
+					$faction = "<neutral>Neutral<end>";
+				}
+
+				if ($data["guild"] == "") {
+					$guild = "Not in a guild";
+				} else {
+					$guild = $data["guild"]."(".$data["rank"].")";
+				}
+
+				$blob .= "$key |  $level  | $ailevel | $faction | $guild\n";
+			}
+			$msg = $this->text->make_blob("History of $name for RK{$dimension}", $blob);
+		}
+
+		$sendto->reply($msg);
+	}
+}
--- modules/WHOIS_MODULE/WHOIS_MODULE.php
+++ modules/WHOIS_MODULE/WHOIS_MODULE.php
@@ -1,9 +1,12 @@
 <?php
+	require_once 'PlayerHistoryController.class.php';
+	
+	$chatBot->registerInstance($MODULE_NAME, 'PlayerHistoryController', new PlayerHistoryController());
+
 	$db->loadSQLFile($MODULE_NAME, 'name_history');
 
 	$command->register($MODULE_NAME, "", "whois.php", "whois", "all", "Show character info and name history", "whois.txt");
 	$command->register($MODULE_NAME, "", "whois.php", "whoisall", "all", "Show character info and name history for all dimensions", "whois.txt");
-	$command->register($MODULE_NAME, "", "history.php", "history", "all", "Show history of a player", "history.txt");
 	$command->register($MODULE_NAME, "", "namehistory.php", "namehistory", "all", "Show name history of a character", "namehistory.txt");
 	$command->register($MODULE_NAME, "", "lookup.php", "lookup", "all", "Find the uid for a character", "lookup.txt");
 
