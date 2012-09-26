09f849607788f288a84f4e36e5cc4dc422c26800
Tyrence <bigwheels16@gmail.com>
2012-09-26 23:58:55 +0000

    removed SHOPPING_MODULE since the site it used hasn't worked for some time

--- modules/SHOPPING_MODULE/SHOPPING_MODULE.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$command->register($MODULE_NAME, "", "shopping.php", "wtb", "all", "Search for items people want to sell on the shopping channels", "shopping.txt");
-	$command->register($MODULE_NAME, "", "shopping.php", "wts", "all", "Search for items people want to buy on the shopping channels", "shopping.txt");
-
-	$setting->add($MODULE_NAME, 'max_shopping_results', 'Number of Items shown for wtb/wts', 'edit', "number", '20', '10;15;20;25', "", "mod");
-?>
