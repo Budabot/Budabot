c8337ceb7febd485841cef87915b8f02989ac764
Tyrence <bigwheels16@gmail.com>
2012-09-26 17:07:30 +0000

    fix for when checking if irc connection is still alive

--- modules/IRC_MODULE/set_irc_link.php
+++ modules/IRC_MODULE/set_irc_link.php
@@ -1,6 +1,9 @@
 <?php
 
 global $ircSocket;
+
+// make sure eof flag is set
+fputs($ircSocket, "PONG ping\n");
 if ($setting->get('irc_status') == '1' && !IRC::isConnectionActive($ircSocket)) {
 	IRC::connect($ircSocket, $setting->get('irc_nickname'), $setting->get('irc_server'), $setting->get('irc_port'), $setting->get('irc_password'), $setting->get('irc_channel'));
 }
