c77c8731c995f0aa90d7acce15c7a8a54e3a62ba
Tyrence <bigwheels16@gmail.com>
2012-09-27 22:11:00 +0000

    converted !server to new 3.0 OO-style

--- modules/HELPBOT_MODULE/HELPBOT_MODULE.php
+++ modules/HELPBOT_MODULE/HELPBOT_MODULE.php
@@ -4,11 +4,13 @@
 	require_once 'OrgCitiesController.class.php';
 	require_once 'ResearchController.class.php';
 	require_once 'RollController.class.php';
+	require_once 'ServerStatusController.class.php';
 	
 	$chatBot->registerInstance($MODULE_NAME, 'PlayfieldController', new PlayfieldController());
 	$chatBot->registerInstance($MODULE_NAME, 'OrgCitiesController', new OrgCitiesController());
 	$chatBot->registerInstance($MODULE_NAME, 'ResearchController', new ResearchController());
 	$chatBot->registerInstance($MODULE_NAME, 'RollController', new RollController());
+	$chatBot->registerInstance($MODULE_NAME, 'ServerStatusController', new ServerStatusController());
 
 	$db->loadSQLFile($MODULE_NAME, "koslist");
 	$db->loadSQLFile($MODULE_NAME, "dyna");
@@ -23,5 +25,4 @@
 	$command->register($MODULE_NAME, "", "buffitem.php", "buffitem", "all", "Buffitem look up", "buffitem.txt");
 	$command->register($MODULE_NAME, "", "whatbuffs.php", "whatbuffs", "all", "Find items that buff", "whatbuffs.txt");
 	$command->register($MODULE_NAME, "", "dyna.php", "dyna", "all", "Search for RK Dynaboss", "dyna.txt");
-	$command->register($MODULE_NAME, "", "server.php", "server", "all", "Show the Server status", "server.txt");
 ?>
---
+++ modules/HELPBOT_MODULE/ServerStatusController.class.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * Authors: 
+ *	- Tyrence (RK2)
+ *
+ * @Instance
+ *
+ * Commands this controller contains:
+ *	@DefineCommand(
+ *		command     = 'server', 
+ *		accessLevel = 'all', 
+ *		description = 'Show the server status', 
+ *		help        = 'server.txt'
+ *	)
+ */
+class ServerStatusController {
+
+	/**
+	 * Name of the module.
+	 * Set automatically by module loader.
+	 */
+	public $moduleName;
+	
+	/** @Inject */
+	public $chatBot;
+
+	/** @Inject */
+	public $text;
+
+	/**
+	 * @HandlesCommand("server")
+	 * @Matches("/^server$/i")
+	 * @Matches("/^server (.)$/i")
+	 */
+	public function playfieldListCommand($message, $channel, $sender, $sendto, $args) {
+		if (count($args) == 1) {
+			$servernum = $this->chatBot->vars['dimension'];
+		} else {
+			$servernum = $args[1];
+		}
+		
+		// config file uses '4' to indicate test server
+		if ($servernum == '4') {
+			$servernum = 't';
+		}
+		
+		if ($servernum != 1 && $servernum != 2 && $servernum != 't') {
+			return false;
+		}
+
+		$server = new server($servernum);
+		if ($server->errorCode != 0) {
+			$msg = $server->errorInfo;
+		} else {
+			$link = '';
+
+			if ($server->servermanager == 1) {
+				$link .= "<highlight>Servermanager<end> is <green>UP<end>\n";
+			} else {
+				$link .= "<highlight>Servermanager<end> is <red>DOWN<end>\n";
+			}
+
+			if ($server->clientmanager == 1) {
+				$link .= "<highlight>Clientmanager<end> is <green>UP<end>\n";
+			} else {
+				$link .= "<highlight>Clientmanager<end> is <red>DOWN<end>\n";
+			}
+
+			if ($server->chatserver == 1) {
+				$link .= "<highlight>Chatserver<end> is <green>UP<end>\n\n";
+			} else {
+				$link .= "<highlight>Chatserver<end> is <red>DOWN<end>\n\n";
+			}
+
+			$link .= "<highlight>Player distribution in % of total players online.<end>\n";
+			ksort($server->data);
+			forEach ($server->data as $zone => $proz) {
+				$link .= "<highlight>$zone<end>: {$proz["players"]} \n";
+			}
+
+			$msg = $this->text->make_blob("$server->name Server Status", $link);
+		}
+
+		$sendto->reply($msg);
+	}
+}
