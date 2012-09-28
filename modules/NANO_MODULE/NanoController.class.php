5e0ee6d3d1001e7abafbc68a8cb0d4d35b42b3f4
Tyrence <bigwheels16@gmail.com>
2012-09-28 23:14:28 +0000

    converted NANO_MODULE to new 3.0 OO-style issue #154

---
+++ modules/NANO_MODULE/NanoController.class.php
@@ -0,0 +1,363 @@
+<?php
+
+/**
+ * Authors: 
+ *	- Tyrence (RK2)
+ *	- Healnjoo (RK2)
+ *	- Mdkdoc420 (RK2)
+ *
+ * @Instance
+ *
+ * Commands this controller contains:
+ *	@DefineCommand(
+ *		command     = 'nano', 
+ *		accessLevel = 'all', 
+ *		description = 'Searches for a nano and tells you were to get it', 
+ *		help        = 'nano.txt'
+ *	)
+ *	@DefineCommand(
+ *		command     = 'nanolines', 
+ *		accessLevel = 'all', 
+ *		description = 'Shows nanos based on nanoline', 
+ *		help        = 'nanolines.txt'
+ *	)
+ *	@DefineCommand(
+ *		command     = 'nanoloc', 
+ *		accessLevel = 'all', 
+ *		description = 'Browse nanos by location', 
+ *		help        = 'nano.txt'
+ *	)
+ *	@DefineCommand(
+ *		command     = 'fp', 
+ *		accessLevel = 'all', 
+ *		description = 'Shows whether or not a nano is usable in false profession', 
+ *		help        = 'fp.txt'
+ *	)
+ */
+class NanoController {
+
+	/**
+	 * Name of the module.
+	 * Set automatically by module loader.
+	 */
+	public $moduleName;
+	
+	/** @Inject */
+	public $db;
+	
+	/** @Inject */
+	public $setting;
+	
+	/** @Inject */
+	public $text;
+	
+	/** @Inject */
+	public $util;
+	
+	/**
+	 * This handler is called on bot startup.
+	 * @Setup
+	 */
+	public function setup() {
+		$this->db->loadSQLFile($this->moduleName, 'nanos');
+		$this->db->loadSQLFile($this->moduleName, 'nanolines');
+		$this->db->loadSQLFile($this->moduleName, 'nanolines_ref');
+		
+		$this->setting->add($this->moduleName, 'maxnano', 'Number of Nanos shown on the list', 'edit', "number", '40', '30;40;50;60', "", "mod");
+		$this->setting->add($this->moduleName, "shownanolineicons", "Show icons for the nanolines", "edit", "options", "0", "true;false", "1;0");
+	}
+
+	/**
+	 * @HandlesCommand("nano")
+	 * @Matches("/^nano (.+)$/i")
+	 */
+	public function nanoCommand($message, $channel, $sender, $sendto, $args) {
+		$name = $args[1];
+
+		$name = htmlspecialchars_decode($name);
+		$name = str_replace("'", "''", $name);
+
+		$tmp = explode(" ", $name);
+		forEach ($tmp as $key => $value) {
+			$query .= " AND n1.`name` LIKE '%$value%'";
+		}
+
+		$sql =
+			"SELECT
+				n1.lowid,
+				n1.lowql,
+				n1.name,
+				n1.location,
+				n3.profession
+			FROM
+				nanos n1
+				LEFT JOIN nano_nanolines_ref n2 ON n1.lowid = n2.lowid
+				LEFT JOIN nanolines n3 ON n2.nanolineid = n3.id
+			WHERE
+				1=1 $query
+			ORDER BY
+				n1.lowql DESC, n1.name ASC
+			LIMIT
+				" . $this->setting->get("maxnano");
+
+		$data = $this->db->query($sql);
+
+		$count = count($data);
+		if ($count == 0) {
+			$msg = "No nanos found.";
+		} else if ($count == 1) {
+			$row = $data[0];
+			$msg = $this->text->make_item($row->lowid, $row->lowid, $row->lowql, $row->name);
+			$msg .= " [$row->lowql] $row->location";
+			if ($row->profession) {
+				$msg .= " - <highlight>$row->profession<end>";
+			}
+		} else {
+			$blob = '';
+			forEach ($data as $row) {
+				$blob .= $this->text->make_item($row->lowid, $row->lowid, $row->lowql, $row->name);
+				$blob .= " [$row->lowql] $row->location";
+				if ($row->profession) {
+					$blob .= " - <highlight>$row->profession<end>";
+				}
+				$blob .= "\n";
+			}
+
+			$msg = $this->text->make_blob("Nano Search Results ($count)", $blob);
+		}
+
+		$sendto->reply($msg);
+	}
+	
+	/**
+	 * @HandlesCommand("nanolines")
+	 * @Matches("/^nanolines$/i")
+	 */
+	public function nanolinesListProfsCommand($message, $channel, $sender, $sendto, $args) {
+		$sql = "SELECT DISTINCT profession FROM nanolines ORDER BY profession ASC";
+		$data = $this->db->query($sql);
+
+		$blob = '';
+		forEach ($data as $row) {
+			$blob .= $this->text->make_chatcmd($row->profession, "/tell <myname> nanolines $row->profession");
+			$blob .= "\n";
+		}
+		$blob .= "\n\nAO Nanos by Voriuste";
+		$blob .= "\nModule created by Tyrence (RK2)";
+
+		$msg = $this->text->make_blob('Nanolines', $blob);
+
+		$sendto->reply($msg);
+	}
+	
+	/**
+	 * @HandlesCommand("nanolines")
+	 * @Matches("/^nanolines (.*)$/i")
+	 */
+	public function nanolinesListCommand($message, $channel, $sender, $sendto, $args) {
+		if (preg_match("/^[0-9]+$/", $args[1])) {
+			$this->nanolinesShow($args[1], $sendto);
+		} else {
+			$this->nanolinesList($args[1], $sendto);
+		}
+	}	
+	
+	private function nanolinesShow($nanolineId, $sendto) {
+		$sql = "SELECT * FROM nanolines WHERE id = ?";
+		$nanoline = $this->db->queryRow($sql, $nanolineId);
+
+		$msg = '';
+		if ($nanoline !== null) {
+			$blob = '';
+
+			$sql = "
+				SELECT
+					n1.lowid,
+					lowql,
+					n1.name,
+					location
+				FROM
+					nanos n1
+					JOIN nano_nanolines_ref n2
+						ON (n1.lowid = n2.lowid)
+				WHERE
+					n2.nanolineid = ?
+				ORDER BY
+					lowql DESC, name ASC";
+			$data = $this->db->query($sql, $nanolineId);
+
+			forEach ($data as $nano) {
+				$blob .= $this->text->make_item($nano->lowid, $nano->lowid, $nano->lowql, $nano->name);
+				$blob .= " [$nano->lowql] $nano->location\n";
+			}
+
+			$blob .= "\n\nAO Nanos by Voriuste";
+			$blob .= "\nModule created by Tyrence (RK2)";
+
+			$msg = $this->text->make_blob("$nanoline->profession $nanoline->name Nanos", $blob);
+		} else {
+			$msg = "No nanoline found.";
+		}
+
+		$sendto->reply($msg);
+	}
+
+	private function nanolinesList($profession, $sendto) {
+		$profession = $this->util->get_profession_name($profession);
+		if ($profession == '') {
+			$msg = "Please choose one of these professions: adv, agent, crat, doc, enf, eng, fix, keep, ma, mp, nt, sol, shade, or trader";
+			$sendto->reply($msg);
+			return;
+		}
+
+		$sql = "SELECT * FROM nanolines WHERE profession LIKE ? ORDER BY name ASC";
+		$data = $this->db->query($sql, $profession);
+
+		$blob = '';
+		forEach ($data as $row) {
+			if ($this->setting->get("shownanolineicons") == "1") {
+				$blob .= "<img src='rdb://$row->image_id'>\n";
+			}
+			$blob .= $this->text->make_chatcmd("$row->name", "/tell <myname> nanolines $row->id");
+			$blob .= "\n";
+		}
+		$blob .= "\n\nAO Nanos by Voriuste";
+		$blob .= "\nModule created by Tyrence (RK2)";
+		$msg = $this->text->make_blob("$profession Nanolines", $blob);
+
+		$sendto->reply($msg);
+	}
+	
+	/**
+	 * @HandlesCommand("nanoloc")
+	 * @Matches("/^nanoloc$/i")
+	 */
+	public function nanolocListCommand($message, $channel, $sender, $sendto, $args) {
+		$data = $this->db->query("SELECT location, count(location) AS count FROM nanos GROUP BY location ORDER BY location ASC");
+
+		$blob = '';
+		forEach ($data as $row) {
+			$blob .= $this->text->make_chatcmd($row->location, "/tell <myname> nanoloc $row->location") . " ($row->count) \n";
+		}
+
+		$msg = $this->text->make_blob("Nano Locations", $blob);
+		$sendto->reply($msg);
+	}
+	
+	/**
+	 * @HandlesCommand("nanoloc")
+	 * @Matches("/^nanoloc (.+)$/i")
+	 */
+	public function nanolocViewCommand($message, $channel, $sender, $sendto, $args) {
+		$location = $args[1];
+
+		$sql =
+			"SELECT
+				n1.lowid,
+				n1.lowql,
+				n1.name,
+				n1.location,
+				n3.profession
+			FROM
+				nanos n1
+				LEFT JOIN nano_nanolines_ref n2 ON n1.lowid = n2.lowid
+				LEFT JOIN nanolines n3 ON n2.nanolineid = n3.id
+			WHERE
+				n1.location LIKE ?
+			ORDER BY
+				n1.profession ASC,
+				n1.name ASC";
+
+		$data = $this->db->query($sql, $location);
+
+		$count = count($data);
+		if ($count == 0) {
+			$msg = "No nanos found.";
+		} else {
+			$blob = '';
+			forEach ($data as $row) {
+				$blob .= $this->text->make_item($row->lowid, $row->lowid, $row->lowql, $row->name);
+				$blob .= " [$row->lowql] $row->location";
+				if ($row->profession) {
+					$blob .= " - <highlight>$row->profession<end>";
+				}
+				$blob .= "\n";
+			}
+
+			$msg = $this->text->make_blob("Nanos for Location '$location' ($count)", $blob);
+		}
+
+		$sendto->reply($msg);
+	}
+	
+	/**
+	 * @HandlesCommand("fp")
+	 * @Matches("/^fp (.+)$/i")
+	 */
+	public function fpCommand($message, $channel, $sender, $sendto, $args) {
+		if (preg_match("/^[0-9]+$/", $args[1])) {
+			$id = $args[1];
+			$data = $this->db->query("SELECT * FROM nanos WHERE `lowid` = ? ORDER BY lowql DESC, name LIMIT 0, " . $this->setting->get('maxnano'), $id);
+		} else {
+			$name = str_replace("'", "''", $args[1]);
+
+			$tmp = explode(" ", $name);
+			forEach ($tmp as $key => $value) {
+				$query .= " AND `name` LIKE '%$value%'";
+			}
+
+			$data = $this->db->query("SELECT * FROM nanos WHERE 1=1 $query ORDER BY lowql DESC, name LIMIT 0, " . $this->setting->get('maxnano'));
+		}
+
+		$count = count($data);
+
+		if ($count == 0) {
+			$msg = "No nanos found.";
+		} else if ($count == 1) {
+			$row = $data[0];
+
+			$url = "http://itemxml.xyphos.com/?id={$row->lowid}";  // use low id for id
+
+			$data = file_get_contents($url, 0);
+			if (empty($data) || '<error>' == substr($data, 0, 7)) {
+				$msg = "Unable to query Items XML Database.";
+				$sendto->reply($msg);
+				return;
+			}
+
+			$doc = new DOMDocument();
+			$doc->prevservWhiteSpace = false;
+			$doc->loadXML($data);
+
+			$name = $doc->getElementsByTagName('name')->item(0)->nodeValue;
+			$requirements = $doc->getElementsByTagName('actions')->item(0)->getElementsByTagName('action')->item(0)->getElementsByTagName('requirements')->item(0)->getElementsByTagName('requirement');
+
+			$fpUsable = false;
+			forEach ($requirements as $requirement) {
+				if ($requirement->getElementsByTagName('stat')->item(0)->attributes->getNamedItem("name")->nodeValue == "VisualProfession") {
+					$fpUsable = true;
+				}
+			}
+
+			$item = $this->text->make_item($row->lowid, $row->lowid, $row->lowql, $row->name) . " ({$row->lowql})";
+
+			if ($fpUsable) {
+				$msg = "$item <green>is<end> usable in false profession.";
+			} else {
+				$msg = "$item <orange>is not<end> usable in false profession.";
+			}
+		} else {
+			$blob = '';
+			forEach ($data as $row) {
+				$check_fp = $this->text->make_chatcmd("Check False Profession", "/tell <myname> fp $row->lowid");
+				$blob .= $this->text->make_item($row->lowid, $row->lowid, $row->lowql, $row->name) . " ({$row->lowql}) $check_fp\n\n";
+			}
+			$blob .= "Written by Tyrence (RK2)\n";
+			$blob .= "Stats provided by xyphos.com";
+
+			$msg = $this->text->make_blob("Nano Search Results ($count)", $blob);
+		}
+
+		$sendto->reply($msg);
+	}
+}
