f7eba191ab7552d1cf00cb721bacf8953a3d0263
Tyrence <bigwheels16@gmail.com>
2012-10-01 18:37:15 +0000

    converted SVN_MODULE to new 3.0 OO-style issue #167

---
+++ modules/SVN_MODULE/SVNController.class.php
@@ -0,0 +1,145 @@
+<?php
+
+/**
+ * Authors: 
+ *  - Tyrence (RK2)
+ *
+ * @Instance
+ *
+ * Commands this class contains:
+ *	@DefineCommand(
+ *		command     = 'svn',
+ *		accessLevel = 'admin',
+ *		description = 'Updates bot from SVN repository',
+ *		help        = 'svn.txt'
+ *	)
+ */
+class SVNController {
+
+	/**
+	 * Name of the module.
+	 * Set automatically by module loader.
+	 */
+	public $moduleName;
+	
+	/** @Inject */
+	public $chatBot;
+	
+	/** @Inject */
+	public $setting;
+
+	/** @Inject */
+	public $text;
+
+	/** @Inject */
+	public $util;
+	
+	/**
+	 * @Setup
+	 */
+	public function setup() {
+		$this->setting->add($this->moduleName, "svnconflict", "How to handle conflicts", "edit", "options", "theirs-conflict", "theirs-conflict;mine-conflict;theirs-full;mine-full;postpone", '', "admin", "");
+		$this->setting->add($this->moduleName, "svnpath", "Path to svn binary", "edit", "text", "svn", "svn;/usr/bin/svn");
+	}
+	
+	/**
+	 * @HandlesCommand("svn")
+	 * @Matches("/^svn dry$/i")
+	 */
+	public function svnDryCommand($message, $channel, $sender, $sendto, $args) {
+		$svnpath = $this->setting->get('svnpath');
+		$command = "$svnpath merge –-dry-run -r BASE:HEAD . 2>&1";
+		$output = array();
+		$return_var = '';
+		exec($command, $output, $return_var);
+
+		$blob = $command . "\n\n";
+		forEach ($output as $line) {
+			$blob .= $line . "\n";
+		}
+
+		$msg = $this->text->make_blob('svn merge –-dry-run output', $blob);
+
+		$sendto->reply($msg);
+	}
+
+	/**
+	 * @HandlesCommand("svn")
+	 * @Matches("/^svn update$/i")
+	 */
+	public function svnUpdateCommand($message, $channel, $sender, $sendto, $args) {
+		$svnpath = $this->setting->get('svnpath');
+		$command = "$svnpath info 2>&1";
+		$output = array();
+		$return_var = '';
+		exec($command, $output, $return_var);
+
+		$blob = $command . "\n\n";
+		forEach ($output as $line) {
+			$blob .= $line . "\n";
+		}
+		$blob .= "\n";
+
+		$command = "$svnpath update --accept " . $this->setting->get('svnconflict') . " 2>&1";
+		$output = array();
+		$return_var = '';
+		exec($command, $output, $return_var);
+
+		$blob .= $command . "\n\n";
+		forEach ($output as $line) {
+			$blob .= $line . "\n";
+		}
+
+		$msg = $this->text->make_blob('svn update output', $blob);
+
+		$sendto->reply($msg);
+	}
+	
+	/**
+	 * @HandlesCommand("svn")
+	 * @Matches("/^svn info$/i")
+	 */
+	public function svnInfoCommand($message, $channel, $sender, $sendto, $args) {
+		$svnpath = $this->setting->get('svnpath');
+		$command = "$svnpath info 2>&1";
+		$output = array();
+		$return_var = '';
+		exec($command, $output, $return_var);
+
+		$blob = $command . "\n\n";
+		forEach ($output as $line) {
+			$blob .= $line . "\n";
+		}
+
+		$msg = $this->text->make_blob('svn info output', $blob);
+
+		$sendto->reply($msg);
+	}
+	
+	/**
+	 * @HandlesCommand("svn")
+	 * @Matches("/^svn status$/i")
+	 * @Matches("/^svn status (.*)$/i")
+	 */
+	public function svnStatusCommand($message, $channel, $sender, $sendto, $args) {
+		$svnpath = $this->setting->get('svnpath');
+		if (count($args) == 2) {
+			$param = $args[1];
+		}
+		$command = "$svnpath status $param 2>&1";
+		$output = array();
+		$return_var = '';
+		exec($command, $output, $return_var);
+
+		$blob = $command . "\n\n";
+		forEach ($output as $line) {
+			$blob .= $line . "\n";
+		}
+
+		$msg = $this->text->make_blob("svn status $param output", $blob);
+
+		$sendto->reply($msg);
+	}
+}
+
+?>
--- modules/SVN_MODULE/SVN_MODULE.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$command->register($MODULE_NAME, "", "svn.php", "svn", "admin", "Updates your bot from svn repository");
-
-	$setting->add($MODULE_NAME, "svnconflict", "How to handle conflicts", "edit", "options", "theirs-conflict", "theirs-conflict;mine-conflict;theirs-full;mine-full;postpone", '', "admin", "");
-	$setting->add($MODULE_NAME, "svnpath", "Path to svn binary", "edit", "text", "svn", "svn;/usr/bin/svn");
-?>
