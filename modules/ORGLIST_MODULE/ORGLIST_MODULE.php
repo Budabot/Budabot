acbf87a6960a58fa3f6342314c601cb90c5ebcb4
Tyrence <bigwheels16@gmail.com>
2012-10-10 22:07:56 +0000

    converted !orgmembers to new 3.0 OO-style

--- modules/ORGLIST_MODULE/ORGLIST_MODULE.php
+++ modules/ORGLIST_MODULE/ORGLIST_MODULE.php
@@ -1,10 +1,11 @@
 <?php
 	require_once 'orglist_functions.php';
 	require_once 'IsOnlineController.class.php';
+	require_once 'OrgMembersController.class.php';
 	
 	$chatBot->registerInstance($MODULE_NAME, 'IsOnlineController', new IsOnlineController());
+	$chatBot->registerInstance($MODULE_NAME, 'OrgMembersController', new OrgMembersController());
 
-	$command->register($MODULE_NAME, "", "orgmembers.php", "orgmembers", "guild", "Show guild members sorted by name", "orgmembers.txt");
 	$command->register($MODULE_NAME, "", "orgranks.php", "orgranks", "guild", "Show guild members sorted by guild rank", "orgranks.txt");
 	$command->register($MODULE_NAME, "", "whoisorg.php", "whoisorg", "all", "Display org info", "whoisorg.txt");
 	$command->register($MODULE_NAME, "", "findorg.php", "findorg", "all", "Find orgs by name", "findorg.txt");
---
+++ modules/ORGLIST_MODULE/OrgMembersController.class.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Authors: 
+ *	- Tyrence (RK2)
+ *
+ * @Instance
+ *
+ * Commands this controller contains:
+ *	@DefineCommand(
+ *		command     = 'orgmembers',
+ *		accessLevel = 'guild',
+ *		description = 'Show guild members sorted by name',
+ *		help        = 'orgmembers.txt'
+ *	)
+ */
+class OrgMembersController {
+
+	/**
+	 * Name of the module.
+	 * Set automatically by module loader.
+	 */
+	public $moduleName;
+
+	/** @Inject */
+	public $db;
+	
+	/** @Inject */
+	public $chatBot;
+
+	/** @Inject */
+	public $text;
+	
+	/**
+	 * @HandlesCommand("orgmembers")
+	 * @Matches("/^orgmembers$/i")
+	 */
+	public function orgmembers1Command($message, $channel, $sender, $sendto, $args) {
+		if ($this->chatBot->vars["my_guild_id"] == "") {
+			$msg = "The Bot needs to be in a org to show the orgmembers.";
+			$sendto->reply($msg);
+			return;
+		}
+
+		$data = $this->db->query("SELECT * FROM org_members_<myname> o LEFT JOIN players p ON (o.name = p.name AND p.dimension = '<dim>') WHERE `mode` != 'del' ORDER BY o.name");
+		$members = count($data);
+		if ($members == 0) {
+			$msg = "No members recorded.";
+			$sendto->reply($msg);
+			return;
+		}
+
+		$currentLetter = "";
+		$blob = '';
+		forEach ($data as $row) {
+			if ($row->name[0] != $currentLetter) {
+				$currentLetter = $row->name[0];
+				$blob .= "\n\n<header2>$currentLetter<end>\n";
+			}
+			
+			if ($buddylistManager->is_online($row->name) == 1) {
+				$logged_off = " :: <highlight>Last logoff:<end> <green>Online<end>";
+			} else if ($row->logged_off != "0") {
+				$logged_off = " :: <highlight>Last logoff:<end> " . $this->util->date($row->logged_off);
+			} else {
+				$logged_off = " :: <highlight>Last logoff:<end> <orange>Unknown<end>";
+			}
+
+			$prof = $this->util->get_profession_abbreviation($row->profession);
+
+			$blob .= "<tab><highlight>$row->name<end> (Lvl $row->level/<green>$row->ai_level<end>/$prof/$row->guild_rank)$logged_off\n";
+		}
+
+		$msg = $this->text->make_blob("<myguild> has $members members currently.", $blob);
+		$sendto->reply($msg);
+	}
+	
+	/**
+	 * @HandlesCommand("orgmembers")
+	 * @Matches("/^orgmembers ([a-z0-9-]+)$/i")
+	 */
+	public function orgmembers2Command($message, $channel, $sender, $sendto, $args) {
+		if (preg_match("/[0-9]+/", $args[1])) {
+			$guild_id = $args[1];
+		} else {
+			// Someone's name.  Doing a whois to get an orgID.
+			$name = ucfirst(strtolower($args[1]));
+			$whois = Player::get_by_name($name);
+
+			if ($whois === null) {
+				$msg = "Could not find character info for $name.";
+				$sendto->reply($msg);
+				return;
+			} else if (!$whois->guild_id) {
+				$msg = "Character <highlight>$name<end> does not seem to be in an org.";
+				$sendto->reply($msg);
+				return;
+			} else {
+				$guild_id = $whois->guild_id;
+			}
+		}
+
+		$msg = "Getting guild info. Please wait...";
+		$sendto->reply($msg);
+
+		$org = Guild::get_by_id($guild_id);
+		if ($org === null) {
+			$msg = "Error in getting the Org info. Either org does not exist or AO's server was too slow to respond.";
+			$sendto->reply($msg);
+			return;
+		}
+
+		$sql = "SELECT * FROM players WHERE guild_id = ? AND dimension = '<dim>' ORDER BY name ASC";
+		$data = $this->db->query($sql, $guild_id);
+		$numrows = count($data);
+
+		$blob = '';
+
+		$currentLetter = '';
+		forEach ($data as $row) {
+			if ($currentLetter != $row->name[0]) {
+				$currentLetter = $row->name[0];
+				$blob .= "\n\n<header2>$currentLetter<end>\n";
+			}
+
+			$blob .= "<tab><highlight>{$row->name}, {$row->guild_rank} (Level {$row->level}";
+			if ($row->ai_level > 0) {
+				$blob .= "<green>/{$row->ai_level}<end>";
+			}
+			$blob .= ", {$row->gender} {$row->breed} {$row->profession})<end>\n";
+		}
+
+		$msg = $this->text->make_blob("Org members for '$org->orgname' ($numrows)", $blob);
+		$sendto->reply($msg);
+	}
+}
+
