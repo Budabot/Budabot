398027cfba1df0729118739f9952254d95dcaf65
Tyrence <bigwheels16@gmail.com>
2012-10-10 19:16:35 +0000

    converted !is to new 3.0 OO-style

---
+++ modules/ORGLIST_MODULE/IsOnlineController.class.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Authors: 
+ *  - Tyrence (RK2)
+ *
+ * @Instance
+ *
+ * Commands this controller contains:
+ *	@DefineCommand(
+ *		command     = 'is', 
+ *		accessLevel = 'all', 
+ *		description = "Checks if a player is online", 
+ *		help        = 'isonline.txt'
+ *	)
+ */
+class IsOnlineController {
+
+	/**
+	 * Name of the module.
+	 * Set automatically by module loader.
+	 */
+	public $moduleName;
+	
+	/** @Inject */
+	public $db;
+
+	/** @Inject */
+	public $chatBot;
+	
+	/** @Inject */
+	public $buddylistManager;
+	
+	/** @Inject */
+	public $util;
+	
+	private $player = null;
+
+	/**
+	 * @HandlesCommand("is")
+	 * @Matches("/^is (.+)$/i")
+	 */
+	public function isOnlineCommand($message, $channel, $sender, $sendto, $args) {
+		$name = ucfirst(strtolower($args[1]));
+		$uid = $this->chatBot->get_uid($name);
+		if (!$uid) {
+			$msg = "Character <highlight>$name<end> does not exist.";
+			$sendto->reply($msg);
+		} else {
+			//if the player is a buddy then
+			$online_status = $this->buddylistManager->is_online($name);
+			if ($online_status === null) {
+				$this->player['playername'] = $name;
+				$this->player['sendto'] = $sendto;
+				$this->buddylistManager->add($name, 'is_online');
+			} else {
+				$row = $this->db->queryRow("SELECT * FROM org_members_<myname> WHERE `name` = ?", $name);
+				if ($row !== null) {
+					if ($row->logged_off != "0") {
+						$logged_off = " Logged off at " . $this->util->date($row->logged_off);
+					}
+				}
+				if ($online_status) {
+					$status = "<green>online<end>";
+				} else {
+					$status = "<red>offline<end>".$logged_off;
+				}
+				$msg = "Character <highlight>$name<end> is $status.";
+				$sendto->reply($msg);
+			}
+		}
+	}
+	
+	/**
+	 * @Event("logOn")
+	 * @Description("Gets online status of player")
+	 */
+	public function logonEvent($eventObj) {
+		$sender = $eventObj->sender;
+		$type = $eventObj->type;
+		if ($this->player !== null && $sender == $this->player['playername']) {
+			if ($type == "logon") {
+				$status = "<green>online<end>";
+			} else if ($type == "logoff") {
+				$status = "<red>offline<end>";
+			}
+			$msg = "Character <highlight>$sender<end> is $status.";
+			$this->player['sendto']->reply($msg);
+			$this->buddylistManager->remove($sender, 'is_online');
+			$this->player = null;
+		}
+	}
+	
+	/**
+	 * @Event("logOff")
+	 * @Description("Gets offline status of player")
+	 */
+	public function logoffEvent($eventObj) {
+		$sender = $eventObj->sender;
+		$type = $eventObj->type;
+		if ($this->player !== null && $sender == $this->player['playername']) {
+			if ($type == "logon") {
+				$status = "<green>online<end>";
+			} else if ($type == "logoff") {
+				$status = "<red>offline<end>";
+			}
+			$msg = "Character <highlight>$sender<end> is $status.";
+			$this->player['sendto']->reply($msg);
+			$this->buddylistManager->remove($sender, 'is_online');
+			$this->player = null;
+		}
+	}
+}
+
--- modules/ORGLIST_MODULE/ORGLIST_MODULE.php
+++ modules/ORGLIST_MODULE/ORGLIST_MODULE.php
@@ -1,15 +1,15 @@
 <?php
 	require_once 'orglist_functions.php';
+	require_once 'IsOnlineController.class.php';
+	
+	$chatBot->registerInstance($MODULE_NAME, 'IsOnlineController', new IsOnlineController());
 
 	$command->register($MODULE_NAME, "", "orgmembers.php", "orgmembers", "guild", "Show guild members sorted by name", "orgmembers.txt");
 	$command->register($MODULE_NAME, "", "orgranks.php", "orgranks", "guild", "Show guild members sorted by guild rank", "orgranks.txt");
 	$command->register($MODULE_NAME, "", "whoisorg.php", "whoisorg", "all", "Display org info", "whoisorg.txt");
 	$command->register($MODULE_NAME, "", "findorg.php", "findorg", "all", "Find orgs by name", "findorg.txt");
-	$command->register($MODULE_NAME, "", "is_online.php", "is", "all", "Checks if a player is online", "isonline.txt");
 	$command->register($MODULE_NAME, "", "orglist.php", "orglist", "guild", "Check someones org roster", "orglist.txt");
 
 	$event->register($MODULE_NAME, "logOn", "orglist_check.php", "Gets online status of org members");
 	$event->register($MODULE_NAME, "logOff", "orglist_check.php", "Gets offline status of org members");
-	$event->register($MODULE_NAME, "logOn", "is_online_check.php", "Gets online status of player");
-	$event->register($MODULE_NAME, "logOff", "is_online_check.php", "Gets offline status of player");
 ?>
