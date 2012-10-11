ff67928d497671c712813d4d89da586b95069480
Tyrence <bigwheels16@gmail.com>
2012-10-11 06:45:43 +0000

    converted !findorg to new 3.0 OO-style

---
+++ modules/ORGLIST_MODULE/FindOrgController.class.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Authors: 
+ *  - Tyrence (RK2)
+ *
+ * @Instance
+ *
+ * Commands this controller contains:
+ *	@DefineCommand(
+ *		command     = 'findorg', 
+ *		accessLevel = 'all', 
+ *		description = "Find orgs by name", 
+ *		help        = 'findorg.txt'
+ *	)
+ */
+class FindOrgController {
+
+	/**
+	 * Name of the module.
+	 * Set automatically by module loader.
+	 */
+	public $moduleName;
+	
+	/** @Inject */
+	public $db;
+
+	/** @Inject */
+	public $chatBot;
+	
+	/** @Inject */
+	public $text;
+
+	/**
+	 * @HandlesCommand("findorg")
+	 * @Matches("/^findorg (.+) (\d)$/i")
+	 * @Matches("/^findorg (.+)$/i")
+	 */
+	public function findOrgCommand($message, $channel, $sender, $sendto, $args) {
+		$guild_name = $args[1];
+
+		$dimension = $this->chatBot->vars['dimension'];
+		if (count($args) == 3) {
+			$dimension = $args[2];
+		}
+
+		$sql = "SELECT DISTINCT guild, guild_id, CASE WHEN guild_id = '' THEN 0 ELSE 1 END AS sort FROM players WHERE guild LIKE ? AND dimension = ? ORDER BY sort DESC, guild ASC LIMIT 30";
+		$data = $this->db->query($sql, '%'.$guild_name.'%', $dimension);
+		if (count($data) > 0) {
+			$blob = '';
+
+			forEach ($data as $row) {
+				if ($row->guild_id != '') {
+					$whoisorg = $this->text->make_chatcmd('Whoisorg', "/tell <myname> whoisorg {$row->guild_id} $dimension");
+					if ($dimension == $this->chatBot->vars['dimension']) {
+						$orglist = $this->text->make_chatcmd('Orglist', "/tell <myname> orglist {$row->guild_id}");
+						$orgranks = $this->text->make_chatcmd('Orgranks', "/tell <myname> orgranks {$row->guild_id}");
+						$orgmembers = $this->text->make_chatcmd('Orgmembers', "/tell <myname> orgmembers {$row->guild_id}");
+						$tower_attacks = $this->text->make_chatcmd('Tower Attacks', "/tell <myname> attacks org {$row->guild}");
+						$tower_victories = $this->text->make_chatcmd('Tower Victories', "/tell <myname> victory org {$row->guild}");
+						$blob .= "<green>{$row->guild} ({$row->guild_id})<end> [$whoisorg] [$orglist] [$orgranks] [$orgmembers] [$tower_attacks] [$tower_victories]\n";
+					} else {
+						$blob .= "<green>{$row->guild} ({$row->guild_id})<end> [$whoisorg]\n";
+					}
+				} else {
+					$blob .= "<green>{$row->guild}<end>\n";
+				}
+			}
+
+			$msg = $this->text->make_blob("Org Search Results for '{$guild_name}' on RK{$dimension}", $blob);
+		} else {
+			$msg = "No matches found.";
+		}
+		$sendto->reply($msg);
+	}
+}
+
--- modules/ORGLIST_MODULE/ORGLIST_MODULE.php
+++ modules/ORGLIST_MODULE/ORGLIST_MODULE.php
@@ -2,13 +2,14 @@
 	require_once 'orglist_functions.php';
 	require_once 'IsOnlineController.class.php';
 	require_once 'OrgMembersController.class.php';
+	require_once 'FindOrgController.class.php';
 	
 	$chatBot->registerInstance($MODULE_NAME, 'IsOnlineController', new IsOnlineController());
 	$chatBot->registerInstance($MODULE_NAME, 'OrgMembersController', new OrgMembersController());
+	$chatBot->registerInstance($MODULE_NAME, 'FindOrgController', new FindOrgController());
 
 	$command->register($MODULE_NAME, "", "orgranks.php", "orgranks", "guild", "Show guild members sorted by guild rank", "orgranks.txt");
 	$command->register($MODULE_NAME, "", "whoisorg.php", "whoisorg", "all", "Display org info", "whoisorg.txt");
-	$command->register($MODULE_NAME, "", "findorg.php", "findorg", "all", "Find orgs by name", "findorg.txt");
 	$command->register($MODULE_NAME, "", "orglist.php", "orglist", "guild", "Check someones org roster", "orglist.txt");
 
 	$event->register($MODULE_NAME, "logOn", "orglist_check.php", "Gets online status of org members");
