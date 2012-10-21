6d4a67db12db171d3e5af1df7b61b5e824174f9b
Tyrence <bigwheels16@gmail.com>
2012-10-21 09:57:46 +0000

    added !testtowerattack and !testtowervictory

--- modules/TEST_MODULE/TestController.class.php
+++ modules/TEST_MODULE/TestController.class.php
@@ -18,6 +18,18 @@
  *		description = "Test the bot commands", 
  *		help        = 'test.txt'
  *	)
+ *	@DefineCommand(
+ *		command     = 'testtowerattack', 
+ *		accessLevel = 'admin', 
+ *		description = "Test the bot commands", 
+ *		help        = 'test.txt'
+ *	)
+ *	@DefineCommand(
+ *		command     = 'testtowervictory', 
+ *		accessLevel = 'admin', 
+ *		description = "Test the bot commands", 
+ *		help        = 'test.txt'
+ *	)
  */
 class TestController {
 
@@ -29,6 +41,9 @@ class TestController {
 
 	/** @Inject */
 	public $chatBot;
+	
+	/** @Inject */
+	public $eventManager;
 	
 	/** @Inject */
 	public $util;
@@ -75,9 +90,37 @@ class TestController {
 		$packet = new stdClass;
 		$packet->type = AOCP_GROUP_MESSAGE;
 		$packet->args = array();
-		$packet->args[0] = $this->chatBot->gid['org msg'];
+		$packet->args[0] = $this->chatBot->get_gid('org msg');
 		$packet->args[1] = (int)0xFFFFFFFF;
 		$packet->args[2] = "$sender invited $args[1] to your organization.";
+
+		$this->chatBot->process_packet($packet);
+	}
+	
+	/**
+	 * @HandlesCommand("testtowerattack")
+	 * @Matches("/^testtowerattack (clan|neutral|omni) (.+) (.+) (clan|neutral|omni) (.+) (.+) (\d+) (\d+)$/i")
+	 */
+	public function testtowerattackCommand($message, $channel, $sender, $sendto, $args) {
+		$eventObj = new stdClass;
+		$eventObj->sender = -1;
+		$eventObj->channel = "All Towers";
+		$eventObj->message = "The $args[1] organization $args[2] just entered a state of war! $args[3] attacked the $args[4] organization $args[5]'s tower in $args[6] at location ($args[7],$args[8]).";
+		$eventObj->type = 'towers';
+		$this->eventManager->fireEvent($eventObj);
+	}
+	
+	/**
+	 * @HandlesCommand("testtowervictory")
+	 * @Matches("/^testtowervictory (Clan|Neutral|Omni) (.+) (Clan|Neutral|Omni) (.+) (.+)$/i")
+	 */
+	public function testtowervictoryCommand($message, $channel, $sender, $sendto, $args) {
+		$packet = new stdClass;
+		$packet->type = AOCP_GROUP_MESSAGE;
+		$packet->args = array();
+		$packet->args[0] = $this->chatBot->get_gid('tower battle outcome');
+		$packet->args[1] = (int)0xFFFFFFFF;
+		$packet->args[2] = "The $args[1] organization $args[2] attacked the $args[3] $args[4] at their base in $args[5]. The attackers won!!";
 
 		$this->chatBot->process_packet($packet);
 	}
