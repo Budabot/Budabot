d19c1fd151dbab17c76dc7acb54f7668256d99c0
Tyrence <bigwheels16@gmail.com>
2012-10-23 07:38:24 +0000

    fix for !alts crashing the bot

--- core/ALTS/AltInfo.class.php
+++ core/ALTS/AltInfo.class.php
@@ -22,15 +22,16 @@ class AltInfo {
 	public function get_alts_blob($showValidateLinks = false, $firstPageOnly = false) {
 		$db = Registry::getInstance('db');
 		$setting = Registry::getInstance('setting');
-		$player = Registry::getInstance('player');
+		$playerManager = Registry::getInstance('playerManager');
 		$buddylistManager = Registry::getInstance('buddylistManager');
+		$text = Registry::getInstance('text');
 
 		if (count($this->alts) == 0) {
 			return "No registered alts.";
 		}
 
 		$blob .= "<tab><tab>{$this->main}";
-		$character = $player->get_by_name($this->main);
+		$character = $playerManager->get_by_name($this->main);
 		if ($character !== null) {
 			$blob .= " (<highlight>{$character->level}<end>/<green>{$character->ai_level}<end> <highlight>{$character->profession}<end>)";
 		}
@@ -38,7 +39,7 @@ class AltInfo {
 		if ($online === null) {
 			$blob .= " - No status\n";
 		} else if ($online == 1) {
-			$blob .= " - <green>Online<end>\n " . Text::make_chatcmd("Send tell", "/tell $this->main");
+			$blob .= " - <green>Online<end>\n " . $text->make_chatcmd("Send tell", "/tell $this->main");
 		} else {
 			$blob .= " - <red>Offline<end>\n";
 		}
@@ -57,19 +58,19 @@ class AltInfo {
 			if ($online === null) {
 				$blob .= " - No status.";
 			} else if ($online == 1) {
-				$blob .= " - <green>Online<end> " . Text::make_chatcmd("Send tell", "/tell $row->alt");
+				$blob .= " - <green>Online<end> " . $text->make_chatcmd("Send tell", "/tell $row->alt");
 			} else {
 				$blob .= " - <red>Offline<end>";
 			}
 
 			if ($showValidateLinks && $setting->get('alts_inherit_admin') == 1 && $row->validated == 0) {
-				$blob .= " [Unvalidated] " . Text::make_chatcmd('Validate', "/tell <myname> <symbol>altvalidate {$row->alt}");
+				$blob .= " [Unvalidated] " . $text->make_chatcmd('Validate', "/tell <myname> <symbol>altvalidate {$row->alt}");
 			}
 
 			$blob .= "\n";
 		}
 
-		$msg = Text::make_blob("Alts of {$this->main}", $blob);
+		$msg = $text->make_blob("Alts of {$this->main}", $blob);
 
 		if ($firstPageOnly && is_array($msg)) {
 			return $msg[0];
