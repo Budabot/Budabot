feaf904308c380f64de26de121d6e01f3c504559
Tyrence <bigwheels16@gmail.com>
2012-10-24 23:47:00 +0000

    !usage now shows links to !usage player

--- core/USAGE/UsageController.class.php
+++ core/USAGE/UsageController.class.php
@@ -50,50 +50,6 @@ class UsageController {
 	
 	/**
 	 * @HandlesCommand("usage")
-	 * @Matches("/^usage$/i")
-	 * @Matches("/^usage ([a-z0-9]+)$/i")
-	 */
-	public function usageCommand($message, $channel, $sender, $sendto, $args) {
-		if (count($args) == 2) {
-			$time = $this->util->parseTime($args[1]);
-			if ($time == 0) {
-				$msg = "Please enter a valid time.";
-				$sendto->reply($msg);
-				return;
-			}
-			$time = $time;
-		} else {
-			$time = 604800;
-		}
-
-		$timeString = $this->util->unixtime_to_readable($time);
-		$time = time() - $time;
-		$limit = 25;
-
-		// most used commands
-		$sql = "SELECT command, COUNT(command) AS count FROM usage_<myname> WHERE dt > ? GROUP BY command ORDER BY count DESC LIMIT $limit";
-		$data = $this->db->query($sql, $time);
-
-		$blob = "<header2> ::: Most Used Commands ::: <end>\n";
-		forEach ($data as $row) {
-			$blob .= "<highlight>{$row->command}<end> ({$row->count})\n";
-		}
-
-		// users who have used the most commands
-		$sql = "SELECT sender, COUNT(sender) AS count FROM usage_<myname> WHERE dt > ? GROUP BY sender ORDER BY count DESC LIMIT $limit";
-		$data = $this->db->query($sql, $time);
-
-		$blob .= "\n<header2> ::: Most Active Users ::: <end>\n";
-		forEach ($data as $row) {
-			$blob .= "<highlight>{$row->sender}<end> ({$row->count})\n";
-		}
-
-		$msg = $this->text->make_blob("Usage Statistics ({$timeString})", $blob);
-		$sendto->reply($msg);
-	}
-	
-	/**
-	 * @HandlesCommand("usage")
 	 * @Matches("/^usage player ([0-9a-z-]+)$/i")
 	 * @Matches("/^usage player ([0-9a-z-]+) ([a-z0-9]+)$/i")
 	 */
@@ -129,6 +85,51 @@ class UsageController {
 		} else {
 			$msg = "No usage statistics found for <highlight>$player<end>.";
 		}
+		$sendto->reply($msg);
+	}
+	
+	/**
+	 * @HandlesCommand("usage")
+	 * @Matches("/^usage$/i")
+	 * @Matches("/^usage ([a-z0-9]+)$/i")
+	 */
+	public function usageCommand($message, $channel, $sender, $sendto, $args) {
+		if (count($args) == 2) {
+			$time = $this->util->parseTime($args[1]);
+			if ($time == 0) {
+				$msg = "Please enter a valid time.";
+				$sendto->reply($msg);
+				return;
+			}
+			$time = $time;
+		} else {
+			$time = 604800;
+		}
+
+		$timeString = $this->util->unixtime_to_readable($time);
+		$time = time() - $time;
+		$limit = 25;
+
+		// most used commands
+		$sql = "SELECT command, COUNT(command) AS count FROM usage_<myname> WHERE dt > ? GROUP BY command ORDER BY count DESC LIMIT $limit";
+		$data = $this->db->query($sql, $time);
+
+		$blob = "<header2> ::: Most Used Commands ::: <end>\n";
+		forEach ($data as $row) {
+			$blob .= "<highlight>{$row->command}<end> ({$row->count})\n";
+		}
+
+		// users who have used the most commands
+		$sql = "SELECT sender, COUNT(sender) AS count FROM usage_<myname> WHERE dt > ? GROUP BY sender ORDER BY count DESC LIMIT $limit";
+		$data = $this->db->query($sql, $time);
+
+		$blob .= "\n<header2> ::: Most Active Users ::: <end>\n";
+		forEach ($data as $row) {
+			$senderLink = $this->text->make_chatcmd($row->sender, "/tell <myname> usage player $row->sender");
+			$blob .= "<highlight>{$senderLink}<end> ({$row->count})\n";
+		}
+
+		$msg = $this->text->make_blob("Usage Statistics ({$timeString})", $blob);
 		$sendto->reply($msg);
 	}
 
