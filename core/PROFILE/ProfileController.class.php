dfe567309ddff8406822fdddd74a27459fde2437
Tyrence <bigwheels16@gmail.com>
2012-10-30 22:11:50 +0000

    updated core PROFILE module

--- core/PROFILE/ProfileController.class.php
+++ core/PROFILE/ProfileController.class.php
@@ -24,6 +24,12 @@ class ProfileController {
 	public $moduleName;
 
 	/** @Inject */
+	public $db;
+	
+	/** @Inject */
+	public $setting;
+	
+	/** @Inject */
 	public $text;
 	
 	/** @Inject */
@@ -63,7 +69,10 @@ class ProfileController {
 			$linkContents = '';
 			forEach ($profileList as $profile) {
 				$name = ucfirst(strtolower($profile));
-				$linkContents .= $this->text->make_chatcmd($name, "/tell <myname> profile $profile") . "\n";
+				$viewLink = $this->text->make_chatcmd("View", "/tell <myname> profile view $profile");
+				$loadLink = $this->text->make_chatcmd("Load", "/tell <myname> profile load $profile");
+				$remLink = $this->text->make_chatcmd("Remove", "/tell <myname> profile rem $profile");
+				$linkContents .= "$profile [$viewLink] [$loadLink] [$remLink]\n";
 			}
 
 			if ($linkContents) {
@@ -80,26 +89,104 @@ class ProfileController {
 	
 	/**
 	 * @HandlesCommand("profile")
-	 * @Matches("/^profile ([a-z0-9_-]+)$/i")
+	 * @Matches("/^profile view ([a-z0-9_-]+)$/i")
 	 */
-	public function profileShowCommand($message, $channel, $sender, $sendto, $args) {
-		// get the filename and read in the file
-		$fileName = strtolower($args[1]);
-		$name = ucfirst($fileName);
-		$info = file_get_contents($this->path . $fileName . $this->fileExt);
-		$lines = explode("\n", $info);
-		$output = '';
-		forEach ($lines as $line) {
-			if ($line[0] == "!") {
-				$output .= "<pagebreak>" . $line . "\n";
-				$line = substr(trim($line), 1);
-				$profileSendTo = new ProfileCommandReply();
-				$this->commandManager->process("msg", $line, $sender, $profileSendTo);
-				$output .= $profileSendTo->result . "\n\n";
-			}
+	public function profileViewCommand($message, $channel, $sender, $sendto, $args) {
+		$profileName = $args[1];
+		$filename = $this->path . '/' . $profileName . $this->fileExt;
+		if (!file_exists($filename)) {
+			$msg = "Profile <highlight>$profileName<end> does not exist.";
+		} else {
+			$blob = htmlspecialchars(file_get_contents($filename));
+			$msg = $this->text->make_blob("Profile $profileName", $blob);
 		}
+		$sendto->reply($msg);
+	}
+	
+	/**
+	 * @HandlesCommand("profile")
+	 * @Matches("/^profile save ([a-z0-9_-]+)$/i")
+	 */
+	public function profileSaveCommand($message, $channel, $sender, $sendto, $args) {
+		$profileName = $args[1];
+		$filename = $this->path . '/' . $profileName . $this->fileExt;
+		if (file_exists($filename)) {
+			$msg = "Profile <highlight>$profileName<end> already exists.";
+		} else {
+			$contents = "# Settings\n";
+			forEach ($this->setting->settings as $name => $value) {
+				$contents .= "!settings save $name $value\n";
+			}
+			$contents .= "\n# Events\n";
+			$data = $this->db->query("SELECT * FROM eventcfg_<myname>");
+			forEach ($data as $row) {
+				$status = "disable";
+				if ($row->status == 1) {
+					$status = "enable";
+				}
+				$contents .= "!config event {$row->type} {$row->file} {$status} all\n";
+			}
+			$contents .= "\n# Commands\n";
+			$data = $this->db->query("SELECT * FROM cmdcfg_<myname>");
+			forEach ($data as $row) {
+				$status = "disable";
+				if ($row->status == 1) {
+					$status = "enable";
+				}
+				$contents .= "!config cmd {$row->cmd} {$status} {$row->type}\n";
+			}
+			file_put_contents($filename, $contents);
+			$msg = "Profile <highlight>$profileName<end> has been saved.";
+		}
+		$sendto->reply($msg);
+	}
+	
+	/**
+	 * @HandlesCommand("profile")
+	 * @Matches("/^profile (rem|remove|del|delete) ([a-z0-9_-]+)$/i")
+	 */
+	public function profileRemCommand($message, $channel, $sender, $sendto, $args) {
+		$profileName = $args[2];
+		$filename = $this->path . '/' . $profileName . $this->fileExt;
+		if (!file_exists($filename)) {
+			$msg = "Profile <highlight>$profileName<end> does not exist.";
+		} else {
+			unlink($filename);
+			$msg = "Profile <highlight>$profileName<end> has been deleted.";
+		}
+		$sendto->reply($msg);
+	}
+	
+	/**
+	 * @HandlesCommand("profile")
+	 * @Matches("/^profile load ([a-z0-9_-]+)$/i")
+	 */
+	public function profileLoadCommand($message, $channel, $sender, $sendto, $args) {
+		$profileName = $args[1];
+		$filename = $this->path . '/' . $profileName . $this->fileExt;
 		
-		$msg = $this->text->make_blob("$name Profile Results", $output);
+		if (!file_exists($filename)) {
+			$msg = "Profile <highlight>$profileName<end> does not exist.";
+		} else {
+			$sendto->reply("Loading profile <highlight>$profileName<end>...");
+			// get the filename and read in the file
+			$fileName = strtolower($args[1]);
+			$name = ucfirst($fileName);
+			$info = file_get_contents($filename);
+			$lines = explode("\n", $info);
+			$output = '';
+			forEach ($lines as $line) {
+				if ($line[0] == "!") {
+					$output .= "<pagebreak>" . $line . "\n";
+					$line = substr(trim($line), 1);
+					$profileSendTo = new ProfileCommandReply();
+					$this->commandManager->process("msg", $line, $sender, $profileSendTo);
+					$output .= $profileSendTo->result . "\n\n";
+				}
+			}
+			
+			$msg = $this->text->make_blob("$name Profile Results", $output);
+		}
 		$sendto->reply($msg);
 	}
 }
