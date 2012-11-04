fc02126c39c78c1a943bcfda110a502031051db0
Marebone <joyer83@gmail.com>
2012-11-04 10:03:43 +0000

    Building linux archive now contains botmanager folder as it is. Added helper scripts botmanager.py and botmanager.bat to bot's root for easier execution of the Bot Manager.

---
+++ botmanager.bat
@@ -0,0 +1,17 @@
+@title Budabot Bot Manager
+python botmanager
+
+:: This file is part of Budabot.
+::
+:: Budabot is free software: you can redistribute it and/org modify
+:: it under the terms of the GNU General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or
+:: (at your option) any later version.
+::
+:: Budabot is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+:: GNU General Public License for more details.
+::
+:: You should have received a copy of the GNU General Public License
+:: along with Budabot. If not, see <http://www.gnu.org/licenses/>.
