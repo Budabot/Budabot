ac95dd3eb2c8718da4c50ad5f93d871da49ca846
Tyrence <bigwheels16@gmail.com>
2012-11-17 18:48:32 +0000

    added 'tell_min_player_age' setting to control the limit for minimum age of characters sending tells to the bot

--- core/LIMITS/Limits.class.php
+++ core/LIMITS/Limits.class.php
@@ -23,6 +23,12 @@ class Limits {
 
 	/** @Inject */
 	public $playerManager;
+	
+	/** @Inject */
+	public $playerHistoryManager;
+	
+	/** @Inject */
+	public $util;
 
 	/** @Inject */
 	public $whitelist;
@@ -34,6 +40,7 @@ class Limits {
 		$this->settingManager->add($this->moduleName, "tell_req_lvl", "Minimum level required to send tell to bot", "edit", "number", "0", "0;10;50;100;150;190;205;215");
 		$this->settingManager->add($this->moduleName, "tell_req_faction", "Faction required to send tell to bot", "edit", "options", "all", "all;Omni;Neutral;Clan;not Omni;not Neutral;not Clan");
 		$this->settingManager->add($this->moduleName, "tell_req_open", "General requirements to send tell to bot", "edit", "options", "all", "all;member;guild;rl;mod");
+		$this->settingManager->add($this->moduleName, "tell_min_player_age", "Minimum age of player to send tell to bot", "edit", "time", "1s", "1s;7days;1month;1year;2years", '', 'mod', 'limits.txt');
 	}
 
 	public function check($sender, $message) {
@@ -45,49 +52,67 @@ class Limits {
 			// if mod or higher, grant access automatically
 			return true;
 		} else {
-			// if neither minlvl, faction, or access level is set, then check passes
-			if ($this->settingManager->get("tell_req_lvl") == 0 &&
-					$this->settingManager->get("tell_req_faction") == "all" &&
-					$this->settingManager->get("tell_req_open") == "all") {
-
-				return true;
-			}
-			
 			// check access level
-			if (!$this->accessManager->checkAccess($sender, $this->settingManager->get("tell_req_open"))) {
-				$msg = "Error! You must have an access level of at least '" . $this->settingManager->get("tell_req_open") . "' to send a tell to this bot.";
-				$this->chatBot->sendTell($msg, $sender);
-				return false;
-			}
-
-			// get player info which is needed for following checks
-			$whois = $this->playerManager->get_by_name($sender);
-			if ($whois === null) {
-				$msg = "Error! Unable to get your character info. Please try again later.";
-				$this->chatBot->sendTell($msg, $sender);
-				return false;
-			}
-
-			// check minlvl
-			if ($this->settingManager->get("tell_req_lvl") != 0 && $this->settingManager->get("tell_req_lvl") > $whois->level) {
-				$msg = "Error! You must be higher than level " . $this->settingManager->get("tell_req_lvl") . " to send a tell to this bot.";
-				$this->chatBot->sendTell($msg, $sender);
-				return false;
-			}
-
-			// check faction limit
-			if (($this->settingManager->get("tell_req_faction") == "Omni" || $this->settingManager->get("tell_req_faction") == "Clan" || $this->settingManager->get("tell_req_faction") == "Neutral") && $this->settingManager->get("tell_req_faction") != $whois->faction) {
-				$msg = "Error! You must be " . $this->settingManager->get("tell_req_faction") . " to send a tell to this bot.";
-				$this->chatBot->sendTell($msg, $sender);
-				return false;
-			} else if ($this->settingManager->get("tell_req_faction") == "not Omni" || $this->settingManager->get("tell_req_faction") == "not Clan" || $this->settingManager->get("tell_req_faction") == "not Neutral") {
-				$tmp = explode(" ", $this->settingManager->get("tell_req_faction"));
-				if ($tmp[1] == $whois->faction) {
-					$msg = "Error! You must not be {$tmp[1]} to send a tell to this bot.";
+			if ($this->settingManager->get("tell_req_open") != "all") {
+				if (!$this->accessManager->checkAccess($sender, $this->settingManager->get("tell_req_open"))) {
+					$msg = "Error! You must have an access level of at least '" . $this->settingManager->get("tell_req_open") . "' to send a tell to this bot.";
 					$this->chatBot->sendTell($msg, $sender);
 					return false;
 				}
 			}
+
+			if ($this->settingManager->get("tell_req_lvl") == 0 || $this->settingManager->get("tell_req_faction") != "all") {
+				// get player info which is needed for following checks
+				$whois = $this->playerManager->get_by_name($sender);
+				if ($whois === null) {
+					$msg = "Error! Unable to get your character info. Please try again later.";
+					$this->chatBot->sendTell($msg, $sender);
+					return false;
+				}
+
+				// check minlvl
+				if ($this->settingManager->get("tell_req_lvl") != 0 && $this->settingManager->get("tell_req_lvl") > $whois->level) {
+					$msg = "Error! You must be higher than level " . $this->settingManager->get("tell_req_lvl") . " to send a tell to this bot.";
+					$this->chatBot->sendTell($msg, $sender);
+					return false;
+				}
+
+				// check faction limit
+				if (($this->settingManager->get("tell_req_faction") == "Omni" || $this->settingManager->get("tell_req_faction") == "Clan" || $this->settingManager->get("tell_req_faction") == "Neutral") && $this->settingManager->get("tell_req_faction") != $whois->faction) {
+					$msg = "Error! You must be " . $this->settingManager->get("tell_req_faction") . " to send a tell to this bot.";
+					$this->chatBot->sendTell($msg, $sender);
+					return false;
+				} else if ($this->settingManager->get("tell_req_faction") == "not Omni" || $this->settingManager->get("tell_req_faction") == "not Clan" || $this->settingManager->get("tell_req_faction") == "not Neutral") {
+					$tmp = explode(" ", $this->settingManager->get("tell_req_faction"));
+					if ($tmp[1] == $whois->faction) {
+						$msg = "Error! You must not be {$tmp[1]} to send a tell to this bot.";
+						$this->chatBot->sendTell($msg, $sender);
+						return false;
+					}
+				}
+			}
+			
+			// check player age
+			if ($this->settingManager->get("tell_min_player_age") > 1) {
+				$history = $this->playerHistoryManager->lookup($sender, $this->chatBot->vars['dimension']);
+				if ($history === null) {
+					$msg = "Error! Unable to get your character history. Please try again later.";
+					$this->chatBot->sendTell($msg, $sender);
+					return false;
+				} else {
+					$minAge = time() - $this->settingManager->get("tell_min_player_age");
+					$entry = array_pop($history->data);
+
+					$age = strtotime($entry->date);
+					if ($age > $minAge) {
+						$timeString = $this->util->unixtime_to_readable($this->settingManager->get("tell_min_player_age"));
+						$msg = "Error! You must be at least $timeString old to send a tell to this bot.";
+						$this->chatBot->sendTell($msg, $sender);
+						return false;
+					}
+				}
+			}
+			
 			return true;
 		}
 	}
