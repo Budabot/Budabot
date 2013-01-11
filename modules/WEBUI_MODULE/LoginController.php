7d88d10d762582f3a67c62bb035cfc7c40b55cb3
Marebone <joyer83@gmail.com>
2013-01-12 00:35:46 +0200

    Added skeleton login handler + unit tests

---
+++ modules/WEBUI_MODULE/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WebUi;
+
+/**
+ * @Instance
+ */
+class LoginController {
+
+	/**
+	 * Name of the module.
+	 * Set automatically by module loader.
+	 */
+	public $moduleName;
+
+	/** @Inject */
+	public $httpApi;
+
+	/**
+	 * @Setup
+	 */
+	public function setup() {
+		$this->httpApi->registerHandler("|^/{$this->moduleName}/login|i", array($this, 'handleLoginResource'));
+	}
+
+	public function handleLoginResource($request, $response) {
+		$response->writeHead(200);
+		$response->end();
+	}
+}
---
+++ tests/modules/WEBUI_MODULE/LoginControllerTest.php
@@ -0,0 +1,63 @@
+<?php
+namespace WebUiTest;
+
+require_once "PHPUnit/Autoload.php";
+require_once "tests/helpers/BudabotTestCase.php";
+require_once "modules/WEBUI_MODULE/LoginController.php";
+require_once "core/HTTPAPI_MODULE/HttpApiController.class.php";
+
+interface MockRequest {
+}
+
+interface MockResponse {
+	function writeHead();
+	function end();
+}
+
+class LoginControllerTest extends \BudabotTestCase {
+
+	private $ctrl;
+
+	function setUp() {
+		$this->ctrl = new \WebUi\LoginController();
+		$this->ctrl->moduleName = 'WEBUI_MODULE';
+		$this->httpApi = $this->injectMock($this->ctrl, 'httpapi', 'HttpApiController');
+	}
+
+	function testIsAutoInstanced() {
+		$this->assertTrue($this->isAutoInstanced($this->ctrl));
+	}
+
+	function testHasSetupHandler() {
+		$this->assertTrue($this->hasSetupHandler($this->ctrl));
+	}
+
+	function testHasHttpApiInject() {
+		$this->assertTrue($this->hasInjection($this->ctrl, 'httpapi'));
+	}
+
+	function testSetupHandlerRegistersLoginHandler() {
+		$this->httpApi->expects($this->once())->method('registerHandler');
+		$this->callSetupHandler($this->ctrl);
+	}
+
+	function testSetupHandlerRegistersLoginHandlerToCorrectPath() {
+		$this->httpApi->expects($this->once())->method('registerHandler')->with("|^/WEBUI_MODULE/login|i");
+		$this->callSetupHandler($this->ctrl);
+	}
+
+	function testSetupHandlerRegistersLoginHandlerWithCallback() {
+		$this->httpApi->expects($this->once())
+			->method('registerHandler')
+			->with($this->anything(), $this->isCallable());
+		$this->callSetupHandler($this->ctrl);
+	}
+
+	function testLoginHandlerWritesResponse() {
+		$request = $this->getMock('WebUiTest\MockRequest');
+		$response = $this->getMock('WebUiTest\MockResponse');
+		$response->expects($this->once())->method('writeHead')->with(200);
+		$response->expects($this->once())->method('end');
+		$this->ctrl->handleLoginResource($request, $response);
+	}
+}
