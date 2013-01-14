e64c25c039e598d40ea35c9db9ed311faeebf369
Marebone <joyer83@gmail.com>
2013-01-14 22:34:58 +0200

    Added passing of credentials in the ajax call. Added page redirection on success. Added showing of error message on failure.

--- modules/WEBUI_MODULE/resources/js/login.js
+++ modules/WEBUI_MODULE/resources/js/login.js
@@ -1,9 +1,30 @@
+function redirectToIndex() {
+	window.location.href = '/webui_module/';
+}
+
+function setErrorMessage(message) {
+	$('#message').innerText = message;
+}
+
+function onLoginClicked() {
+	var data = {
+		username: $('#username').val(),
+		password: $('#password').val()
+	};
+	$.ajax({
+		url: 'check_login',
+		data: data,
+		success: function(result) {
+			if (result === '1') {
+				redirectToIndex();
+			} else {
+				setErrorMessage('Invalid username or password');
+			}
+		}
+	});
+	return false;
+}
 
 $(document).ready(function() {
-	$('#login').click(function() {
-		$.ajax({
-			url: 'check_login'
-		});
-		return false;
-	});
+	$('#login').click(onLoginClicked);
 });
