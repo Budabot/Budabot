640016f6efa3d9bc7e1ac33a0eeb67c643952dc6
Marebone <joyer83@gmail.com>
2013-01-15 19:15:17 +0200

    Added checking of login credentials on server side.

--- modules/WEBUI_MODULE/LoginController.php
+++ modules/WEBUI_MODULE/LoginController.php
@@ -16,17 +16,45 @@ class LoginController {
 	/** @Inject */
 	public $httpApi;
 
+	/** @Inject */
+	public $preferences;
+
 	/**
 	 * @Setup
 	 */
 	public function setup() {
 		$this->httpApi->registerHandler("|^/{$this->moduleName}/login|i", array($this, 'handleLoginResource'));
+		$this->httpApi->registerHandler("|^/{$this->moduleName}/check_login|i", array($this, 'handleCheckLoginResource'));
 		$this->httpApi->registerHandler("|^/{$this->moduleName}/js/login.js|i", array($this, 'handleLoginJsResource'));
 	}
 
 	public function handleLoginResource($request, $response) {
 		$response->writeHead(200);
 		$response->end(file_get_contents(__DIR__ .'/resources/login.html'));
+	}
+
+	public function handleCheckLoginResource($request, $response, $data) {
+		$isValid = false;
+		$params = self::parseQueryParams($data);
+		if (isset($params['username']) && isset($params['password'])) {
+			$isValid = $this->checkCredentials($params['username'], $params['password']);
+		}
+
+		$response->writeHead(200);
+		if ($isValid) {
+			$response->end('1');
+		}
+	}
+
+	private static function parseQueryParams($data) {
+		$params = array();
+		parse_str($data, $params);
+		return $params;
+	}
+
+	private function checkCredentials($username, $password) {
+		$validPassword = $this->preferences->get($username, 'apipassword');
+		return $validPassword === $password;
 	}
 
 	public function handleLoginJsResource($request, $response) {
--- tests/modules/WEBUI_MODULE/LoginControllerTest.php
+++ tests/modules/WEBUI_MODULE/LoginControllerTest.php
@@ -6,6 +6,7 @@ require_once "PHPUnit/Autoload.php";
 require_once "tests/helpers/BudabotTestCase.php";
 require_once "modules/WEBUI_MODULE/LoginController.php";
 require_once "core/HTTPAPI_MODULE/HttpApiController.class.php";
+require_once "core/PREFERENCES/Preferences.class.php";
 
 interface MockRequest {
 }
@@ -23,6 +24,7 @@ class LoginControllerTest extends \BudabotTestCase {
 		$this->ctrl = new \WebUi\LoginController();
 		$this->ctrl->moduleName = 'WEBUI_MODULE';
 		$this->httpApi = $this->injectMock($this->ctrl, 'httpapi', 'HttpApiController');
+		$this->preferences = $this->injectMock($this->ctrl, 'preferences', 'Preferences');
 	}
 
 	function testIsAutoInstanced() {
@@ -35,6 +37,10 @@ class LoginControllerTest extends \BudabotTestCase {
 
 	function testHasHttpApiInject() {
 		$this->assertTrue($this->hasInjection($this->ctrl, 'httpapi'));
+	}
+
+	function testHasPreferencesInject() {
+		$this->assertTrue($this->hasInjection($this->ctrl, 'preferences'));
 	}
 
 	function testSetupHandlerRegistersLoginResource() {
@@ -58,10 +64,10 @@ class LoginControllerTest extends \BudabotTestCase {
 		);
 	}
 
-	private function callHandlerCallback($path, $request, $response) {
+	private function callHandlerCallback($path, $request, $response, $data = '') {
 		$callback = null;
 		\Phake::verify($this->httpApi)->registerHandler($path, \Phake::capture($callback));
-		call_user_func($callback, $request, $response);
+		call_user_func($callback, $request, $response, $data);
 	}
 
 	function testSetupHandlerRegistersLoginJsResource() {
@@ -76,5 +82,24 @@ class LoginControllerTest extends \BudabotTestCase {
 
 		\Phake::verify($response)->writeHead(200);
 		\Phake::verify($response)->end(file_get_contents("modules/WEBUI_MODULE/resources/js/login.js"));
+	}
+
+	function testSetupHandlerRegistersCheckLoginResource() {
+		$this->callSetupHandler($this->ctrl);
+		\Phake::verify($this->httpApi)->registerHandler("|^/WEBUI_MODULE/check_login|i", $this->isCallable());
+	}
+
+	function testCheckLoginHandlerWritesSuccessOnValidCredentials() {
+		list($request, $response) = $this->getHandlerMocks();
+		$this->callSetupHandler($this->ctrl);
+		$data = http_build_query(array(
+			'username' => 'fooman',
+			'password' => 'foopass'
+		));
+		\Phake::when($this->preferences)->get('fooman', 'apipassword')->thenReturn('foopass');
+		$this->callHandlerCallback("|^/WEBUI_MODULE/check_login|i", $request, $response, $data);
+
+		\Phake::verify($response)->writeHead(200);
+		\Phake::verify($response)->end('1');
 	}
 }
