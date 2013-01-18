62a21c875083a4cd8e09c3d5eb54fa7358eeec5b
Tyrence <email1@jkbff.com>
2013-01-18 15:22:29 -0700

    removed !namehistory since !whois already shows name history

--- modules/WHOIS_MODULE/WhoisController.class.php
+++ modules/WHOIS_MODULE/WhoisController.class.php
@@ -20,12 +20,6 @@
  *		help        = 'whois.txt'
  *	)
  *	@DefineCommand(
- *		command     = 'namehistory',
- *		accessLevel = 'all', 
- *		description = 'Show name history of a player', 
- *		help        = 'namehistory.txt'
- *	)
- *	@DefineCommand(
  *		command     = 'lookup',
  *		accessLevel = 'all', 
  *		description = 'Find the charId for a player', 
@@ -95,36 +89,6 @@ class WhoisController {
 		if (($packet->type == AOCP_CLIENT_NAME || $packet->type == AOCP_CLIENT_LOOKUP) && $this->util->isValidSender($packet->args[0])) {
 			$this->nameHistoryCache []= $packet->args;
 		}
-	}
-
-	/**
-	 * @HandlesCommand("namehistory")
-	 * @Matches("/^namehistory (.+)$/i")
-	 */
-	public function namehistoryCommand($message, $channel, $sender, $sendto, $args) {
-		$name = ucfirst(strtolower($args[1]));
-		$uid = $this->chatBot->get_uid($name);
-		if (!$uid) {
-			$msg = "<highlight>$name<end> does not exist.";
-			$sendto->reply($msg);
-			return;
-		}
-
-		$sql = "SELECT * FROM name_history WHERE charid = ? AND dimension = <dim> ORDER BY dt DESC";
-		$data = $this->db->query($sql, $uid);
-		$count = count($data);
-
-		$blob = '';
-		if ($count > 0) {
-			forEach ($data as $row) {
-				$blob .= "<green>{$row->name}<end> " . $this->util->date($row->dt) . "\n";
-			}
-			$msg = $this->text->make_blob("Name History for $name ($count)", $blob);
-		} else {
-			$msg = "No name history available.";
-		}
-
-		$sendto->reply($msg);
 	}
 	
 	/**
