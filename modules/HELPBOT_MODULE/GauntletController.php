984b41fd676fda7486ad0ccbb7c2dd4fc3554a96
Tyrence <email1@jkbff.com>
2013-01-24 12:35:10 -0700

    !gauntlet now shows Vizaresh spawn time

--- modules/HELPBOT_MODULE/GauntletController.php
+++ modules/HELPBOT_MODULE/GauntletController.php
@@ -57,21 +57,46 @@ class GauntletController {
 
 		if ($errno != 0) {
 			$msg = "Could not get Gauntlet information: " . $errstr;
-		} else if ($content == 'null') {
-			$msg = "No factions currently have Gauntlet buff.";
 		} else {
-			$statuses = explode('|', $content);
-			$msg = '';
-			$blob = '';
-			forEach ($statuses as $status) {
-				list($faction, $time) = explode(':', $status);
-				$msg .= "$faction: <highlight>" . $this->util->unixtime_to_readable($time) . "<end>. ";
-				$blob .= "$faction has the Gauntlet buff for <highlight>" . $this->util->unixtime_to_readable($time) . "<end>.\n";
-			}
+			$gauntlet = json_decode($content);
 			
-			$blob .= "\n<highlight>Gauntlet info provided by Macross (RK2)<end>";
-			$msg .= $this->text->make_blob("More info", $blob, "Gauntlet");
+			$blob = $this->getBlob($gauntlet);
+			$msg = $this->getMessage($gauntlet) . $this->text->make_blob("More info", $blob, "Gauntlet");
 		}
 		$sendto->reply($msg);
+	}
+	
+	public function getMessage($gauntlet) {
+		if ($gauntlet->Vizaresh != '-1') {
+			$msg .= "<highlight>Vizaresh<end>: " . $this->util->unixtime_to_readable($gauntlet->Vizaresh) . ". ";
+		} else {
+			$msg .= "<highlight>Vizaresh<end>: Unknown spawn time. ";
+		}
+		
+		if ($gauntlet->Clan != '-1') {
+			$msg .= "<Clan>Clan<end>: " . $this->util->unixtime_to_readable($gauntlet->Clan) . ". ";
+		}
+		
+		if ($gauntlet->Omni != '-1') {
+			$msg .= "<Omni>Omni<end>: " . $this->util->unixtime_to_readable($gauntlet->Omni) . ". ";
+		}
+		return $msg;
+	}
+	
+	public function getBlob($gauntlet) {
+		$blob = '';
+		if ($gauntlet->Clan != '-1') {
+			$blob .= "<Clan>Clan<end> has the Gauntlet buff for " . $this->util->unixtime_to_readable($gauntlet->Clan) . ".\n";
+		} else {
+			$blob .= "<Clan>Clan<end> does not currently have the Gauntlet buff.\n";
+		}
+		if ($gauntlet->Omni != '-1') {
+			$blob .= "<Omni>Omni<end> has the Gauntlet buff for " . $this->util->unixtime_to_readable($gauntlet->Omni) . ".\n";
+		} else {
+			$blob .= "<Omni>Omni<end> does not currently have the Gauntlet buff.\n";
+		}
+		$blob .= "<highlight>Vizaresh<end> spawns in " . $this->util->unixtime_to_readable($gauntlet->Vizaresh) . ".\n";
+		$blob .= "\nGauntlet info provided by <highlight>Macross (RK2)<end>";
+		return $blob;
 	}
 }
