38cfab6d18850aa6d00db2a43c4f5f0e9e93e9fa
Tyrence <email1@jkbff.com>
2013-01-26 14:17:26 -0700

    !pb now functions similarly to other commands

--- modules/POCKETBOSS_MODULE/PocketbossController.class.php
+++ modules/POCKETBOSS_MODULE/PocketbossController.class.php
@@ -51,33 +51,51 @@ class PocketbossController {
 	 */
 	public function pbCommand($message, $channel, $sender, $sendto, $args) {
 		$search = $args[1];
+		$data = $this->pbSearchResults($search);
+		$numrows = count($data);
+		if ($numrows == 0) {
+			$msg = "Could not find any pocket bosses that matched your search criteria.";
+		} else if ($numrows == 1) {
+			$name = $data[0]->pb;
+			$blob .= $this->singlePbBlob($name);
+			$msg = $this->text->make_blob("Remains of $name", $blob);
+		} else {
+			$blob = '';
+			forEach ($data as $row) {
+				$pbLink = $this->text->make_chatcmd($row->pb, "/tell <myname> pb $row->pb");
+				$blob .= $pbLink . "\n";
+			}
+			$msg = $this->text->make_blob("Search results for $search ($numrows)", $blob);
+		}
+		$sendto->reply($msg);
+	}
+	
+	public function singlePbBlob($name) {
+		$data = $this->db->query("SELECT * FROM pbdb WHERE pb = ? ORDER BY ql", $name);
+		$symbs = '';
+		forEach ($data as $symb) {
+			$name = "QL $symb->ql $symb->line $symb->slot Symbiant, $symb->type Unit Aban";
+			$symbs .= $this->text->make_item($symb->itemid, $symb->itemid, $symb->ql, $name)."\n";
+		}
+		
+		$blob = "Location: <highlight>$symb->pb_location, $symb->bp_location<end>\n";
+		$blob .= "Found on: <highlight>$symb->bp_mob, Level $symb->bp_lvl<end>\n";
+		$blob .= "_____________________________\n";
+		$blob .= $symbs;
+
+		return $blob;
+	}
+	
+	public function pbSearchResults($search) {
+		$row = $this->db->queryRow("SELECT pb FROM pbdb WHERE pb LIKE ? GROUP BY `pb` ORDER BY `pb`", $search);
+		if ($row !== null) {
+			return array($row);
+		}
+		
 		$tmp = explode(" ", $search);
 		list($query, $params) = $this->util->generateQueryFromParams($tmp, '`pb`');
 
-		$data = $this->db->query("SELECT * FROM pbdb WHERE $query GROUP BY `pb` ORDER BY `pb`", $params);
-		$numrows = count($data);
-		if ($numrows >= 1 && $numrows <= 5) {
-			$msg = "Pocketbosses matching: ";
-			forEach ($data as $row) {
-				$blob = "Location: <highlight>$row->pb_location<end>\n";
-				$blob .= "Found on: <highlight>$row->bp_mob<end>\n";
-				$blob .= "Mob Level: <highlight>$row->bp_lvl<end>\n";
-				$blob .= "General Location: <highlight>$row->bp_location<end>\n";
-				$blob .= "_____________________________\n";
-				$data2 = $this->db->query("SELECT * FROM pbdb WHERE pb = ? ORDER BY ql", $row->pb);
-				forEach ($data2 as $symb) {
-					$name = "QL $symb->ql $symb->line $symb->slot Symbiant, $symb->type Unit Aban";
-					$blob .= $this->text->make_item($symb->itemid, $symb->itemid, $symb->ql, $name)."\n";
-				}
-				$msg .= "\n" . $this->text->make_blob("Remains of $row->pb", $blob);
-			}
-		} else if ($numrows > 5) {
-			$msg = "Too many results.";
-		} else {
-			$msg = "Could not find any pocket bosses that matched your search criteria.";
-		}
-
-		$sendto->reply($msg);
+		return $this->db->query("SELECT DISTINCT pb FROM pbdb WHERE $query GROUP BY `pb` ORDER BY `pb`", $params);
 	}
 	
 	/**
