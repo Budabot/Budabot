b1dd09e69404723d872a007c745ece8f2af05976
Tyrence <email1@jkbff.com>
2013-02-11 16:06:39 -0700

    add 'auto_join_channel' setting value to buddy list

--- modules/DEV_MODULE/AutoJoinController.php
+++ modules/DEV_MODULE/AutoJoinController.php
@@ -19,6 +19,7 @@ class AutoJoinController extends AutoInject {
 	 */
 	public function setup() {
 		$this->settingManager->add($this->moduleName, "auto_join_channel", "Private channel to automatically join", "edit", "text", "none", "none");
+		$this->settingManager->registerChangeListener("auto_join_channel", array($this, 'changeAutoJoinChannel'));
 	}
 	
 	/**
@@ -41,12 +42,31 @@ class AutoJoinController extends AutoInject {
 		}
 	}
 	
+	/**
+	 * @Event("connect")
+	 * @Description("Add auto join channel to buddy list")
+	 */
+	public function acceptInviteForExternalCommandsEvent($eventObj) {
+		if (strtolower($this->setting->auto_join_channel) != 'none') {
+			$this->buddylistManager->add($this->setting->auto_join_channel, 'autojoin');
+		}
+	}
+	
 	public function isAutoJoinChannel($name) {
 		$name = strtolower($name);
 		if ($name == 'none') {
 			return false;
 		} else {
 			return strtolower($this->setting->auto_join_channel) == $name;
+		}
+	}
+	
+	public function changeAutoJoinChannel($name, $oldValue, $newValue) {
+		if (strtolower($oldValue) != 'none') {
+			$this->buddylistManager->remove($oldValue, 'autojoin');
+		}
+		if (strtolower($newValue) != 'none') {
+			$this->buddylistManager->add($newValue, 'autojoin');
 		}
 	}
 }
