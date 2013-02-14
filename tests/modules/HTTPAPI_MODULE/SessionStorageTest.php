cc10ffd8954ce99b8eae579c387975b22d312d10
Marebone <joyer83@gmail.com>
2013-02-14 23:18:19 +0200

    Added SessionStorage class for storing session data.

---
+++ tests/modules/HTTPAPI_MODULE/SessionStorageTest.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once 'Phake.php';
+require_once 'PHPUnit/Autoload.php';
+require_once __DIR__ . '/../../../lib/vendor/autoload.php';
+require_once __DIR__ . '/../../helpers/BudabotTestCase.php';
+require_once __DIR__ . '/../../../core/HTTPAPI_MODULE/SessionStorage.php';
+
+
+class SessionStorageTest extends \BudabotTestCase {
+
+	/**
+	 * @var SessionStorage
+	 */
+	private $storage;
+
+	function setUp() {
+		$this->storage = new SessionStorage();
+	}
+
+	function testIsAutoInstanced() {
+		$this->assertTrue($this->isAutoInstanced($this->storage));
+	}
+
+	function testCreateSessionReturnsNewId() {
+		$id1 = $this->storage->createSession();
+		$id2 = $this->storage->createSession();
+		$this->assertNotEquals($id1, $id2);
+	}
+
+	function testHasSessionReturnsFalseWhenNoSuchSessionIdExist() {
+		$this->assertFalse($this->storage->hasSession('deadf00d'));
+	}
+
+	function testHasSessionReturnsFalseWithNullId() {
+		$this->assertFalse($this->storage->hasSession(null));
+	}
+
+	function testHasSessionReturnsTrueWhenSessionIdExists() {
+		$id = $this->storage->createSession();
+		$this->assertTrue($this->storage->hasSession($id));
+	}
+
+	function testDestroySessionDestroysExistingSession() {
+		$id = $this->storage->createSession();
+		$this->storage->destroySession($id);
+		$this->assertFalse($this->storage->hasSession($id));
+	}
+
+	function testSetGetDataReturnsNullWithNonExistingSession() {
+		$this->storage->setData('deadf00d', 'foo', 'bar');
+		$this->assertNull($this->storage->getData('deadf00d', 'foo'));
+	}
+
+	function testSetGetDataReturnsSameValueWithExistingSession() {
+		$id = $this->storage->createSession();
+		$this->storage->setData($id, 'foo', 'bar');
+		$this->assertEquals('bar', $this->storage->getData($id, 'foo'));
+	}
+}
