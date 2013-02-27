ca943b032d127113966bcb62338e31680b6d626f
Tyrence <email1@jkbff.com>
2013-02-26 19:20:12 -0700

    updated message for invalid server for merge changes

--- core/BotRunner.php
+++ core/BotRunner.php
@@ -219,7 +219,7 @@ Contacts:      Tyrence, Marebone
 			$server = "chat.d1.funcom.com";
 			$port = 7105;
 		} else {
-			LegacyLogger::log('ERROR', 'StartUp', "No valid server to connect with! Available dimensions are 1, 2, 4 and 5.");
+			LegacyLogger::log('ERROR', 'StartUp', "No valid server to connect with! Available dimensions are 4 and 5.");
 			sleep(10);
 			die();
 		}
