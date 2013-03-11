8b40d0738a3c486a5feef53d1f7adcfa2e63048c
Tyrence <email1@jkbff.com>
2013-03-11 13:11:07 -0600

    changed missing help files log message from ERROR to WARN

--- core/HelpManager.class.php
+++ core/HelpManager.class.php
@@ -109,7 +109,7 @@ class HelpManager extends Annotation {
 	public function checkForHelpFile($module, $file) {
 		$actualFilename = $this->util->verify_filename($module . '/' . $file);
 		if ($actualFilename == '') {
-			$this->logger->log('ERROR', "Error in registering the help file {$module}/{$file}. The file doesn't exist!");
+			$this->logger->log('WARN', "Error in registering the help file {$module}/{$file}. The file doesn't exist!");
 		}
 		return $actualFilename;
 	}
