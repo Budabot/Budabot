f364e2901fa1f387971442a1fcc589230b8fd5c8
Tyrence <email1@jkbff.com>
2014-04-16 03:09:19 -0600

    added !htmldecode

---
+++ modules/DEV_MODULE/HtmlDecodeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Budabot\User\Modules;
+
+use Budabot\Core\AutoInject;
+
+/**
+ * Authors: 
+ *	- Tyrence (RK2)
+ *
+ * @Instance
+ *
+ * Commands this controller contains:
+ *	@DefineCommand(
+ *		command     = 'htmldecode', 
+ *		accessLevel = 'all', 
+ *		description = 'Execute a command by first decoding html entities', 
+ *		help        = 'htmldecode.txt'
+ *	)
+ */
+class HtmlDecodeController extends AutoInject {
+
+	/**
+	 * Name of the module.
+	 * Set automatically by module loader.
+	 */
+	public $moduleName;
+	
+	/**
+	 * @HandlesCommand("htmldecode")
+	 * @Matches("/^htmldecode (.+)$/i")
+	 */
+	public function runasCommand($message, $channel, $sender, $sendto, $args) {
+		$command = html_entity_decode($args[1], ENT_QUOTES);
+		$this->commandManager->process($channel, $command, $sender, $sendto);
+	}
+}
