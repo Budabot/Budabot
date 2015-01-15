360833ad5c4704d810a538f5637019ac5f5f8d26
Tyrence <email1@jkbff.com>
2015-01-15 02:07:33 -0700

    fix for bank.sql

--- modules/BANK_MODULE/bank.sql
+++ modules/BANK_MODULE/bank.sql
@@ -1,1 +1,1 @@
-CREATE TABLE bank (name varchar(150), lowid int, highid int, ql int, player VARCHAR(20), container VARCHAR(150), container_id INT, location VARCHAR(150));
+CREATE TABLE IF NOT EXISTS bank (name varchar(150), lowid int, highid int, ql int, player VARCHAR(20), container VARCHAR(150), container_id INT, location VARCHAR(150));
