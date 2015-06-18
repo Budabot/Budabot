0ad2b5ee5443fdd78143d56f256a09cf6c98a9fa
Tyrence <email1@jkbff.com>
2015-06-18 01:05:50 -0600

    !symb now allows searching by symb line

--- modules/IMPLANT_MODULE/PocketbossController.class.php
+++ modules/IMPLANT_MODULE/PocketbossController.class.php
@@ -106,12 +106,16 @@ class PocketbossController {
 	 * @HandlesCommand("symbiant")
 	 * @Matches("/^symbiant ([a-z]+)$/i")
 	 * @Matches("/^symbiant ([a-z]+) ([a-z]+)$/i")
+	 * @Matches("/^symbiant ([a-z]+) ([a-z]+) ([a-z]+)$/i")
 	 */
 	public function symbiantCommand($message, $channel, $sender, $sendto, $args) {
 		$paramCount = count($args) - 1;
 
 		$slot = '%';
 		$symbtype = '%';
+		$line = '%';
+		
+		$lines = $this->db->query("SELECT DISTINCT line FROM pocketboss");
 
 		for ($i = 1; $i <= $paramCount; $i++) {
 			switch (strtolower($args[$i])) {
@@ -172,21 +176,32 @@ class PocketbossController {
 					$slot = "Feet";
 					break;
 				default:
-					if (preg_match("/^a/i", $args[$i])) {
+					// check if it's a line
+					forEach ($lines as $l) {
+						if (strtolower($l->line) == strtolower($args[$i])) {
+							$line = $l->line;
+							break 2;
+						}
+					}
+
+					// check if it's a type
+					if (preg_match("/^art/i", $args[$i])) {
 						$symbtype = "Artillery";
-					} else if (preg_match("/^s/i", $args[$i])) {
+					} else if (preg_match("/^sup/i", $args[$i])) {
 						$symbtype = "Support";
-					} else if (preg_match("/^i/i", $args[$i])) {
+					} else if (preg_match("/^inf/i", $args[$i])) {
 						$symbtype = "Infantry";
-					} else if (preg_match("/^e/i", $args[$i])) {
+					} else if (preg_match("/^ext/i", $args[$i])) {
 						$symbtype = "Extermination";
-					} else if (preg_match("/^c/i", $args[$i])) {
+					} else if (preg_match("/^control/i", $args[$i])) {
 						$symbtype = "Control";
+					} else {
+						return false;
 					}
 			}
 		}
 
-		$data = $this->db->query("SELECT * FROM pocketboss WHERE `slot` LIKE ? AND `type` LIKE ? ORDER BY `ql` DESC, `type` ASC", $slot, $symbtype);
+		$data = $this->db->query("SELECT * FROM pocketboss WHERE `slot` LIKE ? AND `type` LIKE ? AND `line` LIKE ? ORDER BY `ql` DESC, `type` ASC", $slot, $symbtype, $line);
 		$numrows = count($data);
 		if ($numrows != 0) {
 			$implantDesignerLink = $this->text->make_chatcmd("implant designer", "/tell <myname> implantdesigner");
