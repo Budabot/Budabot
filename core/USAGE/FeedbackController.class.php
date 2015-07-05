9902d9be3ec33a0c0d5ea0903f7a4edad73a9abc
Tyrence <email1@jkbff.com>
2015-07-05 01:07:51 -0600

    removed !feedback confirm

--- core/USAGE/FeedbackController.class.php
+++ core/USAGE/FeedbackController.class.php
@@ -37,9 +37,9 @@ class FeedbackController {
 	
 	/**
 	 * @HandlesCommand("feedback")
-	 * @Matches("/^feedback confirm (.*)$/i")
+	 * @Matches("/^feedback (.*)$/i")
 	 */
-	public function feedbackConfirmCommand($message, $channel, $sender, $sendto, $args) {
+	public function feedbackCommand($message, $channel, $sender, $sendto, $args) {
 		$message = $args[1];
 		
 		global $version;
@@ -55,31 +55,8 @@ class FeedbackController {
 		$msg = $this->http->post($url)->withQueryParams($postArray)->waitAndReturnResponse()->body;
 		
 		if (empty($msg)) {
-			$msg = "There was an error submitting your request.1";
+			$msg = "There was an error submitting your request.";
 		}
-		$sendto->reply($msg);
-	}
-	
-	/**
-	 * @HandlesCommand("feedback")
-	 * @Matches("/^feedback (.*)$/i")
-	 */
-	public function feedbackCommand($message, $channel, $sender, $sendto, $args) {
-		$message = $args[1];
-		
-		global $version;
-
-		$blob = '';
-		$blob .= "This is the message that will be sent to the Budabot team:\n\n";
-		$blob .= "Sender: <highlight>$sender<end>\n";
-		$blob .= "Version: <highlight>$version<end>\n";
-		$blob .= "Message: <highlight>$message<end>\n\n";
-		$blob .= "To send this feedback, click here: ";
-		$blob .= $this->text->make_chatcmd("Submit message", "/tell <myname> feedback confirm $message");
-		$blob .= "\n\nTo change your message use the <symbol>feedback command to create a new message.\n\n";
-		$blob .= "The Budabot team accepts any type of feedback including bug reports, feature requests, praise, and criticism.";
-
-		$msg = $this->text->make_blob("Verify Your Feedback Submission", $blob);
 		$sendto->reply($msg);
 	}
 }
