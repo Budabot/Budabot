d2a9d1c8ab6b36a92d5a306474f1505156a2ac87
Tyrence <email1@jkbff.com>
2015-10-06 09:25:06 -0600

    Util::genRandomString() now optionally allows caller to define characters to use

--- core/Util.class.php
+++ core/Util.class.php
@@ -337,8 +337,7 @@ class Util {
 	}
 
 	// taken from: http://www.lost-in-code.com/programming/php-code/php-random-string-with-numbers-and-letters/
-	public function genRandomString($length = 10) {
-		$characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+	public function genRandomString($length = 10, $characters = '0123456789abcdefghijklmnopqrstuvwxyz') {
 		$string = '';
 		for ($p = 0; $p < $length; $p++) {
 			$string .= $characters[mt_rand(0, strlen($characters))];
