f26be84f8de63a89135f58072f3162f8a5683a7c
Tyrence <email1@jkbff.com>
2018-01-14 12:01:32 -0700

    removed !play; updated !players and !findprof

--- modules/NATUBOT_MODULE/NatubotController.class.php
+++ modules/NATUBOT_MODULE/NatubotController.class.php
@@ -15,19 +15,19 @@ namespace Natubot\Modules;
  * Commands this controller contains:
  *	@DefineCommand(
  *		command     = 'play', 
- *		accessLevel = 'all', 
+ *		accessLevel = 'member', 
  *		description = 'Shows online players and their currently logged alts', 
  *		help        = 'play.txt'
  *	)
  *	@DefineCommand(
  *		command     = 'players', 
- *		accessLevel = 'all', 
+ *		accessLevel = 'member', 
  *		description = 'Shows online players and their currently logged alts in a long format', 
  *		help        = 'players.txt'
  *	)
  *	@DefineCommand(
  *		command     = 'findprof', 
- *		accessLevel = 'all', 
+ *		accessLevel = 'member', 
  *		description = 'Shows online players with alts of a specific profession / level', 
  *		help        = 'findprof.txt'
  *	)
@@ -56,101 +56,37 @@ class NatubotController {
 	/** @Inject */
 	public $chatBot;
 
-
-	/**
-	 * @HandlesCommand("play")
-	 * @Matches("/^play$/i")
-	 */
-	public function playCommand($message, $channel, $sender, $sendto, $args) {
-
-		$mains = $this->db->query("SELECT DISTINCT IFNULL(a.main, o.name) as main FROM online o LEFT JOIN alts a on o.name = a.alt");
-		$count = count($mains);
-
-		if ($count > 0) {
-			forEach ($mains as $row) {
-				$blob .= "<highlight>$row->main<end> on ";
-
-				// First check for mains...
-				$onlineMains = $this->db->query("SELECT name FROM online WHERE name = ?", $row->main);
-				if (count($onlineMains) > 0) {
-					foreach($onlineMains as $row2) {
-						$playerInfo = $this->playerManager->getByName($row2->name);
-						if ($playerInfo === null) {
-							$blob .= "($row2->name) ";
-						} else {
-							$prof = $this->util->getProfessionAbbreviation($playerInfo->profession);
-							$blob.= "($row2->name - $playerInfo->level/<green>$playerInfo->ai_level<end> $prof) ";
-						}
-					}
-				}
-
-				// ... then get alts
-				$onlineAlts = $this->db->query("SELECT a.alt FROM online o JOIN alts a ON a.alt = o.name WHERE a.main = ?", $row->main);
-				if (count($onlineAlts) > 0) {
-					foreach ($onlineAlts as $row3) {
-						$playerInfo = $this->playerManager->getByName($row3->alt);
-						if ($playerInfo === null) {
-							$blob .= "($row3->alt) ";
-						} else {
-							$prof = $this->util->getProfessionAbbreviation($playerInfo->profession);
-							$blob.= "($row3->alt - $playerInfo->level/<green>$playerInfo->ai_level<end> $prof) ";
-						}
-					}
-				}
-
-				$blob .= "\n";
-			}
-			$blob .= "\nWritten by Naturarum (RK2)";
-			$msg = $this->text->makeBlob("$count Players Currently Online", $blob);
-		} else {
-			$msg = "No players online";
-		}
-
-		$sendto->reply($msg);
-	} 
-	
 	/**
 	 * @HandlesCommand("players")
 	 * @Matches("/^players$/i")
 	 */
 	public function playersCommand($message, $channel, $sender, $sendto, $args) {
-		$mains = $this->db->query("SELECT DISTINCT IFNULL(a.main, o.name) as main FROM online o LEFT JOIN alts a on o.name = a.alt");
-		$count = count($mains);
+		$sql = "
+			SELECT p.*, COALESCE(a.main, o.name) AS pmain
+			FROM online o
+			LEFT JOIN alts a ON o.name = a.alt
+			LEFT JOIN players p ON o.name = p.name
+			ORDER BY pmain ASC";
+		$data = $this->db->query($sql);
+		$count = count($data);
+		$currentMain = "";
 
 		if ($count > 0) {
-			forEach ($mains as $row) {
-				$blob .= "<highlight>$row->main<end> on\n";
-
-				// First check for mains...
-				$onlineMains = $this->db->query("SELECT name FROM online WHERE name = ?", $row->main);
-				if (count($onlineMains) > 0) {
-					forEach ($onlineMains as $row2) {
-						$playerInfo = $this->playerManager->getByName($row2->name);
-						if ($playerInfo === null) {
-							$blob .= "| ($row2->name)\n";
-						} else {
-							$blob.= "| $row2->name - $playerInfo->level/<green>$playerInfo->ai_level<end> $playerInfo->breed $playerInfo->profession\n";
-						}
-					}
+			forEach ($data as $row) {
+				if ($currentMain != $row->pmain) {
+					$blob .= "<highlight>$row->pmain<end> on\n";
+					$currentMain = $row->pmain;
 				}
 
-				// ... then get alts
-				$onlineAlts = $this->db->query("SELECT a.alt FROM online o JOIN alts a ON a.alt = o.name WHERE a.main = ?", $row->main);
-				if (count($onlineAlts) > 0) {
-					foreach ($onlineAlts as $row3) {
-						$playerInfo = $this->playerManager->getByName($row3->alt);
-						if ($playerInfo === null) {
-							$blob .= "| ($row3->alt)\n";
-						} else {
-							$blob.= "| $row3->alt - $playerInfo->level/<green>$playerInfo->ai_level<end> $playerInfo->breed $playerInfo->profession\n";
-						}
-					}
+				if ($row->profession === null) {
+					$blob .= "| ($row->name)\n";
+				} else {
+					$prof = $this->util->getProfessionAbbreviation($row->profession);
+					$blob.= "| $row->name - $row->level/<green>$row->ai_level<end> $prof\n";
 				}
-
-				$blob .= "\n";
 			}
-			$blob .= "Written by Naturarum (RK2)";
-			$msg = $this->text->makeBlob("$count Players Currently Online", $blob);
+			$blob .= "\nWritten by Naturarum (RK2)";
+			$msg = $this->text->makeBlob("Players Online ($count)", $blob);
 		} else {
 			$msg = "No players online";
 		}
@@ -165,38 +101,47 @@ class NatubotController {
 	public function findProfCommand($message, $channel, $sender, $sendto, $args) {
 		// Normalize profession information and exit on bad
 		$prof = $this->util->getProfessionName($args[1]);
-		if ($prof === null) {
+		if (empty($prof)) {
 			return false;
 		}
 
-		// Search for alts of online players that fit the criteria
-		$onlineMains = $this->db->query("SELECT DISTINCT IFNULL(a.main, o.name) as name FROM online o LEFT JOIN alts a ON a.alt = o.name");
-		forEach ($onlineMains as $row) {
-			// Determine if $row has any alts of the appropriate profession
-			$sql = "SELECT name FROM alts a JOIN players p ON a.alt = p.name WHERE p.profession = ? AND a.main = ? UNION SELECT p.name FROM players p WHERE p.profession = ? AND p.name = ?";
-			$profCheck = $this->db->query($sql, $prof, $row->name, $prof, $row->name);
-			if (count($profCheck) > 0) {
-				$blob .= "<highlight>$row->name<end> has\n";
-				forEach ($profCheck as $row2) {
-					$playerInfo = $this->playerManager->getByName($row2->name);
-					if ($playerInfo === null) {
-						$blob .= "| $row2->name\n";
-					} else {
-						$blob .= "| $row2->name - $playerInfo->level/<green>$playerInfo->ai_level<end> $playerInfo->breed $playerInfo->profession\n";
-					}
+		$sql = "
+			SELECT DISTINCT p.*, COALESCE(a.main, o.name) AS pmain, (CASE WHEN o2.name IS NULL THEN 0 ELSE 1 END) AS online
+			FROM online o
+			LEFT JOIN alts a ON o.name = a.alt
+			LEFT JOIN alts a2 ON a2.main = pmain
+			LEFT JOIN players p ON a2.alt = p.name OR pmain = p.name
+			LEFT JOIN online o2 ON p.name = o2.name
+			WHERE p.profession = ?
+			ORDER BY pmain ASC";
+		$data = $this->db->query($sql, $prof);
+		$count = count($data);
+		$currentMain = "";
+
+		if ($count > 0) {
+			forEach ($data as $row) {
+				if ($currentMain != $row->pmain) {
+					$blob .= "<highlight>$row->pmain<end> has\n";
+					$currentMain = $row->pmain;
+				}
+
+				if ($row->profession === null) {
+					$blob .= "| ($row->name)\n";
+				} else {
+					$prof = $this->util->getProfessionAbbreviation($row->profession);
+					$blob.= "| $row->name - $row->level/<green>$row->ai_level<end> $prof";
+				}
+				if ($row->online == 1) {
+					$blob .= " <green>Online<end>";
 				}
 				$blob .= "\n";
 			}
-		}
-		
-		if (empty($blob)) {
-			$msg = "No results found.";
+			$blob .= "\nWritten by Naturarum (RK2)";
+			$msg = $this->text->makeBlob("$prof Search Results ($count)", $blob);
 		} else {
-			$blob .= "Written by Naturarum (RK2)";
-			$msg = $this->text->makeBlob("$prof Search Results", $blob);
+			$msg = "No players online";
 		}
 
 		$sendto->reply($msg);
 	}
-
 }
